4404b7ebcc02c49f0272a857b58b1b01227f0d68
Willy TARREAU <willy@pcw.(none)>
2006-05-14 10:00:09 +0200

    [RELEASE] released version 1.2.13.1    - an uninitialized field in the struct session could cause a crash when      the session was freed. This has been encountered on Solaris only.    - Solaris and OpenBSD no not support shutdown() on listening socket. Let's      be nice to them by performing a soft stop if pause fails.

--- haproxy.c
+++ haproxy.c
@@ -89,11 +89,11 @@
 #include "include/mini-clist.h"
 
 #ifndef HAPROXY_VERSION
-#define HAPROXY_VERSION "1.2.13"
+#define HAPROXY_VERSION "1.2.13.1"
 #endif
 
 #ifndef HAPROXY_DATE
-#define HAPROXY_DATE	"2006/05/13"
+#define HAPROXY_DATE	"2006/05/14"
 #endif
 
 /* this is for libc5 for example */
