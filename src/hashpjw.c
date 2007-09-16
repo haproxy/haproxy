19629edd48787cf45d03c4d033318d3b4e6c0528
Willy Tarreau <w@1wt.eu>
2007-09-16 07:45:29 +0200

    [CLEANUP] O'Reilly has clarified its license
    O'Reilly has clarified its license affecting 6 files contained in
    haproxy up to and including 1.3.12.X. The updated license has been
    included within these files.
    
    These files are no longer present in versions starting from 1.3.13,
    but it was better to clear any doubt about the redistribution
    conditions at least for 1.3.12.
    
    Special thanks to the people at the Fedora Project for their efforts
    in getting in touch with O'Reilly to clarify the license.

--- src/hashpjw.c
+++ src/hashpjw.c
@@ -8,6 +8,32 @@
   Publishd by O'Reilly
 
   We have added our own struct to these function.
+
+  Updated O'Reilly's copyright notice follows :
+
+  Copyright (c) 1999 O'Reilly & Associates, Inc.
+
+  Mastering Algorithms with C
+  Kyle Loudon
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE, OR ANY OTHER
+  DEALINGS IN THE SOFTWARE.
+
  */
 
 /*****************************************************************************
