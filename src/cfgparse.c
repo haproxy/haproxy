c2e223d81bcefc20596cd44af606f401c8922f14
Willy Tarreau <w@1wt.eu>
2008-10-12 12:07:48 +0200

    [BUG] ensure that listeners from disabled proxies are correctly unbound.
    There is a problem when an instance is marked "disabled". Its ports are
    still bound but will not be unbound upon termination. This causes processes
    to accumulate during soft restarts, and might even cause failures to restart
    new ones due to the inability to bind to the same port.
    
    The ideal solution would be to bind all ports at the end of the configuration
    parsing. An acceptable workaround is to unbind all listeners of disabled
    proxies. This is what the current patch does.

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -2609,6 +2609,8 @@ int readcfgfile(const char *file)
 		struct listener *listener;
 
 		if (curproxy->state == PR_STSTOPPED) {
+			/* ensure we don't keep listeners uselessly bound */
+			stop_proxy(curproxy);
 			curproxy = curproxy->next;
 			continue;
 		}
