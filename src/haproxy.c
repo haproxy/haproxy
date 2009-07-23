b98fd681793db08df3217163554c15cbacec216e
Willy Tarreau <w@1wt.eu>
2009-07-23 13:45:47 +0200

    [CLEANUP] report 2009 not 2008 in the copyright banner.
    (cherry picked from commit b455691af393f831ab44c70b605fd45e7ca0967b)

--- src/haproxy.c
+++ src/haproxy.c
@@ -1,6 +1,6 @@
 /*
  * HA-Proxy : High Availability-enabled HTTP/TCP proxy
- * Copyright 2000-2008  Willy Tarreau <w@1wt.eu>.
+ * Copyright 2000-2009  Willy Tarreau <w@1wt.eu>.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -167,7 +167,7 @@ static char hostname[MAX_HOSTNAME_LEN] = "";
 void display_version()
 {
 	printf("HA-Proxy version " HAPROXY_VERSION " " HAPROXY_DATE"\n");
-	printf("Copyright 2000-2008 Willy Tarreau <w@1wt.eu>\n\n");
+	printf("Copyright 2000-2009 Willy Tarreau <w@1wt.eu>\n\n");
 }
 
 void display_build_opts()
