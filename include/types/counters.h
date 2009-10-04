8d06b8b8db9b3a9c3a4ba66d6d551f962819eb21
Krzysztof Piotr Oledzki <ole@ans.pl>
2009-10-04 14:32:53 +0200

    [MINOR] Introduce include/types/counters.h
    This patch introduces include/types/counters.h that
    will be used to split couters from other structures
    and to create statistics for listeners.

---
+++ include/types/counters.h
@@ -0,0 +1,74 @@
+/*
+  include/types/counters.h
+  This file contains structure declarations for statistics counters.
+
+  Copyright 2008-2009 Krzysztof Piotr Oledzki <ole@ans.pl>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation, version 2.1
+  exclusively.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _TYPES_COUNTERS_H
+#define _TYPES_COUNTERS_H
+
+struct pxcounters {
+	unsigned int feconn_max, beconn_max;	/* max # of active frontend and backend sessions */
+
+	long long cum_feconn, cum_beconn;	/* cumulated number of processed sessions */
+	long long cum_lbconn;			/* cumulated number of sessions processed by load balancing */
+
+	long long bytes_in;			/* number of bytes transferred from the client to the server */
+	long long bytes_out;			/* number of bytes transferred from the server to the client */
+
+	long long denied_req, denied_resp;	/* blocked requests/responses because of security concerns */
+	long long failed_req;			/* failed requests (eg: invalid or timeout) */
+
+	long long failed_conns, failed_resp;	/* failed connect() and responses */
+	long long retries, redispatches;	/* retried and redispatched connections */
+};
+
+struct licounters {
+	unsigned int conn_max;			/* max # of active listener sessions */
+
+	long long cum_conn;			/* cumulated number of processed sessions */
+
+	long long bytes_in;			/* number of bytes transferred from the client to the server */
+	long long bytes_out;			/* number of bytes transferred from the server to the client */
+
+	long long denied_req, denied_resp;	/* blocked requests/responses because of security concerns */
+	long long failed_req;			/* failed requests (eg: invalid or timeout) */
+};
+
+struct srvcounters {
+	long long cum_sess;			/* cumulated number of sessions really sent to this server */
+	long long cum_lbconn;			/* cumulated number of sessions directed by load balancing */
+
+	long long bytes_in;			/* number of bytes transferred from the client to the server */
+	long long bytes_out;			/* number of bytes transferred from the server to the client */
+
+	long long failed_conns, failed_resp;	/* failed connect() and responses */
+	long long retries, redispatches;	/* retried and redispatched connections */
+	long long failed_secu;			/* blocked responses because of security concerns */
+
+	long long failed_checks, down_trans;	/* failed checks and up->down transitions */
+};
+
+#endif /* _TYPES_COUNTERS_H */
+
+/*
+ * Local variables:
+ *  c-indent-level: 8
+ *  c-basic-offset: 8
+ * End:
+ */
