e5e4a79341b3aa8dc322310c0141cf07c7560442
Willy Tarreau <w@1wt.eu>
2009-12-03 23:10:56 +0100

    [BUG] config: fix erroneous check on cookie domain names
    It was a OR instead of a AND, so it was required to have a cookie
    name which contained a dot AND began with a dot.
    (cherry picked from commit a1e107fc13e5d8886bf900f302322bfa6ed35d37)

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -914,10 +914,10 @@ int cfg_parse_listen(const char *file, int linenum, char **args, int inv)
 					return -1;
 				}
 
-				if (*args[cur_arg + 1] != '.' || !strchr(args[cur_arg + 1] + 1, '.')) {
+				if (*args[cur_arg + 1] != '.' && !strchr(args[cur_arg + 1] + 1, '.')) {
 					/* rfc2109, 4.3.2 Rejecting Cookies */
 					Alert("parsing [%s:%d]: domain '%s' contains no embedded"
-						" dots or does not start with a dot.\n",
+						" dots and does not start with a dot.\n",
 						file, linenum, args[cur_arg + 1]);
 					return -1;
 				}
