b695a6e5fad47a485d63642f98c86ad758e48002
Willy Tarreau <w@1wt.eu>
2010-11-14 14:24:27 +0100

    [BUILD] pattern: use 'int' instead of 'int32_t'
    Ross West reported that int32_t breaks compilation on FreeBSD. Since an
    int is 32-bit on all supported platforms and we already rely on that,
    change the type.

--- include/types/pattern.h
+++ include/types/pattern.h
@@ -54,7 +54,7 @@ enum {
 union pattern_arg_data {
 	struct in_addr ip;        /* used for ipv4 type */
 	uint32_t integer;         /* used for unsigned 32bits integer type */
-	int32_t sinteger;         /* used for signed 32bits integer type */
+	int sinteger;             /* used for signed 32bits integer type */
 	struct chunk str;
 };
 
