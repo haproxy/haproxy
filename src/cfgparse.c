afc47ee7fc779ec1d4682ee76eb082c8174740cd
Simon Horman <horms@verge.net.au>
2013-11-25 10:46:35 +0900

    MEDIUM: Remove option lb-agent-chk
    Remove option lb-agent-chk and thus the facility to configure
    a stand-alone agent health check. This feature was added by
    "MEDIUM: checks: Add agent health check". It will be replaced
    by subsequent patches with a features to allow an agent check
    to be run as either a secondary check, along with any of the existing
    checks, or as part of an http check with the status returned
    in an HTTP header.
    
    This patch does not entirely revert "MEDIUM: checks: Add agent health
    check". The infrastructure it provides to parse the results of an
    agent health check remains and will be re-used by the planned features
    that are mentioned above.
    
    Signed-off-by: Simon Horman <horms@verge.net.au>

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -3571,13 +3571,6 @@ stats_error_parsing:
 				}
 			}
 		}
-		else if (!strcmp(args[1], "lb-agent-chk")) {
-			/* use dynmaic health check */
-			free(curproxy->check_req);
-			curproxy->check_req = NULL;
-			curproxy->options2 &= ~PR_O2_CHK_ANY;
-			curproxy->options2 |= PR_O2_LB_AGENT_CHK;
-		}
 		else if (!strcmp(args[1], "pgsql-check")) {
 			/* use PostgreSQL request to check servers' health */
 			if (warnifnotcap(curproxy, PR_CAP_BE, file, linenum, args[1], NULL))
