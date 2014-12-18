f6b70013389cf9378c6a4d55d3d570de4f95c33c
Willy Tarreau <w@1wt.eu>
2014-12-18 14:00:43 +0100

    BUG/MEDIUM: config: do not propagate processes between stopped processes
    Immo Goltz reported a case of segfault while parsing the config where
    we try to propagate processes across stopped frontends (those with a
    "disabled" statement). The fix is trivial. The workaround consists in
    commenting out these frontends, although not always easy.
    
    This fix must be backported to 1.5.

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -6105,6 +6105,9 @@ void propagate_processes(struct proxy *from, struct proxy *to)
 	if (!(from->cap & PR_CAP_FE))
 		return;
 
+	if (from->state == PR_STSTOPPED)
+		return;
+
 	/* default_backend */
 	if (from->defbe.be)
 		propagate_processes(from, from->defbe.be);
