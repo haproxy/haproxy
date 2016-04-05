haproxy/haproxy

commit 060e57301db98853eef91b344b6ace187b657190

BUG/MINOR: dns: trigger a DNS query type change on resolution timeout
After Cedric Jeanneret reported an issue with HAProxy and DNS resolution
when multiple servers are in use, I saw that the implementation of DNS
query type update on resolution timeout was not implemented, even if it
is documented.

backport: 1.6 and above

src/dns.c
@@ -1164,6 +1164,7 @@ struct task *dns_process_resolve(struct task *t)
 {
 	struct dns_resolvers *resolvers = t->context;
 	struct dns_resolution *resolution, *res_back;
+	int res_preferred_afinet, res_preferred_afinet6;
 
 	/* timeout occurs inevitably for the first element of the FIFO queue */
 	if (LIST_ISEMPTY(&resolvers->curr_resolution)) {
@@ -1192,6 +1193,19 @@ struct task *dns_process_resolve(struct task *t)
 		}
 
 		resolution->try -= 1;
+
+		res_preferred_afinet = resolution->resolver_family_priority == AF_INET && resolution->query_type == DNS_RTYPE_A;
+		res_preferred_afinet6 = resolution->resolver_family_priority == AF_INET6 && resolution->query_type == DNS_RTYPE_AAAA;
+
+		/* let's change the query type if needed */
+		if (res_preferred_afinet6) {
+			/* fallback from AAAA to A */
+			resolution->query_type = DNS_RTYPE_A;
+		}
+		else if (res_preferred_afinet) {
+			/* fallback from A to AAAA */
+			resolution->query_type = DNS_RTYPE_AAAA;
+		}
 
 		/* resend the DNS query */
 		dns_send_query(resolution);

commit 6f79aca33939a75ea68b1d256ba2dc06104b1cd2

BUG/MINOR: DNS: resolution structure change
060e57301db98853eef91b344b6ace187b657190 introduced a bug, related to a
dns option structure change and an improper rebase.

Thanks Lukas Tribus for reporting it.

backport: 1.7 and above

src/dns.c
@@ -1194,8 +1194,8 @@ struct task *dns_process_resolve(struct task *t)
 
 		resolution->try -= 1;
 
-		res_preferred_afinet = resolution->resolver_family_priority == AF_INET && resolution->query_type == DNS_RTYPE_A;
-		res_preferred_afinet6 = resolution->resolver_family_priority == AF_INET6 && resolution->query_type == DNS_RTYPE_AAAA;
+		res_preferred_afinet = resolution->opts->family_prio == AF_INET && resolution->query_type == DNS_RTYPE_A;
+		res_preferred_afinet6 = resolution->opts->family_prio == AF_INET6 && resolution->query_type == DNS_RTYPE_AAAA;
 
 		/* let's change the query type if needed */
 		if (res_preferred_afinet6) {