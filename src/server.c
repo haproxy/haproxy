90b5abe46ea507ae7831221555f5183d16864d4a
Christopher Faulet <cfaulet@haproxy.com>
2016-12-05 14:25:08 +0100

    BUG/MINOR: cli: be sure to always warn the cli applet when input buffer is full
    [wt: may only strike if CLI commands are pipelined. Must be backported to 1.7
     and 1.6, where it's a bit different and in dumpstats.c]

--- src/server.c
+++ src/server.c
@@ -3536,9 +3536,10 @@ static int cli_parse_get_weight(char **args, struct appctx *appctx, void *privat
 
 	/* return server's effective weight at the moment */
 	snprintf(trash.str, trash.size, "%d (initial %d)\n", sv->uweight, sv->iweight);
-	if (bi_putstr(si_ic(si), trash.str) == -1)
+	if (bi_putstr(si_ic(si), trash.str) == -1) {
 		si_applet_cant_put(si);
-
+		return 0;
+	}
 	return 1;
 }
 
