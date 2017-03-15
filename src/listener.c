haproxy/haproxy

commit 1c4b814087189b4b0225a473b7cb0a844bc30839

MEDIUM: listener: support rebinding during resume()
When a listener resumes operations, supporting a full rebind makes it
possible to perform a full stop as a pause(). This will be used for
pausing abstract namespace unix sockets.

src/listener.c
@@ -120,10 +120,26 @@ int pause_listener(struct listener *l)
  * may replace enable_listener(). The resulting state will either be LI_READY
  * or LI_FULL. 0 is returned in case of failure to resume (eg: dead socket).
  * Listeners bound to a different process are not woken up unless we're in
- * foreground mode.
+ * foreground mode. If the listener was only in the assigned state, it's totally
+ * rebound. This can happen if a pause() has completely stopped it. If the
+ * resume fails, 0 is returned and an error might be displayed.
  */
 int resume_listener(struct listener *l)
 {
+	if (l->state == LI_ASSIGNED) {
+		char msg[100];
+		int err;
+
+		err = l->proto->bind(l, msg, sizeof(msg));
+		if (err & ERR_ALERT)
+			Alert("Resuming listener: %s\n", msg);
+		else if (err & ERR_WARN)
+			Warning("Resuming listener: %s\n", msg);
+
+		if (err & (ERR_FATAL | ERR_ABORT))
+			return 0;
+	}
+
 	if (l->state < LI_PAUSED)
 		return 0;
 

commit 3569df3fcf770d6785b32a39c0ff0c3d2a180f44

BUG/MEDIUM: listener: do not try to rebind another process' socket
When the "process" setting of a bind line limits the processes a
listening socket is enabled on, a "disable frontend" operation followed
by an "enable frontend" triggers a bug because all declared listeners
are attempted to be bound again regardless of their assigned processes.
This can at minima create new sockets not receiving traffic, and at worst
prevent from re-enabling a frontend if it's bound to a privileged port.

This bug was introduced by commit 1c4b814 ("MEDIUM: listener: support
rebinding during resume()") merged in 1.6-dev1, trying to perform the
bind() before checking the process list instead of after.

Just move the process check before the bind() operation to fix this.
This fix must be backported to 1.7 and 1.6.

Thanks to Pavlos for reporting this one.

src/listener.c
@@ -128,6 +128,11 @@ int pause_listener(struct listener *l)
  */
 int resume_listener(struct listener *l)
 {
+	if ((global.mode & (MODE_DAEMON | MODE_SYSTEMD)) &&
+	    l->bind_conf->bind_proc &&
+	    !(l->bind_conf->bind_proc & (1UL << (relative_pid - 1))))
+		return 1;
+
 	if (l->state == LI_ASSIGNED) {
 		char msg[100];
 		int err;
@@ -144,11 +149,6 @@ int resume_listener(struct listener *l)
 
 	if (l->state < LI_PAUSED)
 		return 0;
-
-	if ((global.mode & (MODE_DAEMON | MODE_SYSTEMD)) &&
-	    l->bind_conf->bind_proc &&
-	    !(l->bind_conf->bind_proc & (1UL << (relative_pid - 1))))
-		return 1;
 
 	if (l->proto->sock_prot == IPPROTO_TCP &&
 	    l->state == LI_PAUSED &&