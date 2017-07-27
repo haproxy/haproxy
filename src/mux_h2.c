5242ef8095d60cdf2d48b97595aeebf624c253e9
Willy Tarreau <w@1wt.eu>
2017-07-27 11:47:28 +0200

    MINOR: h2: expose tune.h2.max-concurrent-streams to limit the number of streams
    This will be advertised in the settings frame.

--- src/mux_h2.c
+++ src/mux_h2.c
@@ -19,6 +19,7 @@
 /* a few settings from the global section */
 static int h2_settings_header_table_size      =  4096; /* initial value */
 static int h2_settings_initial_window_size    = 65535; /* initial value */
+static int h2_settings_max_concurrent_streams =   100;
 
 
 /*****************************************************************/
@@ -163,6 +164,22 @@ static int h2_parse_initial_window_size(char **args, int section_type, struct pr
 	return 0;
 }
 
+/* config parser for global "tune.h2.max-concurrent-streams" */
+static int h2_parse_max_concurrent_streams(char **args, int section_type, struct proxy *curpx,
+                                           struct proxy *defpx, const char *file, int line,
+                                           char **err)
+{
+	if (too_many_args(1, args, err, NULL))
+		return -1;
+
+	h2_settings_max_concurrent_streams = atoi(args[1]);
+	if (h2_settings_max_concurrent_streams < 0) {
+		memprintf(err, "'%s' expects a positive numeric value.", args[0]);
+		return -1;
+	}
+	return 0;
+}
+
 
 /****************************************/
 /* MUX initialization and instanciation */
@@ -193,6 +210,7 @@ static struct alpn_mux_list alpn_mux_h2 =
 static struct cfg_kw_list cfg_kws = {ILH, {
 	{ CFG_GLOBAL, "tune.h2.header-table-size",      h2_parse_header_table_size      },
 	{ CFG_GLOBAL, "tune.h2.initial-window-size",    h2_parse_initial_window_size    },
+	{ CFG_GLOBAL, "tune.h2.max-concurrent-streams", h2_parse_max_concurrent_streams },
 	{ 0, NULL, NULL }
 }};
 
