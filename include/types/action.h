6d950b92cdf59b5d897b545fab347ec472c39db4
Christopher Faulet <cfaulet@haproxy.com>
2017-09-18 15:12:39 +0200

    MINOR: action: Add a function pointer in act_rule struct to check its validity
    It is possible to define the field "act_rule.check_ptr" if you want to check the
    validity of a tcp/http rule.

--- include/types/action.h
+++ include/types/action.h
@@ -102,6 +102,7 @@ struct act_rule {
 	short deny_status;                     /* HTTP status to return to user when denying */
 	enum act_return (*action_ptr)(struct act_rule *rule, struct proxy *px,  /* ptr to custom action */
 	                              struct session *sess, struct stream *s, int flags);
+	int (*check_ptr)(struct act_rule *rule, struct proxy *px, char **err); /* ptr to check function */
 	struct action_kw *kw;
 	struct applet applet;                  /* used for the applet registration. */
 	union {
