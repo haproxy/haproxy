29023ec5d996cbcc800c7ac39a90ae4d186c66d0
Joseph Herlant <aerostitch@debian.org>
2018-11-15 13:39:46 -0800

    CLEANUP: Fix a typo in the stats subsystem
    Fix a typo in a code comment of the stats subsystem.

--- src/stats.c
+++ src/stats.c
@@ -3300,7 +3300,7 @@ static void http_stats_io_handler(struct appctx *appctx)
 	if (unlikely(si->state == SI_ST_DIS || si->state == SI_ST_CLO))
 		goto out;
 
-	/* Check if the input buffer is avalaible. */
+	/* Check if the input buffer is available. */
 	if (res->buf.size == 0) {
 		/* already subscribed, we'll be called later once the buffer is
 		 * available.
