5662fa4707ea1a37cb3cd8631102270d60b514cf
Joseph Herlant <aerostitch@debian.org>
2018-11-15 13:43:28 -0800

    CLEANUP: Fix typos in the stick_table subsystem
    Fix some typos in the code comments of the stick_table subsystem.

--- src/stick_table.c
+++ src/stick_table.c
@@ -1924,7 +1924,7 @@ smp_fetch_sc_stkctr(struct session *sess, struct stream *strm, const struct arg 
 		if (!conn)
 			return NULL;
 
-		/* Fetch source adress in a sample. */
+		/* Fetch source address in a sample. */
 		smp.px = NULL;
 		smp.sess = sess;
 		smp.strm = strm;
@@ -1986,7 +1986,7 @@ smp_create_src_stkctr(struct session *sess, struct stream *strm, const struct ar
 	if (!conn)
 		return NULL;
 
-	/* Fetch source adress in a sample. */
+	/* Fetch source address in a sample. */
 	smp.px = NULL;
 	smp.sess = sess;
 	smp.strm = strm;
@@ -2521,7 +2521,7 @@ smp_fetch_src_updt_conn_cnt(const struct arg *args, struct sample *smp, const ch
 	if (!conn)
 		return 0;
 
-	/* Fetch source adress in a sample. */
+	/* Fetch source address in a sample. */
 	if (!smp_fetch_src(NULL, smp, NULL, NULL))
 		return 0;
 
