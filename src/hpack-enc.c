haproxy/haproxy

commit 19ed92b47d7cbcdbafa4e18d052b87e04903741c

MINOR: hpack: optimize header encoding for short names
For unknown fields, since we know that most of them are less than 127
characters, we don't need to go through the loop and can instead directly
emit the one-byte length encoding. This increases the request rate by
approximately 0.5%.

src/hpack-enc.c
@@ -114,6 +114,12 @@ int hpack_encode_header(struct buffer *out, const struct ist n,
 		out->area[len++] = 0x58; // literal with indexing -- name="cache-control" (idx 24)
 	else if (isteq(n, ist("content-length")))
 		out->area[len++] = 0x5c; // literal with indexing -- name="content-length" (idx 28)
+	else if (likely(n.len < 127 && len + 1 + n.len <= size)) {
+		out->area[len++] = 0x00;      /* literal without indexing -- new name */
+		out->area[len++] = n.len;     /* single-byte length encoding */
+		ist2bin(out->area + len, n);
+		len += n.len;
+	}
 	else if (len_to_bytes(n.len) && len + 1 + len_to_bytes(n.len) + n.len <= size) {
 		out->area[len++] = 0x00;      /* literal without indexing -- new name */
 		len = hpack_encode_len(out->area, len, n.len);

commit 0dc1b84839ef235a3153d1ef3e729fe1c89f2ea9

BUG/MAJOR: hpack: fix length check for short names encoding
Commit 19ed92b ("MINOR: hpack: optimize header encoding for short names")
introduced an error in the space computation for short names, as it removed
the length encoding from the count without replacing with 1 (the minimum
byte). This results in the last byte of the area being occasionally
overwritten, which is immediately detected with -DDEBUG_MEMORY_POOLS as
the canary at the end gets overwritten.

No backport is needed.

src/hpack-enc.c
@@ -177,7 +177,7 @@ int hpack_encode_header(struct buffer *out, const struct ist n,
 	}
 
  make_literal:
-	if (likely(n.len < 127 && len + 1 + n.len <= size)) {
+	if (likely(n.len < 127 && len + 2 + n.len <= size)) {
 		out->area[len++] = 0x00;      /* literal without indexing -- new name */
 		out->area[len++] = n.len;     /* single-byte length encoding */
 		ist2bin(out->area + len, n);