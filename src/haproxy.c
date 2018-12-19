d22d69bd589508d74485691ee304cd01a8f66465
Willy Tarreau <w@1wt.eu>
2018-12-19 19:07:04 +0100

    CLEANUP: remove my name and address from the copyright banner
    First, it's a pain to always have to think about updating this date,
    second for a long time I've not been the only developer there, and third,
    some users contact me hoping to get help that I can't deliver. It's about
    time to redirect them to the main site where all the useful links should
    be.

--- src/haproxy.c
+++ src/haproxy.c
@@ -370,8 +370,7 @@ void hap_register_per_thread_deinit(void (*fct)())
 
 static void display_version()
 {
-	printf("HA-Proxy version " HAPROXY_VERSION " " HAPROXY_DATE"\n");
-	printf("Copyright 2000-2018 Willy Tarreau <willy@haproxy.org>\n\n");
+	printf("HA-Proxy version " HAPROXY_VERSION " " HAPROXY_DATE" - https://haproxy.org/\n");
 }
 
 static void display_build_opts()
