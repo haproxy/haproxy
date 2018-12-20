645b33d2332d263d00ecf2077c74c988655af0e2
Willy Tarreau <w@1wt.eu>
2018-12-20 15:35:57 +0100

    BUG/MEDIUM: mux-h2: Don't forget to quit the send list on error reports
    Similar to last fix, we need to quit the send list when reporting an
    error via the send side.
    
    This should be backported to 1.9.

--- src/mux_h2.c
+++ src/mux_h2.c
@@ -617,6 +617,8 @@ static void __maybe_unused h2s_notify_send(struct h2s *h2s)
 		sw->events &= ~SUB_RETRY_SEND;
 		tasklet_wakeup(sw->task);
 		h2s->send_wait = NULL;
+		LIST_DEL(&h2s->list);
+		LIST_INIT(&h2s->list);
 	}
 }
 
