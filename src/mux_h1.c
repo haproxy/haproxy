27cd2233a3ba47c012430effbff5470b1545d444
Willy Tarreau <w@1wt.eu>
2019-01-03 21:52:42 +0100

    MINOR: mux-h1: parse the content-length header on output and set H1_MF_CLEN
    The H1_MF_CLEN flag is needed to figure whether a content-length header is
    present or not when producing a request, so let's check it on output just
    like we already check the transfer-encoding header.

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -1538,6 +1538,10 @@ static size_t h1_process_output(struct h1c *h1c, struct buffer *buf, size_t coun
 
 				if (isteqi(n, ist("transfer-encoding")))
 					h1_parse_xfer_enc_header(h1m, v);
+				else if (isteqi(n, ist("content-length"))) {
+					if (h1_parse_cont_len_header(h1m, &v) <= 0)
+						goto skip_hdr;
+				}
 				else if (isteqi(n, ist("connection"))) {
 					h1_parse_connection_header(h1m, v);
 					h1_process_conn_mode(h1s, h1m, NULL, &v);
