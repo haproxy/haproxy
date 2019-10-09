6103836315fd31418e1a09e820dfaf1cdd0abd98
Willy Tarreau <w@1wt.eu>
2019-10-09 07:19:02 +0200

    MINOR: stats: mention in the help message support for "json" and "typed"
    Both "show info" and "show stat" support the "typed" output format and
    the "json" output format. I just never can remind them, which is an
    indication that some help is missing.

--- src/stats.c
+++ src/stats.c
@@ -3863,8 +3863,8 @@ static int cli_io_handler_dump_json_schema(struct appctx *appctx)
 /* register cli keywords */
 static struct cli_kw_list cli_kws = {{ },{
 	{ { "clear", "counters",  NULL }, "clear counters : clear max statistics counters (add 'all' for all counters)", cli_parse_clear_counters, NULL, NULL },
-	{ { "show", "info",  NULL }, "show info      : report information about the running process", cli_parse_show_info, cli_io_handler_dump_info, NULL },
-	{ { "show", "stat",  NULL }, "show stat      : report counters for each proxy and server", cli_parse_show_stat, cli_io_handler_dump_stat, NULL },
+	{ { "show", "info",  NULL }, "show info      : report information about the running process [json|typed]", cli_parse_show_info, cli_io_handler_dump_info, NULL },
+	{ { "show", "stat",  NULL }, "show stat      : report counters for each proxy and server [json|typed]", cli_parse_show_stat, cli_io_handler_dump_stat, NULL },
 	{ { "show", "schema",  "json", NULL }, "show schema json : report schema used for stats", NULL, cli_io_handler_dump_json_schema, NULL },
 	{{},}
 }};
