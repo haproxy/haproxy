haproxy/haproxy

commit c5bb5a0f2b36bffcea807bd7e529abdc35c23067

BUG/MINOR: http-rules: Preserve FLT_END analyzers on reject action
When at least a filter is attached to a stream, FLT_END analyzers must be
preserved on request and response channels.

This patch should be backported as far as 1.8.

src/http_act.c
@@ -369,8 +369,8 @@ static enum act_return http_action_reject(struct act_rule *rule, struct proxy *p
 	si_must_kill_conn(chn_prod(&s->req));
 	channel_abort(&s->req);
 	channel_abort(&s->res);
-	s->req.analysers = 0;
-	s->res.analysers = 0;
+	s->req.analysers &= AN_RES_FLT_END;
+	s->res.analysers &= AN_REQ_FLT_END;
 
 	_HA_ATOMIC_ADD(&s->be->be_counters.denied_req, 1);
 	_HA_ATOMIC_ADD(&sess->fe->fe_counters.denied_req, 1);

commit d4a824e533ef940e9726dbf3d3f94993d97d6391

BUG/MINOR: http-rules: Fix a typo in the reject action function
A typo was introduced by the commit c5bb5a0f2 ("BUG/MINOR: http-rules: Preserve
FLT_END analyzers on reject action").

This patch must be backported with the commit c5bb5a0f2.

src/http_act.c
@@ -369,8 +369,8 @@ static enum act_return http_action_reject(struct act_rule *rule, struct proxy *p
 	si_must_kill_conn(chn_prod(&s->req));
 	channel_abort(&s->req);
 	channel_abort(&s->res);
-	s->req.analysers &= AN_RES_FLT_END;
-	s->res.analysers &= AN_REQ_FLT_END;
+	s->req.analysers &= AN_REQ_FLT_END;
+	s->res.analysers &= AN_RES_FLT_END;
 
 	_HA_ATOMIC_ADD(&s->be->be_counters.denied_req, 1);
 	_HA_ATOMIC_ADD(&sess->fe->fe_counters.denied_req, 1);