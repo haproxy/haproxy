a142c1deb401308055f29b9d52a47e5a0ad962e7
Christopher Faulet <cfaulet@haproxy.com>
2020-04-21 11:48:53 +0200

    BUG/MINOR: obj_type: Handle stream object in obj_base_ptr() function
    The stream object (OBJ_TYPE_STREAM) was missing in the switch statement of the
    obj_base_ptr() function.
    
    This patch must be backported as far as 2.0.

--- include/proto/obj_type.h
+++ include/proto/obj_type.h
@@ -198,6 +198,7 @@ static inline void *obj_base_ptr(enum obj_type *t)
 	case OBJ_TYPE_CONN:     return __objt_conn(t);
 	case OBJ_TYPE_SRVRQ:    return __objt_dns_srvrq(t);
 	case OBJ_TYPE_CS:       return __objt_cs(t);
+	case OBJ_TYPE_STREAM:   return __objt_stream(t);
 	case OBJ_TYPE_CHECK:    return __objt_check(t);
 	default:                return t; // exact pointer for invalid case
 	}
