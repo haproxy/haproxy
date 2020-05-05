5b49ee9ca9163003972c04f882234d8347dc9a48
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2020-05-05 17:26:05 +0300

    Extract the Shell script into a file
    This way it is faster to build and run the Docker image locally

---
+++ .github/workflows/aarch64/build-and-test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -eox
+
+export TARGET="linux-glibc"
+export FLAGS="USE_ZLIB=1 USE_PCRE=1 USE_PCRE_JIT=1 USE_LUA=1 USE_OPENSSL=0 USE_SYSTEMD=1 USE_WURFL=1 WURFL_INC=contrib/wurfl WURFL_LIB=contrib/wurfl USE_DEVICEATLAS=1 DEVICEATLAS_SRC=contrib/deviceatlas USE_51DEGREES=1"
+export SSL_LIB=${HOME}/opt/lib
+export SSL_INC=${HOME}/opt/include
+export TMPDIR=/tmp
+export FIFTYONEDEGREES_SRC="contrib/51d/src/pattern"
+export DEBUG_OPTIONS="DEBUG_STRICT=1"
+
+
+pushd /haproxy
+echo "Target: $TARGET"
+make clean 
+make -C ./vtest FLAGS="-O2 -s -Wall"
+bash ./scripts/build-ssl.sh
+make -C contrib/wurfl
+make -j3 CC=clang-9 ERR=1 DEBUG_STRICT=1 V=0 TARGET=$TARGET $FLAGS DEBUG_CFLAGS="$DEBUG_CFLAGS" LDFLAGS="$LDFLAGS -L$SSL_LIB -Wl,-rpath,$SSL_LIB" 51DEGREES_SRC="$FIFTYONEDEGREES_SRC" EXTRA_OBJS="$EXTRA_OBJS" LUA_INC=/usr/include/lua5.3/
+
+./haproxy -vv
+ldd haproxy
+
+VTEST_PROGRAM=./vtest/vtest REGTESTS_TYPES=default,bug,devel,slow make reg-tests
+
+popd
