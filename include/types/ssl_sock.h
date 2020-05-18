haproxy/haproxy

commit c0cdaffaa33854a040d17080eb13ddd6583cb486

REORG: ssl: move ssl_sock_ctx and fix cross-dependencies issues
In order to move all SSL sample fetches in another file, moving the
ssl_sock_ctx definition in a .h file is required.

Unfortunately it became a cross dependencies hell to solve, because of
the struct wait_event field, so <types/connection.h> is needed which
created other problems.

include/types/ssl_sock.h
@@ -27,6 +27,7 @@
 #include <ebmbtree.h>
 #include <eb64tree.h>
 
+#include <types/connection.h> /* struct wait_event */
 #include <types/ssl_ckch.h>
 #include <types/ssl_crtlist.h>
 
@@ -131,12 +132,6 @@ struct sni_ctx {
 	struct ebmb_node name;    /* node holding the servername value */
 };
 
-struct tls_version_filter {
-	uint16_t flags;     /* ssl options */
-	uint8_t  min;      /* min TLS version */
-	uint8_t  max;      /* max TLS version */
-};
-
 extern struct list tlskeys_reference;
 
 struct tls_sess_key_128 {
@@ -231,6 +226,20 @@ struct ssl_capture {
 	unsigned long long int xxh64;
 	unsigned char ciphersuite_len;
 	char ciphersuite[0];
+};
+
+struct ssl_sock_ctx {
+	struct connection *conn;
+	SSL *ssl;
+	BIO *bio;
+	const struct xprt_ops *xprt;
+	void *xprt_ctx;
+	struct wait_event wait_event;
+	struct wait_event *subs;
+	int xprt_st;                  /* transport layer state, initialized to zero */
+	struct buffer early_buf;      /* buffer to store the early data received */
+	int sent_early_data;          /* Amount of early data we sent so far */
+
 };
 
 struct global_ssl {

include/proto/ssl_sock.h
@@ -28,6 +28,7 @@
 #include <types/connection.h>
 #include <types/listener.h>
 #include <types/proxy.h>
+#include <types/ssl_sock.h>
 #include <types/stream_interface.h>
 
 #include <proto/connection.h>

include/types/listener.h
@@ -27,7 +27,6 @@
 
 #ifdef USE_OPENSSL
 #include <common/openssl-compat.h>
-#include <types/ssl_sock.h>
 #endif
 
 #include <common/config.h>
@@ -111,6 +110,12 @@ enum li_state {
 #define BC_SSL_O_NO_TLS_TICKETS 0x0100	/* disable session resumption tickets */
 #define BC_SSL_O_PREF_CLIE_CIPH 0x0200  /* prefer client ciphers */
 #endif
+
+struct tls_version_filter {
+	uint16_t flags;     /* ssl options */
+	uint8_t  min;      /* min TLS version */
+	uint8_t  max;      /* max TLS version */
+};
 
 /* ssl "bind" settings */
 struct ssl_bind_conf {

src/ssl_sock.c
@@ -143,20 +143,6 @@ struct global_ssl global_ssl = {
 
 static BIO_METHOD *ha_meth;
 
-struct ssl_sock_ctx {
-	struct connection *conn;
-	SSL *ssl;
-	BIO *bio;
-	const struct xprt_ops *xprt;
-	void *xprt_ctx;
-	struct wait_event wait_event;
-	struct wait_event *subs;
-	int xprt_st;                  /* transport layer state, initialized to zero */
-	struct buffer early_buf;      /* buffer to store the early data received */
-	int sent_early_data;          /* Amount of early data we sent so far */
-
-};
-
 DECLARE_STATIC_POOL(ssl_sock_ctx_pool, "ssl_sock_ctx_pool", sizeof(struct ssl_sock_ctx));
 
 static struct task *ssl_sock_io_cb(struct task *, void *, unsigned short);

commit 1665c43fd86fa58ea06907b84a54ecc54d2782dd

BUILD: ssl: include buffer common headers for ssl_sock_ctx
since commit c0cdaffaa338 ("REORG: ssl: move ssl_sock_ctx and fix
cross-dependencies issues"), `struct ssl_sock_ctx` was moved in
ssl_sock.h. As it contains a `struct buffer`, including
`common/buffer.h` is now mandatory. I encountered an issue while
including ssl_sock.h on another patch:

include/types/ssl_sock.h:240:16: error: field ‘early_buf’ has incomplete type
  240 |  struct buffer early_buf;      /* buffer to store the early data received */

no backport needed.

Fixes: c0cdaffaa338 ("REORG: ssl: move ssl_sock_ctx and fix
cross-dependencies issues")
Signed-off-by: William Dauchy <w.dauchy@criteo.com>

include/types/ssl_sock.h
@@ -31,6 +31,7 @@
 #include <types/ssl_ckch.h>
 #include <types/ssl_crtlist.h>
 
+#include <common/buffer.h>
 #include <common/hathreads.h>
 #include <common/mini-clist.h>
 #include <common/openssl-compat.h>