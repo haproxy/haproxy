7bf484ac91b50bb77dbd901a4df692163287d31f
Willy Tarreau <w@1wt.eu>
2020-06-16 19:14:19 +0200

    MINOR: version: report the presence of the compiler's address sanitizer
    Since we've seen clang emit bad code when the address sanitizer is enabled
    at -O2, better clearly report it in the version output. It is detected both
    for clang and gcc (both tested with and without).

--- src/version.c
+++ src/version.c
@@ -14,8 +14,14 @@ char haproxy_version[]      = HAPROXY_VERSION;
 char haproxy_date[]         = HAPROXY_DATE;
 char stats_version_string[] = STATS_VERSION_STRING;
 
+#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+#define SANITIZE_STRING " with address sanitizer"
+#else
+#define SANITIZE_STRING ""
+#endif
+
 #if defined(__clang_version__)
-REGISTER_BUILD_OPTS("Built with clang compiler version " __clang_version__);
+REGISTER_BUILD_OPTS("Built with clang compiler version " __clang_version__ "" SANITIZE_STRING);
 #elif defined(__VERSION__)
-REGISTER_BUILD_OPTS("Built with gcc compiler version " __VERSION__);
+REGISTER_BUILD_OPTS("Built with gcc compiler version " __VERSION__ "" SANITIZE_STRING);
 #endif
