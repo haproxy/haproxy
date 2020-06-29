haproxy/haproxy

commit d645574fd49d19da9513d19f78648de2c9bc670a

MINOR: soft-stop: let the first stopper only signal other threads
When the first thread stops and wakes others up, it's possible some of
them will also start to wake others in parallel. Let's make give this
notification task to the very first one instead since it's enough and
can reduce the amount of needless (though harmless) wakeup calls.

src/haproxy.c
@@ -2835,11 +2835,12 @@ void run_poll_loop()
 			int i;
 
 			if (stopping) {
-				_HA_ATOMIC_OR(&stopping_thread_mask, tid_bit);
-				/* notify all threads that stopping was just set */
-				for (i = 0; i < global.nbthread; i++)
-					if (((all_threads_mask & ~stopping_thread_mask) >> i) & 1)
-						wake_thread(i);
+				if (_HA_ATOMIC_OR(&stopping_thread_mask, tid_bit) == tid_bit) {
+					/* notify all threads that stopping was just set */
+					for (i = 0; i < global.nbthread; i++)
+						if ((all_threads_mask >> i) & 1)
+							wake_thread(i);
+				}
 			}
 
 			/* stop when there's nothing left to do */

commit 369a2efc27ee785a831aff23db8eb9b0d70c2e9d

BUG/MINOR: haproxy: don't wake already stopping threads on exit
Commit d645574 ("MINOR: soft-stop: let the first stopper only signal
other threads") introduced a minor mistake which is that when a stopping
thread signals all other threads, it also signals itself. When
single-threaded, the process constantly wakes up while waiting for
last connections to exit. Let's reintroduce the lost mask to avoid
this.

No backport is needed, this is 2.2-dev only.

src/haproxy.c
@@ -2926,7 +2926,7 @@ void run_poll_loop()
 				if (_HA_ATOMIC_OR(&stopping_thread_mask, tid_bit) == tid_bit) {
 					/* notify all threads that stopping was just set */
 					for (i = 0; i < global.nbthread; i++)
-						if ((all_threads_mask >> i) & 1)
+						if (((all_threads_mask & ~stopping_thread_mask) >> i) & 1)
 							wake_thread(i);
 				}
 			}