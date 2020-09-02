haproxy/haproxy

commit 77b98220e81207aad854e480e6207d501d476666

BUG/MINOR: threads: work around a libgcc_s issue with chrooting
Sander Hoentjen reported another issue related to libgcc_s in issue #671.
What happens is that when the old process quits, pthread_exit() calls
something from libgcc_s.so after the process was chrooted, and this is
the first call to that library, causing an attempt to load it. In a
chroot, this fails, thus libthread aborts. The behavior widely differs
between operating systems because some decided to use a static build for
this library.

In 2.2 this was resolved as a side effect of a workaround for the same issue
with the backtrace() call, which is also in libgcc_s. This was in commit
0214b45 ("MINOR: debug: call backtrace() once upon startup"). But backtraces
are not necessarily enabled, and we need something for older versions.

By inspecting a significant number of ligcc_s on various gcc versions and
platforms, it appears that a few functions have been present since gcc 3.0,
one of which, _Unwind_Find_FDE() has no side effect (it only returns a
pointer). What this patch does is that in the thread initialization code,
if built with gcc >= 3.0, a call to this function is made in order to make
sure that libgcc_s is loaded at start up time and that there will be no
need to load it upon exit.

An easy way to check which libs are loaded under Linux is :

  $ strace -e trace=openat ./haproxy -v

With this patch applied, libgcc_s now appears during init.

Sander confirmed that this patch was enough to put an end to the core
dumps on exit in 2.0, so this patch should be backported there, and maybe
as far as 1.8.

src/thread.c
@@ -201,6 +201,18 @@ static void __thread_init(void)
 		exit(1);
 	}
 
+#if defined(__GNUC__) && (__GNUC__ >= 3) && !defined(__clang__)
+	/* make sure libgcc_s is already loaded, because pthread_exit() may
+	 * may need it on exit after the chroot! _Unwind_Find_FDE() is defined
+	 * there since gcc 3.0, has no side effect, doesn't take any argument
+	 * and seems to be present on all supported platforms.
+	 */
+	{
+		extern void _Unwind_Find_FDE(void);
+		_Unwind_Find_FDE();
+	}
+#endif
+
 	thread_cpus_enabled_at_boot = thread_cpus_enabled();
 
 	memprintf(&ptr, "Built with multi-threading support (MAX_THREADS=%d, default=%d).",

commit 06a1806083dd9f0d21e045baad4c61559211f956

BUILD: thread: limit the libgcc_s workaround to glibc only
Previous commit 77b98220e ("BUG/MINOR: threads: work around a libgcc_s
issue with chrooting") broke the build on cygwin. I didn't even know we
supported threads on cygwin. But the point is that it's actually the
glibc-based libpthread which requires libgcc_s, so in absence of other
reports we should not apply the workaround on other libraries.

This should be backported along with the aforementioned patch.

src/thread.c
@@ -201,7 +201,7 @@ static void __thread_init(void)
 		exit(1);
 	}
 
-#if defined(__GNUC__) && (__GNUC__ >= 3) && !defined(__clang__)
+#if defined(__GNUC__) && (__GNUC__ >= 3) && defined(__GNU_LIBRARY__) && !defined(__clang__)
 	/* make sure libgcc_s is already loaded, because pthread_exit() may
 	 * may need it on exit after the chroot! _Unwind_Find_FDE() is defined
 	 * there since gcc 3.0, has no side effect, doesn't take any argument