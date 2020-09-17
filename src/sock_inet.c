haproxy/haproxy

commit 36722d2274127ef91959296bb5299c8120b16ce7

MINOR: sock_inet: report the errno string in binding errors
With the socket binding code cleanup it becomes easy to add more info to
error messages. One missing thing used to be the error string, which is
now added after the generic one, for example:

  [ALERT] 260/082852 (12974) : Starting frontend f: cannot bind socket (Permission denied) [0.0.0.0:4]
  [ALERT] 260/083053 (13292) : Starting frontend f: cannot bind socket (Address already in use) [0.0.0.0:4444]
  [ALERT] 260/083104 (13298) : Starting frontend f: cannot bind socket (Cannot assign requested address) [1.1.1.1:4444]

src/sock_inet.c
@@ -293,7 +293,7 @@ int sock_inet_bind_receiver(struct receiver *rx, void (*handler)(int fd), char *
 		                 rx->proto->sock_type, rx->proto->sock_prot);
 		if (fd == -1) {
 			err |= ERR_RETRYABLE | ERR_ALERT;
-			memprintf(errmsg, "cannot create receiving socket");
+			memprintf(errmsg, "cannot create receiving socket (%s)", strerror(errno));
 			goto bind_return;
 		}
 	}
@@ -347,7 +347,7 @@ int sock_inet_bind_receiver(struct receiver *rx, void (*handler)(int fd), char *
 		if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE,
 		               rx->settings->interface,
 		               strlen(rx->settings->interface) + 1) == -1) {
-			memprintf(errmsg, "cannot bind receiver to device");
+			memprintf(errmsg, "cannot bind receiver to device (%s)", strerror(errno));
 			err |= ERR_WARN;
 		}
 	}
@@ -369,7 +369,7 @@ int sock_inet_bind_receiver(struct receiver *rx, void (*handler)(int fd), char *
 
 	if (!ext && bind(fd, (struct sockaddr *)&addr_inet, rx->proto->fam->sock_addrlen) == -1) {
 		err |= ERR_RETRYABLE | ERR_ALERT;
-		memprintf(errmsg, "cannot bind socket");
+		memprintf(errmsg, "cannot bind socket (%s)", strerror(errno));
 		goto bind_close_return;
 	}
 

commit f2cda10b1d8db89669fd8b61d6e41ad91a61126e

BUILD: sock_inet: include errno.h
I was careful to have it for sock_unix.c but missed it for sock_inet
which broke with commit 36722d227 ("MINOR: sock_inet: report the errno
string in binding errors") depending on the build options. No backport
is needed.

src/sock_inet.c
@@ -10,6 +10,7 @@
  *
  */
 
+#include <errno.h>
 #include <fcntl.h>
 #include <string.h>
 #include <unistd.h>