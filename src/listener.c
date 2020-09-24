35b44675dcb9f1da56f932352f0bcf4302bc5a81
Willy Tarreau <w@1wt.eu>
2020-09-24 14:46:34 +0200

    MINOR: listeners: check the current listener state in pause_listener()
    It's better not to try to perform pause() actions on wrong states, so
    let's check this and make sure that all callers are now safe.

--- src/listener.c
+++ src/listener.c
@@ -346,6 +346,9 @@ int pause_listener(struct listener *l)
 	    !(proc_mask(l->rx.settings->bind_proc) & pid_bit))
 		goto end;
 
+	if (l->state < LI_READY)
+		goto end;
+
 	if (l->rx.proto->pause) {
 		/* Returns < 0 in case of failure, 0 if the listener
 		 * was totally stopped, or > 0 if correctly paused.
