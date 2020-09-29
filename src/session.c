72d1c359a9174628d92d9c5073c607d959de7c66
alexzk1 <alexzkhr@gmail.com>
2020-09-29 17:17:22 +0300

    fixed access to non-existing address

--- src/session.c
+++ src/session.c
@@ -156,18 +156,18 @@ int session_accept_fd(struct listener *l, int cfd, struct sockaddr_storage *addr
 		goto out_free_conn;
 
 	cli_conn->handle.fd = cfd;
-	*cli_conn->src = *addr;
+	if (p && p->srv && (p->srv->flags & SRV_F_SOCKS4_PROXY) && p->srv->hostname)
+	{
+		DPRINTF(stderr, "Setting proxy to resolve %s\n", p->srv->hostname);
+		conn_set_domain(cli_conn, p->srv->hostname);
+	}
+	else
+		*cli_conn->src = *addr;
 	cli_conn->flags |= CO_FL_ADDR_FROM_SET;
 	cli_conn->proxy_netns = l->rx.settings->netns;
 
 	conn_prepare(cli_conn, l->rx.proto, l->bind_conf->xprt);
 	conn_ctrl_init(cli_conn);
-
-	if ((p->srv->flags & SRV_F_SOCKS4_PROXY) && p->srv->hostname)
-	{
-		DPRINTF(stderr, "Setting proxy to resolve %s\n", p->srv->hostname);
-		conn_set_domain(cli_conn, p->srv->hostname);
-	}
 
 	/* wait for a PROXY protocol header */
 	if (l->options & LI_O_ACC_PROXY)
