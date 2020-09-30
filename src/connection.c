1eb5c041dc19ef00b4c6d5aea5309a71f7a5a577
alexzk1 <alexzkhr@gmail.com>
2020-09-30 08:30:05 +0300

    updated debug text

--- src/connection.c
+++ src/connection.c
@@ -1163,7 +1163,7 @@ int conn_send_socks4_proxy_request(struct connection *conn)
 
 	if (proxy_resolve)
 	{
-		DPRINTF(stderr, "SOCKS PROXY HS FD[%04X]: is_inet_addr(conn->dst) returned 0. Trying SOCKS4A\nFor domain: %s\n",
+		DPRINTF(stderr, "SOCKS PROXY HS FD[%04X]: Have conn->requested_domain set (%s). Trying SOCKS4A\n",
 				conn->handle.fd, conn->requested_domain);
 		req_line.ip = htonl(0x00000001u);
 	}
