haproxy/haproxy

commit 46a030cdda7c96f0b10c43b0e585eed558d325d4

CLEANUP: assorted typo fixes in the code and comments
This is 11th iteration of typo fixes

src/http.c
@@ -288,7 +288,7 @@ const char *http_err_msgs[HTTP_ERR_SIZE] = {
 	"Cache-Control: no-cache\r\n"
 	"Content-Type: text/html\r\n"
 	"\r\n"
-	"<html><body><h1>500 Internal Server Error</h1>\nAn internal server error occured.\n</body></html>\n",
+	"<html><body><h1>500 Internal Server Error</h1>\nAn internal server error occurred.\n</body></html>\n",
 
 	[HTTP_ERR_502] =
 	"HTTP/1.1 502 Bad Gateway\r\n"
@@ -439,7 +439,7 @@ const char *http_get_reason(unsigned int status)
 	case 503: return "Service Unavailable";
 	case 504: return "Gateway Time-out";
 	case 505: return "HTTP Version not supported";
-	case 506: return "Variant also negociate";
+	case 506: return "Variant also negotiate";
 	case 507: return "Insufficient storage";
 	case 508: return "Loop detected";
 	case 509: return "Bandwidth Limit Exceeded";
@@ -691,7 +691,7 @@ char *http_extract_cookie_value(char *hdr, const char *hdr_end,
 			att_end = equal;
 		}
 
-		/* here, <equal> points to '=', a delimitor or the end. <att_end>
+		/* here, <equal> points to '=', a delimiter or the end. <att_end>
 		 * is between <att_beg> and <equal>, both may be identical.
 		 */
 
@@ -705,7 +705,7 @@ char *http_extract_cookie_value(char *hdr, const char *hdr_end,
 			/* find the end of the value, respecting quotes */
 			next = http_find_cookie_value_end(val_beg, hdr_end);
 
-			/* make val_end point to the first white space or delimitor after the value */
+			/* make val_end point to the first white space or delimiter after the value */
 			val_end = next;
 			while (val_end > val_beg && HTTP_IS_SPHT(*(val_end - 1)))
 				val_end--;
@@ -1010,7 +1010,7 @@ int http_parse_stline(const struct ist line, struct ist *p1, struct ist *p2, str
         /* Skip spaces between p2 and p3 */
         for (; p < end && HTTP_IS_SPHT(*p); p++);
 
-        /* The remaing is the third value */
+        /* The remaining is the third value */
         p3->ptr = p;
         p3->len = end - p;
 

include/haproxy/server-t.h
@@ -289,12 +289,12 @@ struct server {
 	struct dns_resolvers *resolvers;	/* pointer to the resolvers structure used by this server */
 	char *lastaddr;				/* the address string provided by the server-state file */
 	struct dns_options dns_opts;
-	int hostname_dn_len;			/* sting length of the server hostname in Domain Name format */
+	int hostname_dn_len;			/* string length of the server hostname in Domain Name format */
 	char *hostname_dn;			/* server hostname in Domain Name format */
 	char *hostname;				/* server hostname */
 	struct sockaddr_storage init_addr;	/* plain IP address specified on the init-addr line */
 	unsigned int init_addr_methods;		/* initial address setting, 3-bit per method, ends at 0, enough to store 10 entries */
-	enum srv_log_proto log_proto;		/* used proto to emmit messages on server lines from ring section */
+	enum srv_log_proto log_proto;		/* used proto to emit messages on server lines from ring section */
 
 #ifdef USE_OPENSSL
 	char *sni_expr;             /* Temporary variable to store a sample expression for SNI */

src/arg.c
@@ -362,7 +362,7 @@ int make_arg_list(const char *in, int len, uint64_t mask, struct arg **argp,
 		memprintf(err_msg, "expected ')' before '%s'", word);
 		if (len > 0)
 			free(word);
-		/* when we're missing a right paren, the empty part preceeding
+		/* when we're missing a right paren, the empty part preceding
 		 * already created an empty arg, adding one to the position, so
 		 * let's fix the reporting to avoid being confusing.
 		 */

src/dns.c
@@ -1273,7 +1273,7 @@ int dns_get_ip_from_response(struct dns_response_packet *dns_p,
 		/* Check for preferred network. */
 		for (j = 0; j < dns_opts->pref_net_nb; j++) {
 
-			/* Compare only the same adresses class. */
+			/* Compare only the same addresses class. */
 			if (dns_opts->pref_net[j].family != ip_type)
 				continue;
 
@@ -1698,7 +1698,7 @@ int dns_link_resolution(void *requester, int requester_type, int requester_locke
 	return -1;
 }
 
-/* Removes a requester from a DNS resoltion. It takes takes care of all the
+/* Removes a requester from a DNS resolution. It takes takes care of all the
  * consequences. It also cleans up some parameters from the requester.
  */
 void dns_unlink_resolution(struct dns_requester *requester)

src/fcgi-app.c
@@ -702,7 +702,7 @@ static int cfg_fcgi_apps_postparser()
 				struct sink *sink = sink_find(logsrv->ring_name);
 
 				if (!sink || sink->type != SINK_TYPE_BUFFER) {
-					ha_alert("config : fcgi-app '%s' : log server uses unkown ring named '%s'.\n",
+					ha_alert("config : fcgi-app '%s' : log server uses unknown ring named '%s'.\n",
 						 curapp->name, logsrv->ring_name);
 					err_code |= ERR_ALERT | ERR_FATAL;
 				}

src/flt_http_comp.c
@@ -458,7 +458,7 @@ select_compression_request_header(struct comp_state *st, struct stream *s, struc
 }
 
 /*
- * Selects a comression algorithm depending of the server response.
+ * Selects a compression algorithm depending of the server response.
  */
 static int
 select_compression_response_header(struct comp_state *st, struct stream *s, struct http_msg *msg)

src/haproxy.c
@@ -7,8 +7,8 @@
  * as published by the Free Software Foundation; either version
  * 2 of the License, or (at your option) any later version.
  *
- * Please refer to RFC7230 - RFC7235 informations about HTTP protocol, and
- * RFC6265 for informations about cookies usage. More generally, the IETF HTTP
+ * Please refer to RFC7230 - RFC7235 information about HTTP protocol, and
+ * RFC6265 for information about cookies usage. More generally, the IETF HTTP
  * Working Group's web site should be consulted for protocol related changes :
  *
  *     http://ftp.ics.uci.edu/pub/ietf/http/
@@ -194,7 +194,7 @@ int unstoppable_jobs = 0;  /* number of active jobs that can't be stopped during
 int active_peers = 0; /* number of active peers (connection attempts and connected) */
 int connected_peers = 0; /* number of connected peers (verified ones) */
 
-/* Here we store informations about the pids of the processes we may pause
+/* Here we store information about the pids of the processes we may pause
  * or kill. We will send them a signal every 10 ms until we can bind to all
  * our ports. With 200 retries, that's about 2 seconds.
  */
@@ -1215,7 +1215,7 @@ static int get_old_sockets(const char *unixsocket)
 				}
 				/*
 				 * Be paranoid and use memcpy() to avoid any
-				 * potential alignement issue.
+				 * potential alignment issue.
 				 */
 				memcpy(&tmpfd[got_fd], CMSG_DATA(cmsg), totlen);
 				got_fd += totlen / sizeof(int);

src/hlua.c
@@ -94,7 +94,7 @@
  *  - The converters wrapper function
  *  - The sample-fetch wrapper functions
  *
- * It is tolerated that the initilisation function returns an abort.
+ * It is tolerated that the initialisation function returns an abort.
  * Before each Lua abort, an error message is written on stderr.
  *
  * The macro SET_SAFE_LJMP initialise the longjmp. The Macro
@@ -994,7 +994,7 @@ static int hlua_ctx_renew(struct hlua *lua, int keep_msg)
 	/* Copy data between the coroutines. */
 	lua_rawgeti(lua->T, LUA_REGISTRYINDEX, lua->Mref);
 	lua_xmove(lua->T, T, 1);
-	new_ref = luaL_ref(T, LUA_REGISTRYINDEX); /* Valur poped. */
+	new_ref = luaL_ref(T, LUA_REGISTRYINDEX); /* Value popped. */
 
 	/* Destroy old data. */
 	luaL_unref(lua->T, LUA_REGISTRYINDEX, lua->Mref);

commit 55633925545717466597d8863cee9c137082eade

BUG/MINOR: http: Fix content-length of the default 500 error
96 bytes is announce in the C-L header for a message of body of 97 bytes. This
bug was introduced by the patch 46a030cdd ("CLEANUP: assorted typo fixes in the
code and comments").

This patch must be backported in all versions where the patch above is (the 2.2
for now).

src/http.c
@@ -284,7 +284,7 @@ const char *http_err_msgs[HTTP_ERR_SIZE] = {
 
 	[HTTP_ERR_500] =
 	"HTTP/1.1 500 Internal Server Error\r\n"
-	"Content-length: 96\r\n"
+	"Content-length: 97\r\n"
 	"Cache-Control: no-cache\r\n"
 	"Content-Type: text/html\r\n"
 	"\r\n"