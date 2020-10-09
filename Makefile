haproxy/haproxy

commit 382001b46bf9b20b6357c68a88b0866b73ac8dc3

BUILD: Add a DragonFlyBSD target
Add a target for DragonFlyBSD 4.3 and above.

Makefile
@@ -142,8 +142,9 @@ DOCDIR = $(PREFIX)/doc/haproxy
 #### TARGET system
 # Use TARGET=<target_name> to optimize for a specific target OS among the
 # following list (use the default "generic" if uncertain) :
-#    linux-glibc, linux-glibc-legacy, linux-musl, solaris, freebsd, openbsd,
-#    netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic, custom
+#    linux-glibc, linux-glibc-legacy, linux-musl, solaris, freebsd, dragonfly,
+#    openbsd, netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic,
+#    custom
 TARGET =
 
 #### TARGET CPU
@@ -366,6 +367,13 @@ endif
 
 # FreeBSD 10 and above
 ifeq ($(TARGET),freebsd)
+  set_target_defaults = $(call default_opts, \
+    USE_POLL USE_TPROXY USE_LIBCRYPT USE_THREAD USE_CPU_AFFINITY USE_KQUEUE   \
+    USE_ACCEPT4 USE_CLOSEFROM USE_GETADDRINFO)
+endif
+
+# DragonFlyBSD 4.3 and above
+ifeq ($(TARGET),dragonfly)
   set_target_defaults = $(call default_opts, \
     USE_POLL USE_TPROXY USE_LIBCRYPT USE_THREAD USE_CPU_AFFINITY USE_KQUEUE   \
     USE_ACCEPT4 USE_CLOSEFROM USE_GETADDRINFO)
@@ -773,8 +781,9 @@ all:
 	@echo
 	@echo "Please choose the target among the following supported list :"
 	@echo
-	@echo "   linux-glibc, linux-glibc-legacy, linux-musl, solaris, freebsd, openbsd, "
-	@echo "   netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic, custom"
+	@echo "   linux-glibc, linux-glibc-legacy, linux-musl, solaris, freebsd, dragonfly, "
+	@echo "   iopenbs, netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic, "
+	@echo "   custom"
 	@echo
 	@echo "Use \"generic\" if you don't want any optimization, \"custom\" if you"
 	@echo "want to precisely tweak every option, or choose the target which"
@@ -850,8 +859,9 @@ help:
 	     fi; \
 	   else \
 	     echo "TARGET not set, you may pass 'TARGET=xxx' to set one among :";\
-	     echo "  linux-glibc, linux-glibc-legacy, solaris, freebsd, netbsd, osx,"; \
-	     echo "  openbsd, aix51, aix52, aix72-gcc, cygwin, haiku, generic, custom"; \
+	     echo "  linux-glibc, linux-glibc-legacy, solaris, freebsd, dragonfly, netbsd,"; \
+	     echo "  osx, openbsd, aix51, aix52, aix72-gcc, cygwin, haiku, generic,"; \
+	     echo "  custom"; \
 	   fi
 	$(Q)echo;echo "Enabled features for TARGET '$(TARGET)' (disable with 'USE_xxx=') :"
 	$(Q)set -- $(foreach opt,$(patsubst USE_%,%,$(use_opts)),$(if $(USE_$(opt)),$(opt),)); echo "  $$*" | (fmt || cat) 2>/dev/null

INSTALL
@@ -376,6 +376,7 @@ and assign it to the TARGET variable :
   - linux-musl          for Linux kernel 2.6.28 and above with musl libc
   - solaris             for Solaris 10 and above
   - freebsd             for FreeBSD 10 and above
+  - dragonfly           for DragonFlyBSD 4.3 and above
   - netbsd              for NetBSD
   - osx                 for Mac OS/X
   - openbsd             for OpenBSD 6.3 and above

commit 4a539f343ac34d0efb2f77739913bf3d98391b7e

BUG/MINOR: makefile: fix a tiny typo in the target list
Previous commit 382001b46 ("BUILD: Add a DragonFlyBSD target") introduced
a tiny typo in the target list ("iopenbs" vs "openbsd"). This will have to
be backported if that patch is backported.

Makefile
@@ -782,7 +782,7 @@ all:
 	@echo "Please choose the target among the following supported list :"
 	@echo
 	@echo "   linux-glibc, linux-glibc-legacy, linux-musl, solaris, freebsd, dragonfly, "
-	@echo "   iopenbs, netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic, "
+	@echo "   openbsd, netbsd, cygwin, haiku, aix51, aix52, aix72-gcc, osx, generic, "
 	@echo "   custom"
 	@echo
 	@echo "Use \"generic\" if you don't want any optimization, \"custom\" if you"