haproxy/haproxy

commit 04a24c5eaa4368b2ff7cd67d2b9e310a3995e1b3

MINOR: connection: don't check priv flag on free
Do not check CO_FL_PRIVATE flag to check if the connection is in session
list on conn_free. This is necessary due to the future patches which add
server connections in the session list even if not private, if the mux
protocol is the subject of HOL blocking.

include/haproxy/connection.h
@@ -464,12 +464,10 @@ static inline void conn_force_unsubscribe(struct connection *conn)
 /* Releases a connection previously allocated by conn_new() */
 static inline void conn_free(struct connection *conn)
 {
-	if (conn->flags & CO_FL_PRIVATE) {
-		/* The connection is private, so remove it from the session's
-		 * connections list, if any.
-		 */
-		if (LIST_ADDED(&conn->session_list))
-			session_unown_conn(conn->owner, conn);
+	/* If the connection is owned by the session, remove it from its list
+	 */
+	if (LIST_ADDED(&conn->session_list)) {
+		session_unown_conn(conn->owner, conn);
 	}
 	else {
 		if (obj_type(conn->target) == OBJ_TYPE_SERVER)

commit 96ddc8ab43bfbf9a317041e5d13168ea1c8edfc0

BUG/MEDIUM: connection: Never cleanup server lists when freeing private conns
When a connection is released, depending on its state, it may be detached from
the session and it may be removed from the server lists. The first case may
happen for private or unsharable active connections. The second one should only
be performed for idle or available connections. We never try to remove a
connection from the server list if it is attached to a session. But it is also
important to never try to remove a private connecion from the server lists, even
if it is not attached to a session. Otherwise, the curr_used_conn server counter
is decremented once too often.

This bug was introduced by the commit 04a24c5ea ("MINOR: connection: don't check
priv flag on free"). It is related to the issue #881. It only affects the 2.3,
no backport is needed.

include/haproxy/connection.h
@@ -473,7 +473,7 @@ static inline void conn_free(struct connection *conn)
 	if (LIST_ADDED(&conn->session_list)) {
 		session_unown_conn(conn->owner, conn);
 	}
-	else {
+	else if (!(conn->flags & CO_FL_PRIVATE)) {
 		if (obj_type(conn->target) == OBJ_TYPE_SERVER)
 			srv_del_conn_from_list(__objt_server(conn->target), conn);
 	}