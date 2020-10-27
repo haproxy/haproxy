a0133fcf3509afd1727a947b79284027f94245e0
Willy Tarreau <w@1wt.eu>
2020-10-27 10:35:32 +0100

    BUG/MINOR: log: fix risk of null deref on error path
    Previous commit ae32ac74db ("BUG/MINOR: log: fix memory leak on logsrv
    parse error") addressed one issue and introduced another one, the logsrv
    pointer may also be null at the end of the function so we must test it
    before deciding to dereference it.
    
    This should be backported along with the patch above to 2.2.

--- src/log.c
+++ src/log.c
@@ -1039,7 +1039,8 @@ int parse_logsrv(char **args, struct list *logsrvs, int do_del, char **err)
 
   error:
 	free(smp_rgs);
-	free(logsrv->ring_name);
+	if (logsrv)
+		free(logsrv->ring_name);
 	free(logsrv);
 	return 0;
 }
