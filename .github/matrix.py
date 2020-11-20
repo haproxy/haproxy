cf67a12598d93e14f18d45ecf096792732e5652a
Ilya Shipitsin <chipitsine@gmail.com>
2020-11-20 14:42:27 +0500

    CI: Github Actions: remove LibreSSL-3.0.2 builds

--- .github/matrix.py
+++ .github/matrix.py
@@ -96,7 +96,6 @@ for CC in ["gcc", "clang"]:
         "stock",
         "OPENSSL_VERSION=1.0.2u",
         "LIBRESSL_VERSION=2.9.2",
-        "LIBRESSL_VERSION=3.0.2",
         "LIBRESSL_VERSION=3.1.1",
     ]:
         flags = ["USE_OPENSSL=1"]
