0a76901926e26ac39f334a8818d3a3994e448d88
Frédéric Lécaille <flecaille@haproxy.com>
2020-11-23 15:50:46 +0100

    MINOR: cfgparse: QUIC default server transport parameters init.
    This patch is there to initialize the default transport parameters for QUIC
    as a preparation for one of the QUIC next steps to come: fully support QUIC
    protocol for haproxy servers.

--- src/cfgparse.c
+++ src/cfgparse.c
@@ -458,6 +458,10 @@ void init_default_instance()
 
 	defproxy.email_alert.level = LOG_ALERT;
 	defproxy.load_server_state_from_file = PR_SRV_STATE_FILE_UNSPEC;
+#if defined(USE_QUIC)
+	quic_transport_params_init(&defproxy.defsrv.quic_params, 0);
+#endif
+
 }
 
 /* Allocate and initialize the frontend of a "peers" section found in
