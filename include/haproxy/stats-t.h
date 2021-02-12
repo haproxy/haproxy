haproxy/haproxy

commit ee4f5f83d3159d8dc0f0eef3894b86abb1f10cf2

MINOR: stats: get rid of the ST_CONVDONE flag
This flag was added in 1.4-rc1 by commit 329f74d463 ("[BUG] uri_auth: do
not attemp to convert uri_auth -> http-request more than once") to
address the case where two proxies inherit the stats settings from
the defaults instance, and the first one compiles the expression while
the second one uses it. In this case since they use the exact same
uri_auth pointer, only the first one should compile and the second one
must not fail the check. This was addressed by adding an ST_CONVDONE
flag indicating that the expression conversion was completed and didn't
need to be done again. But this is a hack and it becomes cumbersome in
the middle of the other flags which are all relevant to the stats
applet. Let's instead fix it by checking if we're dealing with an
alias of the defaults instance and refrain from compiling this twice.
This allows us to remove the ST_CONVDONE flag.

A typical config requiring this check is :

   defaults
        mode http
        stats auth foo:bar

   listen l1
        bind :8080

   listen l2
        bind :8181

Without this (or previous) check it would cmoplain when checking l2's
validity since the rule was already built.

src/cfgparse.c
@@ -2875,7 +2875,7 @@ int check_config_validity()
 			}
 		}
 
-		if (curproxy->uri_auth && !(curproxy->uri_auth->flags & ST_CONVDONE) &&
+		if (curproxy->uri_auth && curproxy->uri_auth != defproxy.uri_auth &&
 		    !LIST_ISEMPTY(&curproxy->uri_auth->http_req_rules) &&
 		    (curproxy->uri_auth->userlist || curproxy->uri_auth->auth_realm )) {
 			ha_alert("%s '%s': stats 'auth'/'realm' and 'http-request' can't be used at the same time.\n",
@@ -2884,11 +2884,12 @@ int check_config_validity()
 			goto out_uri_auth_compat;
 		}
 
-		if (curproxy->uri_auth && curproxy->uri_auth->userlist && !(curproxy->uri_auth->flags & ST_CONVDONE)) {
+		if (curproxy->uri_auth && curproxy->uri_auth->userlist &&
+		    (curproxy->uri_auth != defproxy.uri_auth ||
+		     LIST_ISEMPTY(&curproxy->uri_auth->http_req_rules))) {
 			const char *uri_auth_compat_req[10];
 			struct act_rule *rule;
 			int i = 0;
-
 			/* build the ACL condition from scratch. We're relying on anonymous ACLs for that */
 			uri_auth_compat_req[i++] = "auth";
 
@@ -2915,8 +2916,6 @@ int check_config_validity()
 				free(curproxy->uri_auth->auth_realm);
 				curproxy->uri_auth->auth_realm = NULL;
 			}
-
-			curproxy->uri_auth->flags |= ST_CONVDONE;
 		}
 out_uri_auth_compat:
 

include/common/uri_auth.h
@@ -30,7 +30,7 @@ struct stat_scope {
 #define	ST_SHNODE	0x00000002	/* show node name */
 #define	ST_SHDESC	0x00000004	/* show description */
 #define	ST_SHLGNDS	0x00000008	/* show legends */
-#define	ST_CONVDONE	0x00000010	/* req_acl conversion done */
+/* unused:              0x00000010 */
 #define	ST_SHOWADMIN	0x00000020	/* show the admin column */
 
 /* later we may link them to support multiple URI matching */

commit 5bbc676608f654ae76c7a4cc5852a443bfe8bd41

BUG/MINOR: stats: revert the change on ST_CONVDONE
In 2.1, commit ee4f5f83d ("MINOR: stats: get rid of the ST_CONVDONE flag")
introduced a subtle bug. By testing curproxy against defproxy in
check_config_validity(), it tried to eliminate the need for a flag
to indicate that stats authentication rules were already compiled,
but by doing so it left the issue opened for the case where a new
defaults section appears after the two proxies sharing the first
one:

      defaults
          mode http
          stats auth foo:bar

      listen l1
          bind :8080

      listen l2
          bind :8181

      defaults
          # just to break above

This config results in:
  [ALERT] 042/113725 (3121) : proxy 'f2': stats 'auth'/'realm' and 'http-request' can't be used at the same time.
  [ALERT] 042/113725 (3121) : Fatal errors found in configuration.

Removing the last defaults remains OK. It turns out that the cleanups
that followed that patch render it useless, so the best fix is to revert
the change (with the up-to-date flags instead). The flag was marked as
belonging to the config. It's not exact but it's the closest to the
reality, as it's not there to configure the behavior but ti mention
that the config parser did its job.

This could be backported as far as 2.1, but in practice it looks like
nobody ever hit it.

include/haproxy/stats-t.h
@@ -41,6 +41,7 @@
 #define STAT_SHOW_FDESC 0x00001000      /* show the field descriptions when possible */
 #define STAT_SHMODULES  0x00002000      /* conf: show modules */
 #define STAT_HIDE_MAINT 0x00004000	/* hide maint/disabled servers */
+#define STAT_CONVDONE   0x00008000	/* conf: rules conversion done */
 
 #define STAT_BOUND      0x00800000	/* bound statistics to selected proxies/types/services */
 #define STAT_STARTED    0x01000000	/* some output has occurred */

src/cfgparse.c
@@ -2842,7 +2842,7 @@ int check_config_validity()
 			}
 		}
 
-		if (curproxy->uri_auth && curproxy->uri_auth != defproxy.uri_auth &&
+		if (curproxy->uri_auth && !(curproxy->uri_auth->flags & STAT_CONVDONE) &&
 		    !LIST_ISEMPTY(&curproxy->uri_auth->http_req_rules) &&
 		    (curproxy->uri_auth->userlist || curproxy->uri_auth->auth_realm )) {
 			ha_alert("%s '%s': stats 'auth'/'realm' and 'http-request' can't be used at the same time.\n",
@@ -2852,7 +2852,7 @@ int check_config_validity()
 		}
 
 		if (curproxy->uri_auth && curproxy->uri_auth->userlist &&
-		    (curproxy->uri_auth != defproxy.uri_auth ||
+		    (!(curproxy->uri_auth->flags & STAT_CONVDONE) ||
 		     LIST_ISEMPTY(&curproxy->uri_auth->http_req_rules))) {
 			const char *uri_auth_compat_req[10];
 			struct act_rule *rule;
@@ -2883,6 +2883,7 @@ int check_config_validity()
 				free(curproxy->uri_auth->auth_realm);
 				curproxy->uri_auth->auth_realm = NULL;
 			}
+			curproxy->uri_auth->flags |= STAT_CONVDONE;
 		}
 out_uri_auth_compat:
 