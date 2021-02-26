cccded98c7b3f2281fe4481895b1392e78cc5df0
Christopher Faulet <cfaulet@haproxy.com>
2021-02-26 12:45:56 +0100

    BUG/MINOR: http-ana: Only consider dst address to process originalto option
    When an except parameter is used for originalto option, only the destination
    address must be evaluated. Especially, the address family of the destination
    must be tested and not the source one.
    
    This patch must be backported to all stable versions. However be careful,
    depending the versions the code may be slightly different.

--- src/http_ana.c
+++ src/http_ana.c
@@ -719,7 +719,7 @@ int http_process_request(struct stream *s, struct channel *req, int an_bit)
 	if ((sess->fe->options | s->be->options) & PR_O_ORGTO) {
 
 		/* FIXME: don't know if IPv6 can handle that case too. */
-		if (cli_conn && conn_get_src(cli_conn) && cli_conn->src->ss_family == AF_INET && conn_get_dst(cli_conn)) {
+		if (cli_conn && conn_get_dst(cli_conn) && cli_conn->dst->ss_family == AF_INET) {
 			/* Add an X-Original-To header unless the destination IP is
 			 * in the 'except' network range.
 			 */
