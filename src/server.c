haproxy/haproxy

commit d127ffa9f439a4eb05455f371191694f22dd7a5b

BUG/MEDIUM: resolvers: Reset address for unresolved servers
If the DNS resolution failed for a server, its ip address must be
removed. Otherwise, the server is stopped but keeps its ip. This may be
confusing when the servers state are retrieved on the CLI and it may lead to
undefined behavior if HAproxy is configured to load its servers state from a
file.

This patch should be backported as far as 2.0.

src/server.c
@@ -4162,6 +4162,7 @@ int snr_resolution_error_cb(struct resolv_requester *requester, int error_code)
 		return 1;
 	HA_SPIN_LOCK(SERVER_LOCK, &s->lock);
 	snr_update_srv_status(s, 0);
+	memset(&s->addr, 0, sizeof(s->addr));
 	HA_SPIN_UNLOCK(SERVER_LOCK, &s->lock);
 	return 1;
 }

commit 5130c21fbb806a28ab4e535b700df872a80798d8

BUG/MINOR: resolvers: Reset server address on DNS error only on status change
When a DNS resolution error is detected, in snr_resolution_error_cb(), the
server address must be reset only if the server status has changed. It this
case, it means the server is set to RMAINT. Thus the server address may by
reset.

This patch fixes a bug introduced by commit d127ffa9f ("BUG/MEDIUM:
resolvers: Reset address for unresolved servers"). It must be backported as
far as 2.0.

src/server.c
@@ -3293,8 +3293,8 @@ int snr_resolution_error_cb(struct resolv_requester *requester, int error_code)
 	if (!s)
 		return 1;
 	HA_SPIN_LOCK(SERVER_LOCK, &s->lock);
-	snr_update_srv_status(s, 1);
-	memset(&s->addr, 0, sizeof(s->addr));
+	if (!snr_update_srv_status(s, 1))
+		memset(&s->addr, 0, sizeof(s->addr));
 	HA_SPIN_UNLOCK(SERVER_LOCK, &s->lock);
 	return 1;
 }