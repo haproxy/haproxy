6a6dabe66809f4c33b4f71c8825a943ea6b08674
Willy Tarreau <w@1wt.eu>
2021-04-02 14:33:40 +0200

    CONTRIB: merge ip6range with iprange
    No need to have two separate sub-projects doing the same thing, let's
    merge them.

--- contrib/iprange/ip6range.c
+++ contrib/iprange/ip6range.c
@@ -314,7 +314,7 @@ static void usage(const char *argv0)
 		"\n", argv0);
 }
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	char line[MAXLINE];
 	int l, lnum;
@@ -393,4 +393,5 @@ main(int argc, char **argv)
 			convert_range(&sa, &da, he, NULL);
 		}
 	}
+	return 0;
 }
