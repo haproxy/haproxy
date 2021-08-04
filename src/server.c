d332f1396b101f3f03e9fef405d42f03fc3647b6
Willy Tarreau <w@1wt.eu>
2021-08-04 19:35:13 +0200

    BUG/MINOR: server: update last_change on maint->ready transitions too
    Nenad noticed that when leaving maintenance, the servers' last_change
    field was not updated. This is visible in the Status column of the stats
    page in front of the state, as the cumuled time spent in the current state
    is wrong, it starts from the last transition (typically ready->maint). In
    addition, the backend's state was not updated either, because the down
    transition is performed by set_backend_down() which also emits a log, and
    it is this function which was extended to update the backend's last_change,
    but it's not called for down->up transitions so that was not done.
    
    The most visible (and unpleasant) effect of this bug is that it affects
    slowstart so such a server could immediately restart with a significant
    load ratio.
    
    This should likely be backported to all stable releases.

--- src/server.c
+++ src/server.c
@@ -5138,6 +5138,7 @@ static void srv_update_status(struct server *s)
 				s->next_state = SRV_ST_STOPPING;
 			}
 			else {
+				s->last_change = now.tv_sec;
 				s->next_state = SRV_ST_STARTING;
 				if (s->slowstart > 0) {
 					if (s->warmup)
@@ -5197,6 +5198,8 @@ static void srv_update_status(struct server *s)
 
 		if (prev_srv_count && s->proxy->srv_bck == 0 && s->proxy->srv_act == 0)
 			set_backend_down(s->proxy);
+		else if (!prev_srv_count && (s->proxy->srv_bck || s->proxy->srv_act))
+			s->proxy->last_change = now.tv_sec;
 
 		/* If the server is set with "on-marked-up shutdown-backup-sessions",
 		 * and it's not a backup server and its effective weight is > 0,
