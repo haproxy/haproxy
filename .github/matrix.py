f14edc82124e34ac88e08239f3c6ddd09da1cddd
Willy Tarreau <w@1wt.eu>
2021-09-09 14:27:37 +0200

    CI: Github Actions: temporarily disable Opentracing
    As discussed in the thread below, the recent variables changes
    unfortunately broke Opentracing. Discussions are ongoing about
    possible solutions but none of them can be done in a 3-liner so
    we'd rather disable opentracing from the full-features build for
    the time being.
    
    Link: https://www.mail-archive.com/haproxy@formilux.org/msg41131.html

--- .github/matrix.py
+++ .github/matrix.py
@@ -73,7 +73,6 @@ for CC in ["gcc", "clang"]:
             "CC": CC,
             "FLAGS": [
                 "USE_ZLIB=1",
-                "USE_OT=1",
                 "OT_INC=${HOME}/opt/include",
                 "OT_LIB=${HOME}/opt/lib",
                 "OT_RUNPATH=1",
@@ -148,7 +147,6 @@ matrix.append(
         "FLAGS": get_asan_flags(CC)
         + [
             "USE_ZLIB=1",
-            "USE_OT=1",
             "OT_INC=${HOME}/opt/include",
             "OT_LIB=${HOME}/opt/lib",
             "OT_RUNPATH=1",
