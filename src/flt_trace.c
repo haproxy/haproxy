haproxy/haproxy

commit c41d8bd65aac1fc3525759280cc6d9b511d1dc93

CLEANUP: flt-trace: Remove unused random-parsing option
This option was only used by the legacy HTTP mode. In HTX, it is not used. So it
can be removed.

src/flt_trace.c
@@ -32,7 +32,6 @@ struct flt_ops trace_ops;
 struct trace_config {
 	struct proxy *proxy;
 	char         *name;
-	int           rand_parsing;
 	int           rand_forwarding;
 	int           hexdump;
 };
@@ -188,8 +187,7 @@ trace_init(struct proxy *px, struct flt_conf *fconf)
 	fconf->flags |= FLT_CFG_FL_HTX;
 	fconf->conf = conf;
 
-	FLT_TRACE(conf, "filter initialized [read random=%s - fwd random=%s - hexdump=%s]",
-	      (conf->rand_parsing ? "true" : "false"),
+	FLT_TRACE(conf, "filter initialized [fwd random=%s - hexdump=%s]",
 	      (conf->rand_forwarding ? "true" : "false"),
 	      (conf->hexdump ? "true" : "false"));
 	return 0;
@@ -633,7 +631,7 @@ parse_trace_flt(char **args, int *cur_arg, struct proxy *px,
 				pos++;
 			}
 			else if (!strcmp(args[pos], "random-parsing"))
-				conf->rand_parsing = 1;
+				continue; // ignore
 			else if (!strcmp(args[pos], "random-forwarding"))
 				conf->rand_forwarding = 1;
 			else if (!strcmp(args[pos], "hexdump"))

commit a8018eb470f3299d1658abdd2ba1a1d0091ca8e8

BUG/MINOR: flt-trace: fix an infinite loop when random-parsing is set
The issue is introduced with the commit c41d8bd65 ("CLEANUP: flt-trace:
Remove unused random-parsing option").

This must be backported everywhere the above commit is.

src/flt_trace.c
@@ -642,7 +642,7 @@ parse_trace_flt(char **args, int *cur_arg, struct proxy *px,
 			else if (strcmp(args[pos], "quiet") == 0)
 				conf->flags |= TRACE_F_QUIET;
 			else if (strcmp(args[pos], "random-parsing") == 0)
-				continue; // ignore
+				; // ignore
 			else if (strcmp(args[pos], "random-forwarding") == 0)
 				conf->flags |= TRACE_F_RAND_FWD;
 			else if (strcmp(args[pos], "hexdump") == 0)