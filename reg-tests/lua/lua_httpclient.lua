039cc083ffe3860033c7d6bbf79504b48f81cd84
William Lallemand <wlallemand@haproxy.org>
2021-09-24 19:02:50 +0200

    REGTESTS: lua: test the httpclient:get() feature
    This reg-test is heavily inspired by the lua_socket.vtc one.
    
    It replaces the HTTP/1.1 request made manually with a socket object with
    an httpclient object.

---
+++ reg-tests/lua/lua_httpclient.lua
@@ -0,0 +1,29 @@
+
+local vtc_port = 0
+
+core.register_service("fakeserv", "http", function(applet)
+	vtc_port = applet.headers["vtcport"][0]
+	core.Info("APPLET START")
+	local response = "OK"
+	applet:add_header("Server", "haproxy/webstats")
+	applet:add_header("Content-Length", string.len(response))
+	applet:add_header("Content-Type", "text/html")
+	applet:start_response()
+	applet:send(response)
+	core.Info("APPLET DONE")
+end)
+
+local function cron()
+	-- wait for until the correct port is set through the c0 request..
+	while vtc_port == 0 do
+		core.msleep(1)
+	end
+	core.Debug('CRON port:' .. vtc_port)
+
+	local httpclient = core.httpclient()
+	local response = httpclient:get("http://127.0.0.1:" .. vtc_port)
+
+	core.Info("Received: " .. response.body)
+end
+
+core.register_task(cron)
