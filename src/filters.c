d28b2b2352291991190fd0693fdfd3d7430e6ddb
Christopher Faulet <cfaulet@haproxy.com>
2021-10-04 07:50:13 +0200

    BUG/MEDIUM: filters: Fix a typo when a filter is attached blocking the release
    When a filter is attached to a stream, the wrong FLT_END analyzer is added
    on the request channel. AN_REQ_FLT_END must be added instead of
    AN_RES_FLT_END. Because of this bug, the stream may hang on the filter
    release stage.
    
    It seems to be ok for HTTP filters (cache & compression) in HTTP mode. But
    when enabled on a TCP proxy, the stream is blocked until the client or the
    server timeout expire because data forwarding is blocked. The stream is then
    prematurely aborted.
    
    This bug was introduced by commit 26eb5ea35 ("BUG/MINOR: filters: Always set
    FLT_END analyser when CF_FLT_ANALYZE flag is set"). The patch must be
    backported in all stable versions.

--- src/filters.c
+++ src/filters.c
@@ -475,7 +475,7 @@ flt_stream_start(struct stream *s)
 	}
 	if (strm_li(s) && (strm_li(s)->analysers & AN_REQ_FLT_START_FE)) {
 		s->req.flags |= CF_FLT_ANALYZE;
-		s->req.analysers |= AN_RES_FLT_END;
+		s->req.analysers |= AN_REQ_FLT_END;
 	}
 	return 0;
 }
