haproxy/haproxy

commit 11c6c396560107ac892a553bfa0dada222823b1c

MINOR: resolvers: Clean server in a dedicated function when removing a SRV item
A dedicated function is now used to clean up servers when a SRV item becomes
obsolete or when a requester is removed from a resolution. This patch is
mandatory to fix a bug.

src/resolvers.c
@@ -571,6 +571,28 @@ int resolv_read_name(unsigned char *buffer, unsigned char *bufend,
 	return 0;
 }
 
+/* Cleanup fqdn/port and address of a server attached to a SRV resolution. This
+ * happens when an SRV item is purged or when the server status is considered as
+ * obsolete.
+ *
+ * Must be called with the DNS lock held.
+ */
+static void resolv_srvrq_cleanup_srv(struct server *srv)
+{
+	resolv_unlink_resolution(srv->resolv_requester, 0);
+	HA_SPIN_LOCK(SERVER_LOCK, &srv->lock);
+	srvrq_update_srv_status(srv, 1);
+	ha_free(&srv->hostname);
+	ha_free(&srv->hostname_dn);
+	srv->hostname_dn_len = 0;
+	memset(&srv->addr, 0, sizeof(srv->addr));
+	srv->svc_port = 0;
+	srv->flags |= SRV_F_NO_RESOLUTION;
+	HA_SPIN_UNLOCK(SERVER_LOCK, &srv->lock);
+	LIST_DELETE(&srv->srv_rec_item);
+	LIST_APPEND(&srv->srvrq->attached_servers, &srv->srv_rec_item);
+}
+
 /* Checks for any obsolete record, also identify any SRV request, and try to
  * find a corresponding server.
 */
@@ -604,20 +626,8 @@ static void resolv_check_response(struct resolv_resolution *res)
 			}
 			else if (item->type == DNS_RTYPE_SRV) {
 				/* Remove any associated server */
-				list_for_each_entry_safe(srv, srvback, &item->attached_servers, srv_rec_item) {
-					resolv_unlink_resolution(srv->resolv_requester, 0);
-					HA_SPIN_LOCK(SERVER_LOCK, &srv->lock);
-					srvrq_update_srv_status(srv, 1);
-					ha_free(&srv->hostname);
-					ha_free(&srv->hostname_dn);
-					srv->hostname_dn_len = 0;
-					memset(&srv->addr, 0, sizeof(srv->addr));
-					srv->svc_port = 0;
-					srv->flags |= SRV_F_NO_RESOLUTION;
-					HA_SPIN_UNLOCK(SERVER_LOCK, &srv->lock);
-					LIST_DELETE(&srv->srv_rec_item);
-					LIST_APPEND(&srv->srvrq->attached_servers, &srv->srv_rec_item);
-				}
+				list_for_each_entry_safe(srv, srvback, &item->attached_servers, srv_rec_item)
+					resolv_srvrq_cleanup_srv(srv);
 			}
 
 			LIST_DELETE(&item->list);
@@ -1930,20 +1940,8 @@ void resolv_detach_from_resolution_answer_items(struct resolv_resolution *res,  
 		list_for_each_entry_safe(item, itemback, &res->response.answer_list, list) {
 			if (item->type == DNS_RTYPE_SRV) {
 				list_for_each_entry_safe(srv, srvback, &item->attached_servers, srv_rec_item) {
-					if (srv->srvrq == srvrq) {
-						HA_SPIN_LOCK(SERVER_LOCK, &srv->lock);
-						resolv_unlink_resolution(srv->resolv_requester, safe);
-						srvrq_update_srv_status(srv, 1);
-						ha_free(&srv->hostname);
-						ha_free(&srv->hostname_dn);
-						srv->hostname_dn_len = 0;
-						memset(&srv->addr, 0, sizeof(srv->addr));
-						srv->svc_port = 0;
-						srv->flags |= SRV_F_NO_RESOLUTION;
-						HA_SPIN_UNLOCK(SERVER_LOCK, &srv->lock);
-						LIST_DELETE(&srv->srv_rec_item);
-						LIST_APPEND(&srvrq->attached_servers, &srv->srv_rec_item);
-					}
+					if (srv->srvrq == srvrq)
+						resolv_srvrq_cleanup_srv(srv);
 				}
 			}
 		}

commit 2a67aa0a51acc2d3469061b8732faf7e597f9c69

BUG/MAJOR: resolvers: add other missing references during resolution removal
There is a fundamental design bug in the resolvers code which is that
a list of active resolutions is being walked to try to delete outdated
entries, and that the code responsible for removing them also removes
other elements, including the next one which will be visited by the
list iterator. This randomly causes a use-after-free condition leading
to crashes, infinite loops and various other issues such as random memory
corruption.

A first fix for the memory fix for this was brought by commit 0efc0993e
("BUG/MEDIUM: resolvers: Don't release resolution from a requester
callbacks"). While preparing for more fixes, some code was factored by
commit 11c6c3965 ("MINOR: resolvers: Clean server in a dedicated function
when removing a SRV item"), which inadvertently passed "0" as the "safe"
argument all the time, missing one case of removal protection, instead
of always using "safe". This patch reintroduces the correct argument.

This must be backported with all fixes above.

Cc: Christopher Faulet <cfaulet@haproxy.com>

src/resolvers.c
@@ -564,9 +564,9 @@ int resolv_read_name(unsigned char *buffer, unsigned char *bufend,
  *
  * Must be called with the DNS lock held.
  */
-static void resolv_srvrq_cleanup_srv(struct server *srv)
+static void resolv_srvrq_cleanup_srv(struct server *srv, int safe)
 {
-	resolv_unlink_resolution(srv->resolv_requester, 0);
+	resolv_unlink_resolution(srv->resolv_requester, safe);
 	HA_SPIN_LOCK(SERVER_LOCK, &srv->lock);
 	srvrq_update_srv_status(srv, 1);
 	ha_free(&srv->hostname);
@@ -597,7 +597,7 @@ static struct task *resolv_srvrq_expire_task(struct task *t, void *context, unsi
 		goto end;
 
 	HA_SPIN_LOCK(DNS_LOCK, &srv->srvrq->resolvers->lock);
-	resolv_srvrq_cleanup_srv(srv);
+	resolv_srvrq_cleanup_srv(srv, 0);
 	HA_SPIN_UNLOCK(DNS_LOCK, &srv->srvrq->resolvers->lock);
 
   end:
@@ -638,7 +638,7 @@ static void resolv_check_response(struct resolv_resolution *res)
 			else if (item->type == DNS_RTYPE_SRV) {
 				/* Remove any associated server */
 				list_for_each_entry_safe(srv, srvback, &item->attached_servers, srv_rec_item)
-					resolv_srvrq_cleanup_srv(srv);
+					resolv_srvrq_cleanup_srv(srv, 0);
 			}
 
 			LIST_DELETE(&item->list);
@@ -1944,7 +1944,7 @@ void resolv_detach_from_resolution_answer_items(struct resolv_resolution *res,  
 			if (item->type == DNS_RTYPE_SRV) {
 				list_for_each_entry_safe(srv, srvback, &item->attached_servers, srv_rec_item) {
 					if (srv->srvrq == srvrq)
-						resolv_srvrq_cleanup_srv(srv);
+						resolv_srvrq_cleanup_srv(srv, safe);
 				}
 			}
 		}