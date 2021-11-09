4c5a591b1029ef03c3461a7771483aa523e3111a
Christopher Faulet <cfaulet@haproxy.com>
2021-11-09 17:48:39 +0100

    Revert "BUG/MINOR: http-ana: Don't eval front after-response rules if stopped on back"
    This reverts commit 597909f4e67866c4f3ecf77f95f2cd4556c0c638
    
    http-after-response rules evaluation was changed to do the same that was
    done for http-response, in the code. However, the opposite must be performed
    instead. Only the rules of the current section must be stopped. Thus the
    above commit is reverted and the http-response rules evaluation will be
    fixed instead.
    
    Note that only "allow" action is concerned. It is most probably an uncommon
    action for an http-after-request rule.
    
    This patch must be backported as far as 2.2 if the above commit was
    backported.

--- src/http_ana.c
+++ src/http_ana.c
@@ -2982,12 +2982,11 @@ int http_eval_after_res_rules(struct stream *s)
 		vars_init_head(&s->vars_reqres, SCOPE_RES);
 	}
 
-
 	def_rules = (s->be->defpx ? &s->be->defpx->http_after_res_rules : NULL);
 	rules = &s->be->http_after_res_rules;
 
 	ret = http_res_get_intercept_rule(s->be, def_rules, rules, s);
-	if (ret == HTTP_RULE_RES_CONT && sess->fe != s->be) {
+	if ((ret == HTTP_RULE_RES_CONT || ret == HTTP_RULE_RES_STOP) && sess->fe != s->be) {
 		def_rules = ((sess->fe->defpx && sess->fe->defpx != s->be->defpx) ? &sess->fe->defpx->http_after_res_rules : NULL);
 		rules = &sess->fe->http_after_res_rules;
 		ret = http_res_get_intercept_rule(sess->fe, def_rules, rules, s);
