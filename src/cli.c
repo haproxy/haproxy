740629e2966641871b183d1c728fa7aeda45a1cc
William Lallemand <wlallemand@haproxy.org>
2021-12-14 15:22:29 +0100

    MINOR: cli: "show version" displays the current process version
    This patch implements a simple "show version" command which returns
    the version of the current process.
    
    It's available from the master and the worker processes, so it is easy
    to check if the master and the workers have the same version.
    
    This is a minor patch that really improve compatibility checks
    for scripts.
    
    Could be backported in haproxy version as far as 2.0.

--- src/cli.c
+++ src/cli.c
@@ -1756,6 +1756,14 @@ static int cli_parse_expert_experimental_mode(char **args, char *payload, struct
 	return 1;
 }
 
+/* shows HAProxy version */
+static int cli_parse_show_version(char **args, char *payload, struct appctx *appctx, void *private)
+{
+	char *msg = NULL;
+
+	return cli_dynmsg(appctx, LOG_INFO, memprintf(&msg, "%s\n", haproxy_version));
+}
+
 int cli_parse_default(char **args, char *payload, struct appctx *appctx, void *private)
 {
 	return 0;
@@ -2980,6 +2988,7 @@ static struct cli_kw_list cli_kws = {{ },{
 	{ { "show", "cli", "level", NULL },      "show cli level                          : display the level of the current CLI session",            cli_parse_show_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{ { "show", "fd", NULL },                "show fd [num]                           : dump list of file descriptors in use or a specific one",  cli_parse_show_fd, cli_io_handler_show_fd, NULL },
 	{ { "show", "activity", NULL },          "show activity                           : show per-thread activity stats (for support/developers)", cli_parse_default, cli_io_handler_show_activity, NULL },
+	{ { "show", "version", NULL },           "show version                            : show version of the current process",                     cli_parse_show_version, NULL, NULL, NULL, ACCESS_MASTER },
 	{ { "operator", NULL },                  "operator                                : lower the level of the current CLI session to operator",  cli_parse_set_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{ { "user", NULL },                      "user                                    : lower the level of the current CLI session to user",      cli_parse_set_lvl, NULL, NULL, NULL, ACCESS_MASTER},
 	{{},}
