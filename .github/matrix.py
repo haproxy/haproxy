874c907a2e5ffae50599b1f368e0488d8eb2fe4f
Ilya Shipitsin <chipitsine@gmail.com>
2021-12-25 14:01:52 +0500

    CI: github actions: update OpenSSL to 3.0.1
    OpenSSL-3.0.1 was released on 14 Dec 2021, let's switch to it

--- .github/matrix.py
+++ .github/matrix.py
@@ -110,7 +110,7 @@ for CC in ["gcc", "clang"]:
     for ssl in [
         "stock",
         "OPENSSL_VERSION=1.0.2u",
-        "OPENSSL_VERSION=3.0.0",
+        "OPENSSL_VERSION=3.0.1",
         "LIBRESSL_VERSION=2.9.2",
         "LIBRESSL_VERSION=3.3.3",
         "QUICTLS=yes",
@@ -119,7 +119,7 @@ for CC in ["gcc", "clang"]:
         flags = ["USE_OPENSSL=1"]
         if ssl == "BORINGSSL=yes" or ssl == "QUICTLS=yes":
             flags.append("USE_QUIC=1")
-        if "OPENSSL_VERSION=3.0.0" in ssl or ssl == "QUICTLS=yes":
+        if "OPENSSL_VERSION=3.0." in ssl or ssl == "QUICTLS=yes":
             flags.append('DEBUG_CFLAGS="-g -Wno-deprecated-declarations"')
         if ssl != "stock":
             flags.append("SSL_LIB=${HOME}/opt/lib")
