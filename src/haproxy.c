haproxy/haproxy

commit df91cbd5845eb7ccdd2586712cb62585fd55d7c0

MINOR: cpuset: switch to sched_setaffinity for FreeBSD 14 and above.
Following up previous update on cpuset-t.h. Ultimately, at some point
 the cpuset_setaffinity code path could be removed.

src/haproxy.c
@@ -3233,12 +3233,11 @@ int main(int argc, char **argv)
 #ifdef USE_CPU_AFFINITY
 		if (!in_parent && ha_cpuset_count(&cpu_map.proc)) {   /* only do this if the process has a CPU map */
 
-#ifdef __FreeBSD__
 			struct hap_cpuset *set = &cpu_map.proc;
-			ret = cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(set->cpuset), &set->cpuset);
-#elif defined(__linux__) || defined(__DragonFly__)
-			struct hap_cpuset *set = &cpu_map.proc;
+#if defined(CPUSET_USE_CPUSET) || defined(__DragonFly__)
 			sched_setaffinity(0, sizeof(set->cpuset), &set->cpuset);
+#elif defined(__FreeBSD__)
+			ret = cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(set->cpuset), &set->cpuset);
 #endif
 		}
 #endif

commit bb10dad5a8321ecc437ef637385ee3a0e7c09747

BUILD: cpuset: fix build issue on macos introduced by previous change
The build on macos was broken by recent commit df91cbd58 ("MINOR: cpuset:
switch to sched_setaffinity for FreeBSD 14 and above."), let's move the
variable declaration inside the ifdef.

src/haproxy.c
@@ -3238,10 +3238,11 @@ int main(int argc, char **argv)
 #ifdef USE_CPU_AFFINITY
 		if (!in_parent && ha_cpuset_count(&cpu_map.proc)) {   /* only do this if the process has a CPU map */
 
-			struct hap_cpuset *set = &cpu_map.proc;
 #if defined(CPUSET_USE_CPUSET) || defined(__DragonFly__)
+			struct hap_cpuset *set = &cpu_map.proc;
 			sched_setaffinity(0, sizeof(set->cpuset), &set->cpuset);
 #elif defined(__FreeBSD__)
+			struct hap_cpuset *set = &cpu_map.proc;
 			ret = cpuset_setaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID, -1, sizeof(set->cpuset), &set->cpuset);
 #endif
 		}