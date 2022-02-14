e1c3546efa7ad60939908ceb704423b3974145b4
Frédéric Lécaille <flecaille@haproxy.com>
2022-02-14 19:01:21 +0100

    MINOR: quic: Avoid warning about NULL pointer dereferences
    This is the same fixe as for this commit:
        "BUILD: tree-wide: avoid warnings caused by redundant checks of obj_types"
    Should fix CID 1469649 for GH #1546

--- src/proto_quic.c
+++ src/proto_quic.c
@@ -263,11 +263,11 @@ int quic_connect_server(struct connection *conn, int flags)
 
 	switch (obj_type(conn->target)) {
 	case OBJ_TYPE_PROXY:
-		be = objt_proxy(conn->target);
+		be = __objt_proxy(conn->target);
 		srv = NULL;
 		break;
 	case OBJ_TYPE_SERVER:
-		srv = objt_server(conn->target);
+		srv = __objt_server(conn->target);
 		be = srv->proxy;
 		break;
 	default:
