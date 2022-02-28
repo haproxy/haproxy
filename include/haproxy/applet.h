haproxy/haproxy

commit 693b23bb102ada09b2324804ababc3279d938589

MEDIUM: tree-wide: Use unsafe conn-stream API when it is relevant
The unsafe conn-stream API (__cs_*) is now used when we are sure the good
endpoint or application is attached to the conn-stream. This avoids compiler
warnings about possible null derefs. It also simplify the code and clear up
any ambiguity about manipulated entities.

src/stream_interface.c
@@ -387,12 +387,10 @@ int conn_si_send_proxy(struct connection *conn, unsigned int flag)
 		 */
 
 		if (cs && cs->data_cb == &si_conn_cb) {
-			struct stream *strm = cs_strm(cs);
-
 			ret = make_proxy_line(trash.area, trash.size,
 					      objt_server(conn->target),
 					      cs_conn(si_opposite(cs_si(cs))->cs),
-					      strm);
+					      cs_strm(cs));
 		}
 		else {
 			/* The target server expects a LOCAL line to be sent first. Retrieving
@@ -595,7 +593,7 @@ static void stream_int_notify(struct stream_interface *si)
  */
 static int si_cs_process(struct conn_stream *cs)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	struct stream_interface *si = cs_si(cs);
 	struct channel *ic = si_ic(si);
 	struct channel *oc = si_oc(si);
@@ -683,13 +681,11 @@ static int si_cs_process(struct conn_stream *cs)
  */
 int si_cs_send(struct conn_stream *cs)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	struct stream_interface *si = cs_si(cs);
 	struct channel *oc = si_oc(si);
 	int ret;
 	int did_send = 0;
-
-	BUG_ON(!conn);
 
 	if (conn->flags & CO_FL_ERROR || cs->flags & (CS_FL_ERROR|CS_FL_ERR_PENDING)) {
 		/* We're probably there because the tasklet was woken up,
@@ -998,12 +994,12 @@ void si_update_both(struct stream_interface *si_f, struct stream_interface *si_b
 	if (cs_appctx(si_f->cs) &&
 	    ((si_rx_endp_ready(si_f) && !si_rx_blocked(si_f)) ||
 	     (si_tx_endp_ready(si_f) && !si_tx_blocked(si_f))))
-		appctx_wakeup(cs_appctx(si_f->cs));
+		appctx_wakeup(__cs_appctx(si_f->cs));
 
 	if (cs_appctx(si_b->cs) &&
 	    ((si_rx_endp_ready(si_b) && !si_rx_blocked(si_b)) ||
 	     (si_tx_endp_ready(si_b) && !si_tx_blocked(si_b))))
-		appctx_wakeup(cs_appctx(si_b->cs));
+		appctx_wakeup(__cs_appctx(si_b->cs));
 }
 
 /*
@@ -1251,14 +1247,12 @@ static void stream_int_chk_snd_conn(struct stream_interface *si)
  */
 int si_cs_recv(struct conn_stream *cs)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	struct stream_interface *si = cs_si(cs);
 	struct channel *ic = si_ic(si);
 	int ret, max, cur_read = 0;
 	int read_poll = MAX_READ_POLL_LOOPS;
 	int flags = 0;
-
-	BUG_ON(!conn);
 
 	/* If not established yet, do nothing. */
 	if (si->state != SI_ST_EST)
@@ -1638,6 +1632,8 @@ void si_applet_wake_cb(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 
+	BUG_ON(cs_appctx(si->cs));
+
 	/* If the applet wants to write and the channel is closed, it's a
 	 * broken pipe and it must be reported.
 	 */
@@ -1661,7 +1657,7 @@ void si_applet_wake_cb(struct stream_interface *si)
 	 */
 	if ((si_rx_endp_ready(si) && !si_rx_blocked(si)) ||
 	    (si_tx_endp_ready(si) && !si_tx_blocked(si)))
-		appctx_wakeup(cs_appctx(si->cs));
+		appctx_wakeup(__cs_appctx(si->cs));
 }
 
 /*
@@ -1675,6 +1671,8 @@ void si_applet_wake_cb(struct stream_interface *si)
 static void stream_int_shutr_applet(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
+
+	BUG_ON(cs_appctx(si->cs));
 
 	si_rx_shut_blk(si);
 	if (ic->flags & CF_SHUTR)
@@ -1710,6 +1708,8 @@ static void stream_int_shutw_applet(struct stream_interface *si)
 	struct channel *ic = si_ic(si);
 	struct channel *oc = si_oc(si);
 
+	BUG_ON(cs_appctx(si->cs));
+
 	oc->flags &= ~CF_SHUTW_NOW;
 	if (oc->flags & CF_SHUTW)
 		return;
@@ -1723,7 +1723,7 @@ static void stream_int_shutw_applet(struct stream_interface *si)
 	}
 
 	/* on shutw we always wake the applet up */
-	appctx_wakeup(cs_appctx(si->cs));
+	appctx_wakeup(__cs_appctx(si->cs));
 
 	switch (si->state) {
 	case SI_ST_RDY:
@@ -1761,13 +1761,15 @@ static void stream_int_chk_rcv_applet(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 
+	BUG_ON(cs_appctx(si->cs));
+
 	DPRINTF(stderr, "%s: si=%p, si->state=%d ic->flags=%08x oc->flags=%08x\n",
 		__FUNCTION__,
 		si, si->state, ic->flags, si_oc(si)->flags);
 
 	if (!ic->pipe) {
 		/* (re)start reading */
-		appctx_wakeup(cs_appctx(si->cs));
+		appctx_wakeup(__cs_appctx(si->cs));
 	}
 }
 
@@ -1775,6 +1777,8 @@ static void stream_int_chk_rcv_applet(struct stream_interface *si)
 static void stream_int_chk_snd_applet(struct stream_interface *si)
 {
 	struct channel *oc = si_oc(si);
+
+	BUG_ON(cs_appctx(si->cs));
 
 	DPRINTF(stderr, "%s: si=%p, si->state=%d ic->flags=%08x oc->flags=%08x\n",
 		__FUNCTION__,
@@ -1793,7 +1797,7 @@ static void stream_int_chk_snd_applet(struct stream_interface *si)
 
 	if (!channel_is_empty(oc)) {
 		/* (re)start sending */
-		appctx_wakeup(cs_appctx(si->cs));
+		appctx_wakeup(__cs_appctx(si->cs));
 	}
 }
 

include/haproxy/stream_interface.h
@@ -55,18 +55,16 @@ void si_sync_send(struct stream_interface *si);
 /* returns the channel which receives data from this stream interface (input channel) */
 static inline struct channel *si_ic(struct stream_interface *si)
 {
-	struct stream *strm = cs_strm(si->cs);
+	struct stream *strm = __cs_strm(si->cs);
 
-	ALREADY_CHECKED(strm);
 	return ((si->flags & SI_FL_ISBACK) ? &(strm->res) : &(strm->req));
 }
 
 /* returns the channel which feeds data to this stream interface (output channel) */
 static inline struct channel *si_oc(struct stream_interface *si)
 {
-	struct stream *strm = cs_strm(si->cs);
+	struct stream *strm = __cs_strm(si->cs);
 
-	ALREADY_CHECKED(strm);
 	return ((si->flags & SI_FL_ISBACK) ? &(strm->req) : &(strm->res));
 }
 
@@ -85,27 +83,22 @@ static inline struct buffer *si_ob(struct stream_interface *si)
 /* returns the stream associated to a stream interface */
 static inline struct stream *si_strm(struct stream_interface *si)
 {
-	struct stream *strm = cs_strm(si->cs);
-
-	ALREADY_CHECKED(strm);
-	return strm;
+	return __cs_strm(si->cs);
 }
 
 /* returns the task associated to this stream interface */
 static inline struct task *si_task(struct stream_interface *si)
 {
-	struct stream *strm = cs_strm(si->cs);
+	struct stream *strm = __cs_strm(si->cs);
 
-	ALREADY_CHECKED(strm);
 	return strm->task;
 }
 
 /* returns the stream interface on the other side. Used during forwarding. */
 static inline struct stream_interface *si_opposite(struct stream_interface *si)
 {
-	struct stream *strm = cs_strm(si->cs);
+	struct stream *strm = __cs_strm(si->cs);
 
-	ALREADY_CHECKED(strm);
 	return ((si->flags & SI_FL_ISBACK) ? strm->csf->si : strm->csb->si);
 }
 
@@ -161,8 +154,8 @@ static inline void si_applet_release(struct stream_interface *si)
 {
 	struct appctx *appctx;
 
-	appctx = cs_appctx(si->cs);
-	if (appctx && appctx->applet->release && !si_state_in(si->state, SI_SB_DIS|SI_SB_CLO))
+	appctx = __cs_appctx(si->cs);
+	if (appctx->applet->release && !si_state_in(si->state, SI_SB_DIS|SI_SB_CLO))
 		appctx->applet->release(appctx);
 }
 

src/check.c
@@ -1019,7 +1019,7 @@ int httpchk_build_status_header(struct server *s, struct buffer *buf)
 static int wake_srv_chk(struct conn_stream *cs)
 {
 	struct connection *conn;
-	struct check *check = cs_check(cs);
+	struct check *check = __cs_check(cs);
 	struct email_alertq *q = container_of(check, typeof(*q), check);
 	int ret = 0;
 

src/hlua.c
@@ -2684,8 +2684,7 @@ __LJMP static int hlua_socket_connect_yield(struct lua_State *L, int status, lua
 		return 2;
 	}
 
-	appctx = cs_appctx(s->csf);
-	ALREADY_CHECKED(appctx);
+	appctx = __cs_appctx(s->csf);
 
 	/* Check for connection established. */
 	if (appctx->ctx.hlua_cosocket.connected) {

src/http_ana.c
@@ -3878,12 +3878,10 @@ static int http_handle_stats(struct stream *s, struct channel *req)
 	struct http_msg *msg = &txn->req;
 	struct uri_auth *uri_auth = s->be->uri_auth;
 	const char *h, *lookup, *end;
-	struct appctx *appctx;
+	struct appctx *appctx = __cs_appctx(s->csb);
 	struct htx *htx;
 	struct htx_sl *sl;
 
-	appctx = cs_appctx(s->csb);
-	ALREADY_CHECKED(appctx);
 	memset(&appctx->ctx.stats, 0, sizeof(appctx->ctx.stats));
 	appctx->st1 = appctx->st2 = 0;
 	appctx->ctx.stats.st_code = STAT_STATUS_INIT;
@@ -5004,7 +5002,7 @@ static void http_debug_stline(const char *dir, struct stream *s, const struct ht
         chunk_printf(&trash, "%08x:%s.%s[%04x:%04x]: ", s->uniq_id, s->be->id,
                      dir,
                      objt_conn(sess->origin) ? (unsigned short)__objt_conn(sess->origin)->handle.fd : -1,
-                     cs_conn(s->csb) ? (unsigned short)(cs_conn(s->csb))->handle.fd : -1);
+                     cs_conn(s->csb) ? (unsigned short)(__cs_conn(s->csb))->handle.fd : -1);
 
         max = HTX_SL_P1_LEN(sl);
         UBOUND(max, trash.size - trash.data - 3);
@@ -5035,7 +5033,7 @@ static void http_debug_hdr(const char *dir, struct stream *s, const struct ist n
         chunk_printf(&trash, "%08x:%s.%s[%04x:%04x]: ", s->uniq_id, s->be->id,
                      dir,
                      objt_conn(sess->origin) ? (unsigned short)__objt_conn(sess->origin)->handle.fd : -1,
-                     cs_conn(s->csb) ? (unsigned short)(cs_conn(s->csb))->handle.fd : -1);
+                     cs_conn(s->csb) ? (unsigned short)(__cs_conn(s->csb))->handle.fd : -1);
 
         max = n.len;
         UBOUND(max, trash.size - trash.data - 3);

src/mux_fcgi.c
@@ -3314,9 +3314,9 @@ static void fcgi_strm_capture_bad_message(struct fcgi_conn *fconn, struct fcgi_s
 
 	if (fstrm->cs && cs_strm(fstrm->cs)) {
 		if (sess == NULL)
-			sess = cs_strm(fstrm->cs)->sess;
+			sess = __cs_strm(fstrm->cs)->sess;
 		if (!(h1m->flags & H1_MF_RESP))
-			other_end = cs_strm(fstrm->cs)->be;
+			other_end = __cs_strm(fstrm->cs)->be;
 		else
 			other_end = sess->fe;
 	} else

src/mux_h1.c
@@ -1353,9 +1353,9 @@ static void h1_capture_bad_message(struct h1c *h1c, struct h1s *h1s,
 
 	if ((h1c->flags & H1C_F_ST_ATTACHED) && cs_strm(h1s->cs)) {
 		if (sess == NULL)
-			sess = cs_strm(h1s->cs)->sess;
+			sess = __cs_strm(h1s->cs)->sess;
 		if (!(h1m->flags & H1_MF_RESP))
-			other_end = cs_strm(h1s->cs)->be;
+			other_end = __cs_strm(h1s->cs)->be;
 		else
 			other_end = sess->fe;
 	} else

src/mux_pt.c
@@ -406,7 +406,7 @@ static void mux_pt_destroy_meth(void *ctx)
  */
 static void mux_pt_detach(struct conn_stream *cs)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	struct mux_pt_ctx *ctx;
 
 	ALREADY_CHECKED(conn);
@@ -444,7 +444,7 @@ static int mux_pt_avail_streams(struct connection *conn)
 
 static void mux_pt_shutr(struct conn_stream *cs, enum cs_shr_mode mode)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 
 	TRACE_ENTER(PT_EV_STRM_SHUT, conn, cs);
 
@@ -464,7 +464,7 @@ static void mux_pt_shutr(struct conn_stream *cs, enum cs_shr_mode mode)
 
 static void mux_pt_shutw(struct conn_stream *cs, enum cs_shw_mode mode)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 
 	TRACE_ENTER(PT_EV_STRM_SHUT, conn, cs);
 
@@ -496,7 +496,7 @@ static void mux_pt_shutw(struct conn_stream *cs, enum cs_shw_mode mode)
  */
 static size_t mux_pt_rcv_buf(struct conn_stream *cs, struct buffer *buf, size_t count, int flags)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	size_t ret = 0;
 
 	TRACE_ENTER(PT_EV_RX_DATA, conn, cs, buf, (size_t[]){count});
@@ -525,7 +525,7 @@ static size_t mux_pt_rcv_buf(struct conn_stream *cs, struct buffer *buf, size_t 
 /* Called from the upper layer, to send data */
 static size_t mux_pt_snd_buf(struct conn_stream *cs, struct buffer *buf, size_t count, int flags)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	size_t ret;
 
 	TRACE_ENTER(PT_EV_TX_DATA, conn, cs, buf, (size_t[]){count});
@@ -546,7 +546,7 @@ static size_t mux_pt_snd_buf(struct conn_stream *cs, struct buffer *buf, size_t 
  */
 static int mux_pt_subscribe(struct conn_stream *cs, int event_type, struct wait_event *es)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 
 	TRACE_POINT(PT_EV_RX_DATA|PT_EV_TX_DATA, conn, cs, 0, (size_t[]){event_type});
 	return conn->xprt->subscribe(conn, conn->xprt_ctx, event_type, es);
@@ -558,7 +558,7 @@ static int mux_pt_subscribe(struct conn_stream *cs, int event_type, struct wait_
  */
 static int mux_pt_unsubscribe(struct conn_stream *cs, int event_type, struct wait_event *es)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 
 	TRACE_POINT(PT_EV_RX_DATA|PT_EV_TX_DATA, conn, cs, 0, (size_t[]){event_type});
 	return conn->xprt->unsubscribe(conn, conn->xprt_ctx, event_type, es);
@@ -568,7 +568,7 @@ static int mux_pt_unsubscribe(struct conn_stream *cs, int event_type, struct wai
 /* Send and get, using splicing */
 static int mux_pt_rcv_pipe(struct conn_stream *cs, struct pipe *pipe, unsigned int count)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	int ret;
 
 	TRACE_ENTER(PT_EV_RX_DATA, conn, cs, 0, (size_t[]){count});
@@ -589,7 +589,7 @@ static int mux_pt_rcv_pipe(struct conn_stream *cs, struct pipe *pipe, unsigned i
 
 static int mux_pt_snd_pipe(struct conn_stream *cs, struct pipe *pipe)
 {
-	struct connection *conn = cs_conn(cs);
+	struct connection *conn = __cs_conn(cs);
 	int ret;
 
 	TRACE_ENTER(PT_EV_TX_DATA, conn, cs, 0, (size_t[]){pipe->data});

commit 8bc1759f609ef4e34d3b303b3c72517b4f4b2f4a

DEBUG: stream-int: Fix BUG_ON used to test appctx in si_applet_ops callbacks
693b23bb1 ("MEDIUM: tree-wide: Use unsafe conn-stream API when it is
relevant") introduced a regression in DEBUG_STRICT mode because some BUG_ON
conditions were inverted. It should ok now.

In addition, ALREADY_CHECKED macro was removed from appctx_wakeup() function
because it is useless now.

include/haproxy/applet.h
@@ -113,8 +113,6 @@ static inline void appctx_free(struct appctx *appctx)
 /* wakes up an applet when conditions have changed */
 static inline void appctx_wakeup(struct appctx *appctx)
 {
-	ALREADY_CHECKED(appctx);
-
 	task_wakeup(appctx->t, TASK_WOKEN_OTHER);
 }
 

src/stream_interface.c
@@ -1656,7 +1656,7 @@ void si_applet_wake_cb(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 
-	BUG_ON(cs_appctx(si->cs));
+	BUG_ON(!cs_appctx(si->cs));
 
 	/* If the applet wants to write and the channel is closed, it's a
 	 * broken pipe and it must be reported.
@@ -1696,7 +1696,7 @@ static void stream_int_shutr_applet(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 
-	BUG_ON(cs_appctx(si->cs));
+	BUG_ON(!cs_appctx(si->cs));
 
 	si_rx_shut_blk(si);
 	if (ic->flags & CF_SHUTR)
@@ -1732,7 +1732,7 @@ static void stream_int_shutw_applet(struct stream_interface *si)
 	struct channel *ic = si_ic(si);
 	struct channel *oc = si_oc(si);
 
-	BUG_ON(cs_appctx(si->cs));
+	BUG_ON(!cs_appctx(si->cs));
 
 	oc->flags &= ~CF_SHUTW_NOW;
 	if (oc->flags & CF_SHUTW)
@@ -1785,7 +1785,7 @@ static void stream_int_chk_rcv_applet(struct stream_interface *si)
 {
 	struct channel *ic = si_ic(si);
 
-	BUG_ON(cs_appctx(si->cs));
+	BUG_ON(!cs_appctx(si->cs));
 
 	DPRINTF(stderr, "%s: si=%p, si->state=%d ic->flags=%08x oc->flags=%08x\n",
 		__FUNCTION__,
@@ -1802,7 +1802,7 @@ static void stream_int_chk_snd_applet(struct stream_interface *si)
 {
 	struct channel *oc = si_oc(si);
 
-	BUG_ON(cs_appctx(si->cs));
+	BUG_ON(!cs_appctx(si->cs));
 
 	DPRINTF(stderr, "%s: si=%p, si->state=%d ic->flags=%08x oc->flags=%08x\n",
 		__FUNCTION__,