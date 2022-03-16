haproxy/haproxy

commit 71e31583957895c6601fbad352292497176b7fa3

BUG/MINOR: httpclient: send the SNI using the host header
Generate an SNI expression which uses the Host header of the request.
This is mandatory for most of the SSL servers nowadays.

Must be backported in 2.5 with the previous patch which export
server_parse_sni_expr().

src/http_client.c
@@ -1061,6 +1061,8 @@ static int httpclient_cfg_postparser()
 {
 	struct logsrv *logsrv;
 	struct proxy *curproxy = httpclient_proxy;
+	int err_code = 0;
+	char *errmsg = NULL;
 
 	/* copy logs from "global" log list */
 	list_for_each_entry(logsrv, &global.logsrvs, list) {
@@ -1089,6 +1091,19 @@ static int httpclient_cfg_postparser()
 		curproxy->conf.args.file = NULL;
 		curproxy->conf.args.line = 0;
 	}
+
+#ifdef USE_OPENSSL
+	/* init the SNI expression */
+	/* always use the host header as SNI, without the port */
+	httpclient_srv_ssl->sni_expr = strdup("req.hdr(host),field(1,:)");
+	err_code |= server_parse_sni_expr(httpclient_srv_ssl, httpclient_proxy, &errmsg);
+	if (err_code & ERR_CODE) {
+		ha_alert("httpclient: failed to configure sni: %s.\n", errmsg);
+		free(errmsg);
+		goto err;
+	}
+#endif
+
 	return 0;
 err:
 	return 1;

commit 715c101a196187a4f4c9a60093e30b1a1a434483

BUILD: httpclient: fix build without SSL
src/http_client.c: In function ‘httpclient_cfg_postparser’:
src/http_client.c:1065:8: error: unused variable ‘errmsg’ [-Werror=unused-variable]
 1065 |  char *errmsg = NULL;
      |        ^~~~~~
src/http_client.c:1064:6: error: unused variable ‘err_code’ [-Werror=unused-variable]
 1064 |  int err_code = 0;
      |      ^~~~~~~~

Fix the build of the httpclient without SSL, the problem was introduced
with previous patch 71e3158 ("BUG/MINOR: httpclient: send the SNI using
the host header")

Must be backported in 2.5 as well.

src/http_client.c
@@ -1061,7 +1061,6 @@ static int httpclient_cfg_postparser()
 {
 	struct logsrv *logsrv;
 	struct proxy *curproxy = httpclient_proxy;
-	int err_code = 0;
 	char *errmsg = NULL;
 
 	/* copy logs from "global" log list */
@@ -1078,14 +1077,12 @@ static int httpclient_cfg_postparser()
 		LIST_APPEND(&curproxy->logsrvs, &node->list);
 	}
 	if (curproxy->conf.logformat_string) {
-		char *err = NULL;
-
 		curproxy->conf.args.ctx = ARGC_LOG;
 		if (!parse_logformat_string(curproxy->conf.logformat_string, curproxy, &curproxy->logformat,
 					    LOG_OPT_MANDATORY|LOG_OPT_MERGE_SPACES,
-					    SMP_VAL_FE_LOG_END, &err)) {
-			ha_alert("httpclient: failed to parse log-format : %s.\n", err);
-			free(err);
+					    SMP_VAL_FE_LOG_END, &errmsg)) {
+			ha_alert("httpclient: failed to parse log-format : %s.\n", errmsg);
+			free(errmsg);
 			goto err;
 		}
 		curproxy->conf.args.file = NULL;
@@ -1093,14 +1090,18 @@ static int httpclient_cfg_postparser()
 	}
 
 #ifdef USE_OPENSSL
-	/* init the SNI expression */
-	/* always use the host header as SNI, without the port */
-	httpclient_srv_ssl->sni_expr = strdup("req.hdr(host),field(1,:)");
-	err_code |= server_parse_sni_expr(httpclient_srv_ssl, httpclient_proxy, &errmsg);
-	if (err_code & ERR_CODE) {
-		ha_alert("httpclient: failed to configure sni: %s.\n", errmsg);
-		free(errmsg);
-		goto err;
+	{
+		int err_code = 0;
+
+		/* init the SNI expression */
+		/* always use the host header as SNI, without the port */
+		httpclient_srv_ssl->sni_expr = strdup("req.hdr(host),field(1,:)");
+		err_code |= server_parse_sni_expr(httpclient_srv_ssl, httpclient_proxy, &errmsg);
+		if (err_code & ERR_CODE) {
+			ha_alert("httpclient: failed to configure sni: %s.\n", errmsg);
+			free(errmsg);
+			goto err;
+		}
 	}
 #endif
 