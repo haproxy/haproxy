haproxy/haproxy

commit cda94accb1f0e2615b36688a7271829cb3a3be33

MAJOR: stream/conn_stream: Move the stream-interface into the conn-stream
Thanks to all previous changes, it is now possible to move the
stream-interface into the conn-stream. To do so, some SI functions are
removed and their conn-stream counterparts are added. In addition, the
conn-stream is now responsible to create and release the
stream-interface. While the stream-interfaces were inlined in the stream
structure, there is now a pointer in the conn-stream. stream-interfaces are
now dynamically allocated. Thus a dedicated pool is added. It is a temporary
change because, at the end, the stream-interface structure will most
probably disappear.

src/mux_h1.c
@@ -682,11 +682,12 @@ static struct conn_stream *h1s_new_cs(struct h1s *h1s, struct buffer *input)
 	struct conn_stream *cs;
 
 	TRACE_ENTER(H1_EV_STRM_NEW, h1c->conn, h1s);
-	cs = cs_new(&h1c->conn->obj_type, h1s, NULL, NULL, NULL);
+	cs = cs_new();
 	if (!cs) {
 		TRACE_ERROR("CS allocation failure", H1_EV_STRM_NEW|H1_EV_STRM_END|H1_EV_STRM_ERR, h1c->conn, h1s);
 		goto err;
 	}
+	cs_attach_endp(cs, &h1c->conn->obj_type, h1s);
 	h1s->cs = cs;
 
 	if (h1s->flags & H1S_F_NOT_FIRST)

include/haproxy/channel.h
@@ -68,18 +68,18 @@ static inline struct stream *chn_strm(const struct channel *chn)
 static inline struct stream_interface *chn_prod(const struct channel *chn)
 {
 	if (chn->flags & CF_ISRESP)
-		return &LIST_ELEM(chn, struct stream *, res)->si[1];
+		return LIST_ELEM(chn, struct stream *, res)->csb->si;
 	else
-		return &LIST_ELEM(chn, struct stream *, req)->si[0];
+		return LIST_ELEM(chn, struct stream *, req)->csf->si;
 }
 
 /* returns a pointer to the stream interface consuming the channel (producer) */
 static inline struct stream_interface *chn_cons(const struct channel *chn)
 {
 	if (chn->flags & CF_ISRESP)
-		return &LIST_ELEM(chn, struct stream *, res)->si[0];
+		return LIST_ELEM(chn, struct stream *, res)->csf->si;
 	else
-		return &LIST_ELEM(chn, struct stream *, req)->si[1];
+		return LIST_ELEM(chn, struct stream *, req)->csb->si;
 }
 
 /* c_orig() : returns the pointer to the channel buffer's origin */

include/haproxy/conn_stream-t.h
@@ -25,6 +25,8 @@
 
 #include <haproxy/obj_type-t.h>
 
+struct stream_interface;
+
 /* conn_stream flags */
 enum {
 	CS_FL_NONE          = 0x00000000,  /* Just for initialization purposes */
@@ -92,7 +94,7 @@ struct conn_stream {
 	unsigned int flags;                  /* CS_FL_* */
 	enum obj_type *end;                  /* points to the end point (connection or appctx) */
 	enum obj_type *app;                  /* points to the applicative point (stream or check) */
-	void *data;                          /* pointer to upper layer's entity (eg: stream interface) */
+	struct stream_interface *si;
 	const struct data_cb *data_cb;       /* data layer callbacks. Must be set before xprt->init() */
 	void *ctx;                           /* mux-specific context */
 };

include/haproxy/conn_stream.h
@@ -29,13 +29,16 @@
 #include <haproxy/obj_type.h>
 
 struct stream;
+struct stream_interface;
 struct check;
 
 #define IS_HTX_CS(cs)     (cs_conn(cs) && IS_HTX_CONN(cs_conn(cs)))
 
-struct conn_stream *cs_new(enum obj_type *endp, void *ctx, enum obj_type *app, void *data, const struct data_cb *data_cb);
+struct conn_stream *cs_new();
 void cs_free(struct conn_stream *cs);
-
+void cs_attach_endp(struct conn_stream *cs, enum obj_type *endp, void *ctx);
+int cs_attach_app(struct conn_stream *cs, enum obj_type *app);
+void cs_detach_endp(struct conn_stream *cs);
 
 /*
  * Initializes all required fields for a new conn_strema.
@@ -47,7 +50,7 @@ static inline void cs_init(struct conn_stream *cs)
 	cs->end = NULL;
 	cs->app = NULL;
 	cs->ctx = NULL;
-	cs->data = NULL;
+	cs->si = NULL;
 	cs->data_cb = NULL;
 }
 
@@ -77,11 +80,6 @@ static inline struct appctx *cs_appctx(const struct conn_stream *cs)
 	return (cs ? objt_appctx(cs->end) : NULL);
 }
 
-static inline struct stream_interface *cs_si(const struct conn_stream *cs)
-{
-	return (cs ? cs->data : NULL);
-}
-
 static inline struct stream *cs_strm(const struct conn_stream *cs)
 {
 	return (cs ? objt_stream(cs->app) : NULL);
@@ -92,57 +90,9 @@ static inline struct check *cs_check(const struct conn_stream *cs)
 	return (cs ? objt_check(cs->app) : NULL);
 }
 
-/* Attaches a conn_stream to an endpoint and sets the endpoint ctx */
-static inline void cs_attach_endp(struct conn_stream *cs, enum obj_type *endp, void *ctx)
+static inline struct stream_interface *cs_si(const struct conn_stream *cs)
 {
-	cs->end = endp;
-	cs->ctx = ctx;
-}
-
-/* Attaches a conn_stream to a app layer and sets the relevant callbacks */
-static inline void cs_attach_app(struct conn_stream *cs, enum obj_type *app, void *data, const struct data_cb *data_cb)
-{
-	cs->app = app;
-	cs->data = data;
-	cs->data_cb = data_cb;
-}
-
-/* Detach the conn_stream from the endpoint, if any. For a connecrion, if a mux
- * owns the connection ->detach() callback is called. Otherwise, it means the
- * conn-stream owns the connection. In this case the connection is closed and
- * released. For an applet, the appctx is released. At the end, the conn-stream
- * is not released but some fields a reset.
- */
-static inline void cs_detach_endp(struct conn_stream *cs)
-{
-	struct connection *conn;
-	struct appctx *appctx;
-
-	if ((conn = cs_conn(cs))) {
-		if (conn->mux)
-			conn->mux->detach(cs);
-		else {
-			/* It's too early to have a mux, let's just destroy
-			 * the connection
-			 */
-			conn_stop_tracking(conn);
-			conn_full_close(conn);
-			if (conn->destroy_cb)
-				conn->destroy_cb(conn);
-			conn_free(conn);
-		}
-	}
-	else if ((appctx = cs_appctx(cs))) {
-		if (appctx->applet->release)
-			appctx->applet->release(appctx);
-		appctx_free(appctx);
-	}
-
-	/* Rest CS */
-	cs->flags = CS_FL_NONE;
-	cs->end = NULL;
-	cs->ctx = NULL;
-	cs->data_cb = NULL;
+	return (cs_strm(cs) ? cs->si : NULL);
 }
 
 /* Release a conn_stream */

include/haproxy/stream-t.h
@@ -167,7 +167,6 @@ struct stream {
 	struct conn_stream *csf;                /* frontend conn-stream */
 	struct conn_stream *csb;                /* backend conn-stream */
 
-	struct stream_interface si[2];          /* client and server stream interfaces */
 	struct strm_logs logs;                  /* logs for this stream */
 
 	void (*do_log)(struct stream *s);       /* the function to call in order to log (or NULL) */

include/haproxy/stream_interface.h
@@ -33,6 +33,10 @@ extern struct si_ops si_embedded_ops;
 extern struct si_ops si_conn_ops;
 extern struct si_ops si_applet_ops;
 extern struct data_cb si_conn_cb;
+extern struct data_cb check_conn_cb;
+
+struct stream_interface *si_new(struct conn_stream *cs);
+void si_free(struct stream_interface *si);
 
 /* main event functions used to move data between sockets and buffers */
 int si_check_timeouts(struct stream_interface *si);
@@ -87,7 +91,7 @@ static inline struct task *si_task(struct stream_interface *si)
 /* returns the stream interface on the other side. Used during forwarding. */
 static inline struct stream_interface *si_opposite(struct stream_interface *si)
 {
-	return ((si->flags & SI_FL_ISBACK) ? &(cs_strm(si->cs)->si[0]) : &(cs_strm(si->cs)->si[1]));
+	return ((si->flags & SI_FL_ISBACK) ? cs_strm(si->cs)->csf->si : cs_strm(si->cs)->csb->si);
 }
 
 /* initializes a stream interface in the SI_ST_INI state. It's detached from
@@ -105,6 +109,7 @@ static inline int si_reset(struct stream_interface *si)
 	si->cs             = NULL;
 	si->state          = si->prev_state = SI_ST_INI;
 	si->ops            = &si_embedded_ops;
+	si->l7_buffer      = BUF_NULL;
 	si->wait_event.tasklet = tasklet_new();
 	if (!si->wait_event.tasklet)
 		return -1;
@@ -135,81 +140,6 @@ static inline int si_state_in(enum si_state state, enum si_state_bit mask)
 {
 	BUG_ON(mask & ~SI_SB_ALL);
 	return !!(si_state_bit(state) & mask);
-}
-
-/* Reset the endpoint detaching it from the conn-stream. For a connection
- * attached to a mux, it is unsubscribe from any event.
- */
-static inline void si_reset_endpoint(struct stream_interface *si)
-{
-	if (!si->cs)
-		return;
-
-	if (cs_conn_mux(si->cs) && si->wait_event.events != 0)
-		(cs_conn_mux(si->cs))->unsubscribe(si->cs, si->wait_event.events, &si->wait_event);
-
-	cs_detach_endp(si->cs);
-	si->ops = &si_embedded_ops;
-}
-
-/* Release the endpoint if it's a connection or an applet, then nullify it.
- * Note: released connections are closed then freed.
- */
-static inline void si_release_endpoint(struct stream_interface *si)
-{
-	if (!si->cs)
-		return;
-	si_reset_endpoint(si);
-	cs_free(si->cs);
-	si->cs = NULL;
-	si->ops = &si_embedded_ops;
-
-}
-
-/* Attach conn_stream <cs> to the stream interface <si>. */
-static inline void si_attach_cs(struct stream_interface *si, struct conn_stream *cs)
-{
-	si->cs = cs;
-	if (cs_conn(cs)) {
-		si->ops = &si_conn_ops;
-		cs_attach_app(cs, &si_strm(si)->obj_type, si, &si_conn_cb);
-	}
-	else if (cs_appctx(cs)) {
-		struct appctx *appctx = cs_appctx(cs);
-
-		si->ops = &si_applet_ops;
-		appctx->owner = cs;
-		cs_attach_app(cs, &si_strm(si)->obj_type, si, NULL);
-	}
-	else {
-		si->ops = &si_embedded_ops;
-		cs_attach_app(cs, &si_strm(si)->obj_type, si, NULL);
-	}
-}
-
-/* Attach connection <conn> to the stream interface <si>. The stream interface
- * is configured to work with a connection context.
- */
-static inline void si_attach_conn(struct stream_interface *si, struct connection *conn)
-{
-	si_reset_endpoint(si);
-	if (!conn->ctx)
-		conn->ctx = si->cs;
-	si->ops = &si_conn_ops;
-	cs_attach_endp(si->cs, &conn->obj_type, conn);
-	cs_attach_app(si->cs, &si_strm(si)->obj_type, si, &si_conn_cb);
-}
-
-/* Attach appctx <appctx> to the stream interface <si>. The stream interface
- * is configured to work with an applet context.
- */
-static inline void si_attach_appctx(struct stream_interface *si, struct appctx *appctx)
-{
-	si_reset_endpoint(si);
-	appctx->owner = si->cs;
-	si->ops = &si_applet_ops;
-	cs_attach_endp(si->cs, &appctx->obj_type, appctx);
-	cs_attach_app(si->cs, &si_strm(si)->obj_type, si, NULL);
 }
 
 /* call the applet's release function if any. Needs to be called upon close() */

src/backend.c
@@ -1495,9 +1495,9 @@ int connect_server(struct stream *s)
 			}
 
 			if (avail >= 1) {
-				si_attach_conn(cs_si(s->csb), srv_conn);
+				cs_attach_endp(s->csb, &srv_conn->obj_type, srv_conn);
 				if (srv_conn->mux->attach(srv_conn, s->csb, s->sess) == -1) {
-					si_reset_endpoint(cs_si(s->csb));
+					cs_detach_endp(s->csb);
 					srv_conn = NULL;
 				}
 			}
@@ -1571,7 +1571,7 @@ skip_reuse:
 			return SF_ERR_INTERNAL;  /* how did we get there ? */
 		}
 
-		si_attach_conn(cs_si(s->csb), srv_conn);
+		cs_attach_endp(s->csb, &srv_conn->obj_type, srv_conn);
 #if defined(USE_OPENSSL) && defined(TLSEXT_TYPE_application_layer_protocol_negotiation)
 		if (!srv ||
 		    (srv->use_ssl != 1 || (!(srv->ssl_ctx.alpn_str) && !(srv->ssl_ctx.npn_str)) ||
@@ -2289,7 +2289,7 @@ void back_handle_st_cer(struct stream *s)
 	 * Note: the stream-interface will be switched to ST_REQ, ST_ASS or
 	 * ST_TAR and SI_FL_ERR and SI_FL_EXP flags will be unset.
 	 */
-	si_reset_endpoint(cs_si(s->csb));
+	cs_detach_endp(s->csb);
 
 	stream_choose_redispatch(s);
 

src/cli.c
@@ -2715,7 +2715,7 @@ int pcli_wait_for_response(struct stream *s, struct channel *rep, int an_bit)
 		 * connection.
 		 */
 		if (!si_conn_ready(cs_si(s->csb))) {
-			si_reset_endpoint(cs_si(s->csb));
+			cs_detach_endp(s->csb);
 			s->srv_conn = NULL;
 		}
 

commit 91449b0351d54345420d2fbffb0af0a641417473

BUG/MINOR: mux-h1: Don't release unallocated CS on error path
cs_free() must not be called when we fail to allocate the conn-stream in
h1s_new_cs() function. This bug was introduced by the commit cda94accb
("MAJOR: stream/conn_stream: Move the stream-interface into the
conn-stream").

It is 2.6-specific, no backport is needed.

src/mux_h1.c
@@ -735,7 +735,7 @@ static struct conn_stream *h1s_new_cs(struct h1s *h1s, struct buffer *input)
 
 	if (!stream_new(h1c->conn->owner, cs, input)) {
 		TRACE_DEVEL("leaving on stream creation failure", H1_EV_STRM_NEW|H1_EV_STRM_END|H1_EV_STRM_ERR, h1c->conn, h1s);
-		goto err;
+		goto err_cs;
 	}
 
 	HA_ATOMIC_INC(&h1c->px_counters->open_streams);
@@ -745,8 +745,9 @@ static struct conn_stream *h1s_new_cs(struct h1s *h1s, struct buffer *input)
 	TRACE_LEAVE(H1_EV_STRM_NEW, h1c->conn, h1s);
 	return cs;
 
-  err:
+  err_cs:
 	cs_free(cs);
+  err:
 	h1s->cs = NULL;
 	TRACE_DEVEL("leaving on error", H1_EV_STRM_NEW|H1_EV_STRM_ERR, h1c->conn, h1s);
 	return NULL;