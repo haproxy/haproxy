c694471b2149e97e4f49c1742ffb17cbdb330ac4
Willy Tarreau <w@1wt.eu>
2022-05-20 15:52:31 +0200

    MINOR: listener: add a new "options" entry in bind_conf
    There is no way to store useful info there, yet there's about one entry
    per boolean. Let's add an "options" attribute which will collect various
    options.
    
    In practice, even the BC_O_SSL_* flags and a few info such as strict_sni
    could move there.

--- include/haproxy/listener-t.h
+++ include/haproxy/listener-t.h
@@ -176,6 +176,7 @@ struct bind_conf {
 	struct proxy *frontend;    /* the frontend all these listeners belong to, or NULL */
 	const struct mux_proto_list *mux_proto; /* the mux to use for all incoming connections (specified by the "proto" keyword) */
 	struct xprt_ops *xprt;     /* transport-layer operations for all listeners */
+	uint options;              /* set of BC_O_* flags */
 	int is_ssl;                /* SSL is required for these listeners */
 	int generate_certs;        /* 1 if generate-certificates option is set, else 0 */
 	int level;                 /* stats access level (ACCESS_LVL_*) */
