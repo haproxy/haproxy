haproxytech/quic-dev

MINOR: quic: QUIC transport parameters split.
Make the transport parameters be standlone as much as possible as
it consists only in encoding/decoding data into/from buffers.
Reduce the size of xprt_quic.h. Unfortunalety, I think we will
have to continue to include <xprt_quic-t.h> to use the trace API
into this module.

Makefile
@@ -637,7 +637,7 @@ OPTIONS_OBJS += src/quic_sock.o src/proto_quic.o src/xprt_quic.o src/quic_tls.o 
                 src/quic_frame.o src/quic_cc.o src/quic_cc_newreno.o src/mux_quic.o \
                 src/cbuf.o src/qpack-dec.o src/qpack-tbl.o src/h3.o src/qpack-enc.o \
                 src/hq_interop.o src/cfgparse-quic.o src/quic_loss.o \
-                src/quic_stream.o src/quic_stats.o
+                src/quic_transp_params.o src/quic_stream.o src/quic_stats.o
 endif
 
 ifneq ($(USE_LUA),)

include/haproxy/xprt_quic-t.h
@@ -35,8 +35,9 @@
 #include <haproxy/mux_quic-t.h>
 #include <haproxy/quic_cc-t.h>
 #include <haproxy/quic_frame-t.h>
-#include <haproxy/quic_tls-t.h>
 #include <haproxy/quic_loss-t.h>
+#include <haproxy/quic_tls-t.h>
+#include <haproxy/quic_transp_params-t.h>
 #include <haproxy/task.h>
 
 #include <import/ebtree-t.h>
@@ -49,9 +50,6 @@ typedef unsigned long long ull;
 
 #define QUIC_INITIAL_IPV4_MTU      1252 /* (bytes) */
 #define QUIC_INITIAL_IPV6_MTU      1232
-/* XXX TO DO XXX */
-/* Maximum packet length during handshake */
-#define QUIC_PACKET_MAXLEN     2048
 
 /* The minimum length of Initial packets. */
 #define QUIC_INITIAL_PACKET_MINLEN 1200
@@ -194,7 +192,6 @@ enum quic_pkt_type {
  */
 #define QUIC_CONN_MAX_PACKET  64
 
-#define QUIC_STATELESS_RESET_TOKEN_LEN 16
 #define QUIC_STATELESS_RESET_PACKET_HEADER_LEN 5
 #define QUIC_STATELESS_RESET_PACKET_MINLEN     (22 + QUIC_HAP_CID_LEN)
 
@@ -298,88 +295,6 @@ struct quic_connection_id {
 	struct quic_conn *qc;  /* QUIC connection using this CID */
 };
 
-struct preferred_address {
-	uint16_t ipv4_port;
-	uint16_t ipv6_port;
-	uint8_t ipv4_addr[4];
-	uint8_t ipv6_addr[16];
-	struct quic_cid cid;
-	uint8_t stateless_reset_token[QUIC_STATELESS_RESET_TOKEN_LEN];
-};
-
-/* Default values for the absent transport parameters */
-#define QUIC_DFLT_MAX_UDP_PAYLOAD_SIZE   65527 /* bytes */
-#define QUIC_DFLT_ACK_DELAY_COMPONENT        3 /* milliseconds */
-#define QUIC_DFLT_MAX_ACK_DELAY             25 /* milliseconds */
-#define QUIC_ACTIVE_CONNECTION_ID_LIMIT      2 /* number of connections */
-
-/* Types of QUIC transport parameters */
-#define QUIC_TP_ORIGINAL_DESTINATION_CONNECTION_ID   0
-#define QUIC_TP_MAX_IDLE_TIMEOUT                     1
-#define QUIC_TP_STATELESS_RESET_TOKEN                2
-#define QUIC_TP_MAX_UDP_PAYLOAD_SIZE                 3
-#define QUIC_TP_INITIAL_MAX_DATA                     4
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_BIDI_LOCAL   5
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_BIDI_REMOTE  6
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_UNI          7
-#define QUIC_TP_INITIAL_MAX_STREAMS_BIDI             8
-#define QUIC_TP_INITIAL_MAX_STREAMS_UNI              9
-#define QUIC_TP_ACK_DELAY_EXPONENT                  10
-#define QUIC_TP_MAX_ACK_DELAY                       11
-#define QUIC_TP_DISABLE_ACTIVE_MIGRATION            12
-#define QUIC_TP_PREFERRED_ADDRESS                   13
-#define QUIC_TP_ACTIVE_CONNECTION_ID_LIMIT          14
-#define QUIC_TP_INITIAL_SOURCE_CONNECTION_ID        15
-#define QUIC_TP_RETRY_SOURCE_CONNECTION_ID          16
-
-/*
- * These defines are not for transport parameter type, but the maximum accepted value for
- * transport parameter types.
- */
-#define QUIC_TP_ACK_DELAY_EXPONENT_LIMIT 20
-#define QUIC_TP_MAX_ACK_DELAY_LIMIT      (1UL << 14)
-
-/* The maximum length of encoded transport parameters for any QUIC peer. */
-#define QUIC_TP_MAX_ENCLEN    128
-/*
- * QUIC transport parameters.
- * Note that forbidden parameters sent by clients MUST generate TRANSPORT_PARAMETER_ERROR errors.
- */
-struct quic_transport_params {
-	uint64_t max_idle_timeout;
-	uint64_t max_udp_payload_size;                 /* Default: 65527 bytes (max of UDP payload for IPv6) */
-	uint64_t initial_max_data;
-	uint64_t initial_max_stream_data_bidi_local;
-	uint64_t initial_max_stream_data_bidi_remote;
-	uint64_t initial_max_stream_data_uni;
-	uint64_t initial_max_streams_bidi;
-	uint64_t initial_max_streams_uni;
-	uint64_t ack_delay_exponent;                   /* Default: 3, max: 20 */
-	uint64_t max_ack_delay;                        /* Default: 3ms, max: 2^14ms*/
-	uint64_t active_connection_id_limit;
-
-	/* Booleans */
-	uint8_t disable_active_migration;
-	uint8_t with_stateless_reset_token;
-	uint8_t with_preferred_address;
-	uint8_t original_destination_connection_id_present;
-	uint8_t initial_source_connection_id_present;
-
-	uint8_t stateless_reset_token[QUIC_STATELESS_RESET_TOKEN_LEN]; /* Forbidden for clients */
-	/*
-	 * MUST be sent by servers.
-	 * When received by clients, must be set to 1 if present.
-	 */
-	struct quic_cid original_destination_connection_id;            /* Forbidden for clients */
-	/*
-	 * MUST be sent by servers after Retry.
-	 */
-	struct quic_cid retry_source_connection_id;                    /* Forbidden for clients */
-	/* MUST be present both for servers and clients. */
-	struct quic_cid initial_source_connection_id;
-	struct preferred_address preferred_address;                    /* Forbidden for clients */
-};
-
 /* Structure to hold a range of ACKs sent in ACK frames. */
 struct quic_arng {
 	int64_t first;
@@ -466,9 +381,6 @@ struct quic_dgram {
 
 /* The QUIC packet numbers are 62-bits integers */
 #define QUIC_MAX_PACKET_NUM      ((1ULL << 62) - 1)
-
-/* Default QUIC connection transport parameters */
-extern struct quic_transport_params quic_dflt_transport_params;
 
 /* Maximum number of ack-eliciting received packets since the last
  * ACK frame was sent

src/listener.c
@@ -31,12 +31,12 @@
 #include <haproxy/log.h>
 #include <haproxy/protocol.h>
 #include <haproxy/proxy.h>
+#include <haproxy/quic_transp_params.h>
 #include <haproxy/sample.h>
 #include <haproxy/stream.h>
 #include <haproxy/task.h>
 #include <haproxy/ticks.h>
 #include <haproxy/tools.h>
-#include <haproxy/xprt_quic.h>
 
 
 /* List head of all known bind keywords */

src/mux_quic.c
@@ -10,6 +10,7 @@
 #include <haproxy/ncbuf.h>
 #include <haproxy/pool.h>
 #include <haproxy/quic_stream.h>
+#include <haproxy/quic_transp_params-t.h>
 #include <haproxy/ssl_sock-t.h>
 #include <haproxy/stconn.h>
 #include <haproxy/trace.h>

src/proxy.c
@@ -40,6 +40,7 @@
 #include <haproxy/proto_tcp.h>
 #include <haproxy/proxy.h>
 #include <haproxy/sc_strm.h>
+#include <haproxy/quic_transp_params.h>
 #include <haproxy/server-t.h>
 #include <haproxy/signal.h>
 #include <haproxy/stats-t.h>
@@ -49,7 +50,6 @@
 #include <haproxy/tcpcheck.h>
 #include <haproxy/time.h>
 #include <haproxy/tools.h>
-#include <haproxy/xprt_quic.h>
 
 
 int listeners;	/* # of proxy listeners, set by cfgparse */

src/ssl_sock.c
@@ -65,6 +65,7 @@
 #include <haproxy/proxy.h>
 #include <haproxy/sample.h>
 #include <haproxy/sc_strm.h>
+#include <haproxy/quic_transp_params.h>
 #include <haproxy/server.h>
 #include <haproxy/shctx.h>
 #include <haproxy/ssl_ckch.h>

src/xprt_quic.c
@@ -47,6 +47,7 @@
 #include <haproxy/quic_sock.h>
 #include <haproxy/quic_stats-t.h>
 #include <haproxy/quic_stream.h>
+#include <haproxy/quic_transp_params.h>
 #include <haproxy/cbuf.h>
 #include <haproxy/proto_quic.h>
 #include <haproxy/quic_tls.h>
@@ -62,17 +63,6 @@ static int quic_supported_version[] = {
 
 	/* placeholder, do not add entry after this */
 	0x0
-};
-
-/* This is the values of some QUIC transport parameters when absent.
- * Should be used to initialize any transport parameters (local or remote)
- * before updating them with customized values.
- */
-struct quic_transport_params quic_dflt_transport_params = {
-	.max_udp_payload_size = QUIC_PACKET_MAXLEN,
-	.ack_delay_exponent   = QUIC_DFLT_ACK_DELAY_COMPONENT,
-	.max_ack_delay        = QUIC_DFLT_MAX_ACK_DELAY,
-	.active_connection_id_limit = QUIC_ACTIVE_CONNECTION_ID_LIMIT,
 };
 
 /* trace source and events */
@@ -4223,40 +4213,6 @@ static int parse_retry_token(const unsigned char *token, const unsigned char *en
 	return 0;
 }
 
-/* Initialize the transport parameters for <qc> QUIC connection attached
- * to <l> listener from <pkt> Initial packet information.
- * Returns 1 if succeeded, 0 if not.
- */
-static int qc_lstnr_params_init(struct quic_conn *qc, struct listener *l,
-                                const unsigned char *token, size_t token_len,
-                                const struct quic_connection_id *icid,
-                                const struct quic_cid *dcid, const struct quic_cid *odcid)
-{
-	struct quic_cid *odcid_param = &qc->rx.params.original_destination_connection_id;
-
-	/* Copy the transport parameters. */
-	qc->rx.params = l->bind_conf->quic_params;
-	/* Copy the stateless reset token */
-	memcpy(qc->rx.params.stateless_reset_token, icid->stateless_reset_token,
-	       sizeof qc->rx.params.stateless_reset_token);
-	/* Copy original_destination_connection_id transport parameter. */
-	if (token_len) {
-		memcpy(odcid_param->data, odcid->data, odcid->len);
-		odcid_param->len = odcid->len;
-		/* Copy retry_source_connection_id transport parameter. */
-		quic_cid_cpy(&qc->rx.params.retry_source_connection_id, dcid);
-	}
-	else {
-		memcpy(odcid_param->data, dcid->data, dcid->len);
-		odcid_param->len = dcid->len;
-	}
-
-	/* Copy the initial source connection ID. */
-	quic_cid_cpy(&qc->rx.params.initial_source_connection_id, &qc->scid);
-
-	return 1;
-}
-
 /* Allocate a new QUIC connection with <version> as QUIC version. <ipv4>
  * boolean is set to 1 for IPv4 connection, 0 for IPv6. <server> is set to 1
  * for QUIC servers (or haproxy listeners).
@@ -4269,8 +4225,7 @@ static struct quic_conn *qc_new_conn(unsigned int version, int ipv4,
                                      struct quic_cid *dcid, struct quic_cid *scid,
                                      const struct quic_cid *odcid,
                                      struct sockaddr_storage *saddr,
-                                     const unsigned char *token, size_t token_len,
-                                     int server, void *owner)
+                                     int server, int token, void *owner)
 {
 	int i;
 	struct quic_conn *qc;
@@ -4389,7 +4344,11 @@ static struct quic_conn *qc_new_conn(unsigned int version, int ipv4,
 	qc->sendto_err = 0;
 	memcpy(&qc->peer_addr, saddr, sizeof qc->peer_addr);
 
-	if (server && !qc_lstnr_params_init(qc, l, token, token_len, icid, dcid, odcid))
+	if (server && !qc_lstnr_params_init(qc, &l->bind_conf->quic_params,
+	                                    icid->stateless_reset_token,
+	                                    dcid->data, dcid->len,
+	                                    qc->scid.data, qc->scid.len,
+	                                    odcid->data, odcid->len, token))
 		goto err;
 
 	qc->enc_params_len =
@@ -5376,8 +5335,8 @@ static void qc_lstnr_pkt_rcv(unsigned char *buf, const unsigned char *end,
 
 			pkt->saddr = dgram->saddr;
 			ipv4 = dgram->saddr.ss_family == AF_INET;
-			qc = qc_new_conn(pkt->version, ipv4, &pkt->dcid, &pkt->scid, &odcid, &pkt->saddr,
-			                 pkt->token, pkt->token_len, 1, l);
+			qc = qc_new_conn(pkt->version, ipv4, &pkt->dcid, &pkt->scid, &odcid,
+			                 &pkt->saddr, 1, !!pkt->token_len, l);
 			if (qc == NULL)
 				goto drop;
 

haproxy/haproxy

Makefile
@@ -637,7 +637,7 @@ OPTIONS_OBJS += src/quic_sock.o src/proto_quic.o src/xprt_quic.o src/quic_tls.o 
                 src/quic_frame.o src/quic_cc.o src/quic_cc_newreno.o src/mux_quic.o \
                 src/cbuf.o src/qpack-dec.o src/qpack-tbl.o src/h3.o src/qpack-enc.o \
                 src/hq_interop.o src/cfgparse-quic.o src/quic_loss.o \
-                src/quic_stream.o src/quic_stats.o
+                src/quic_tp.o src/quic_stream.o src/quic_stats.o
 endif
 
 ifneq ($(USE_LUA),)

include/haproxy/xprt_quic-t.h
@@ -35,8 +35,9 @@
 #include <haproxy/mux_quic-t.h>
 #include <haproxy/quic_cc-t.h>
 #include <haproxy/quic_frame-t.h>
-#include <haproxy/quic_tls-t.h>
 #include <haproxy/quic_loss-t.h>
+#include <haproxy/quic_tls-t.h>
+#include <haproxy/quic_tp-t.h>
 #include <haproxy/task.h>
 
 #include <import/ebtree-t.h>
@@ -49,9 +50,6 @@ typedef unsigned long long ull;
 
 #define QUIC_INITIAL_IPV4_MTU      1252 /* (bytes) */
 #define QUIC_INITIAL_IPV6_MTU      1232
-/* XXX TO DO XXX */
-/* Maximum packet length during handshake */
-#define QUIC_PACKET_MAXLEN     2048
 
 /* The minimum length of Initial packets. */
 #define QUIC_INITIAL_PACKET_MINLEN 1200
@@ -194,7 +192,6 @@ enum quic_pkt_type {
  */
 #define QUIC_CONN_MAX_PACKET  64
 
-#define QUIC_STATELESS_RESET_TOKEN_LEN 16
 #define QUIC_STATELESS_RESET_PACKET_HEADER_LEN 5
 #define QUIC_STATELESS_RESET_PACKET_MINLEN     (22 + QUIC_HAP_CID_LEN)
 
@@ -298,88 +295,6 @@ struct quic_connection_id {
 	struct quic_conn *qc;  /* QUIC connection using this CID */
 };
 
-struct preferred_address {
-	uint16_t ipv4_port;
-	uint16_t ipv6_port;
-	uint8_t ipv4_addr[4];
-	uint8_t ipv6_addr[16];
-	struct quic_cid cid;
-	uint8_t stateless_reset_token[QUIC_STATELESS_RESET_TOKEN_LEN];
-};
-
-/* Default values for the absent transport parameters */
-#define QUIC_DFLT_MAX_UDP_PAYLOAD_SIZE   65527 /* bytes */
-#define QUIC_DFLT_ACK_DELAY_COMPONENT        3 /* milliseconds */
-#define QUIC_DFLT_MAX_ACK_DELAY             25 /* milliseconds */
-#define QUIC_ACTIVE_CONNECTION_ID_LIMIT      2 /* number of connections */
-
-/* Types of QUIC transport parameters */
-#define QUIC_TP_ORIGINAL_DESTINATION_CONNECTION_ID   0
-#define QUIC_TP_MAX_IDLE_TIMEOUT                     1
-#define QUIC_TP_STATELESS_RESET_TOKEN                2
-#define QUIC_TP_MAX_UDP_PAYLOAD_SIZE                 3
-#define QUIC_TP_INITIAL_MAX_DATA                     4
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_BIDI_LOCAL   5
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_BIDI_REMOTE  6
-#define QUIC_TP_INITIAL_MAX_STREAM_DATA_UNI          7
-#define QUIC_TP_INITIAL_MAX_STREAMS_BIDI             8
-#define QUIC_TP_INITIAL_MAX_STREAMS_UNI              9
-#define QUIC_TP_ACK_DELAY_EXPONENT                  10
-#define QUIC_TP_MAX_ACK_DELAY                       11
-#define QUIC_TP_DISABLE_ACTIVE_MIGRATION            12
-#define QUIC_TP_PREFERRED_ADDRESS                   13
-#define QUIC_TP_ACTIVE_CONNECTION_ID_LIMIT          14
-#define QUIC_TP_INITIAL_SOURCE_CONNECTION_ID        15
-#define QUIC_TP_RETRY_SOURCE_CONNECTION_ID          16
-
-/*
- * These defines are not for transport parameter type, but the maximum accepted value for
- * transport parameter types.
- */
-#define QUIC_TP_ACK_DELAY_EXPONENT_LIMIT 20
-#define QUIC_TP_MAX_ACK_DELAY_LIMIT      (1UL << 14)
-
-/* The maximum length of encoded transport parameters for any QUIC peer. */
-#define QUIC_TP_MAX_ENCLEN    128
-/*
- * QUIC transport parameters.
- * Note that forbidden parameters sent by clients MUST generate TRANSPORT_PARAMETER_ERROR errors.
- */
-struct quic_transport_params {
-	uint64_t max_idle_timeout;
-	uint64_t max_udp_payload_size;                 /* Default: 65527 bytes (max of UDP payload for IPv6) */
-	uint64_t initial_max_data;
-	uint64_t initial_max_stream_data_bidi_local;
-	uint64_t initial_max_stream_data_bidi_remote;
-	uint64_t initial_max_stream_data_uni;
-	uint64_t initial_max_streams_bidi;
-	uint64_t initial_max_streams_uni;
-	uint64_t ack_delay_exponent;                   /* Default: 3, max: 20 */
-	uint64_t max_ack_delay;                        /* Default: 3ms, max: 2^14ms*/
-	uint64_t active_connection_id_limit;
-
-	/* Booleans */
-	uint8_t disable_active_migration;
-	uint8_t with_stateless_reset_token;
-	uint8_t with_preferred_address;
-	uint8_t original_destination_connection_id_present;
-	uint8_t initial_source_connection_id_present;
-
-	uint8_t stateless_reset_token[QUIC_STATELESS_RESET_TOKEN_LEN]; /* Forbidden for clients */
-	/*
-	 * MUST be sent by servers.
-	 * When received by clients, must be set to 1 if present.
-	 */
-	struct quic_cid original_destination_connection_id;            /* Forbidden for clients */
-	/*
-	 * MUST be sent by servers after Retry.
-	 */
-	struct quic_cid retry_source_connection_id;                    /* Forbidden for clients */
-	/* MUST be present both for servers and clients. */
-	struct quic_cid initial_source_connection_id;
-	struct preferred_address preferred_address;                    /* Forbidden for clients */
-};
-
 /* Structure to hold a range of ACKs sent in ACK frames. */
 struct quic_arng {
 	int64_t first;
@@ -466,9 +381,6 @@ struct quic_dgram {
 
 /* The QUIC packet numbers are 62-bits integers */
 #define QUIC_MAX_PACKET_NUM      ((1ULL << 62) - 1)
-
-/* Default QUIC connection transport parameters */
-extern struct quic_transport_params quic_dflt_transport_params;
 
 /* Maximum number of ack-eliciting received packets since the last
  * ACK frame was sent

src/listener.c
@@ -31,12 +31,12 @@
 #include <haproxy/log.h>
 #include <haproxy/protocol.h>
 #include <haproxy/proxy.h>
+#include <haproxy/quic_tp.h>
 #include <haproxy/sample.h>
 #include <haproxy/stream.h>
 #include <haproxy/task.h>
 #include <haproxy/ticks.h>
 #include <haproxy/tools.h>
-#include <haproxy/xprt_quic.h>
 
 
 /* List head of all known bind keywords */

src/mux_quic.c
@@ -10,6 +10,7 @@
 #include <haproxy/ncbuf.h>
 #include <haproxy/pool.h>
 #include <haproxy/quic_stream.h>
+#include <haproxy/quic_tp-t.h>
 #include <haproxy/ssl_sock-t.h>
 #include <haproxy/stconn.h>
 #include <haproxy/trace.h>

src/proxy.c
@@ -40,6 +40,7 @@
 #include <haproxy/proto_tcp.h>
 #include <haproxy/proxy.h>
 #include <haproxy/sc_strm.h>
+#include <haproxy/quic_tp.h>
 #include <haproxy/server-t.h>
 #include <haproxy/signal.h>
 #include <haproxy/stats-t.h>
@@ -49,7 +50,6 @@
 #include <haproxy/tcpcheck.h>
 #include <haproxy/time.h>
 #include <haproxy/tools.h>
-#include <haproxy/xprt_quic.h>
 
 
 int listeners;	/* # of proxy listeners, set by cfgparse */

src/ssl_sock.c
@@ -65,6 +65,7 @@
 #include <haproxy/proxy.h>
 #include <haproxy/sample.h>
 #include <haproxy/sc_strm.h>
+#include <haproxy/quic_tp.h>
 #include <haproxy/server.h>
 #include <haproxy/shctx.h>
 #include <haproxy/ssl_ckch.h>

src/xprt_quic.c
@@ -47,6 +47,7 @@
 #include <haproxy/quic_sock.h>
 #include <haproxy/quic_stats-t.h>
 #include <haproxy/quic_stream.h>
+#include <haproxy/quic_tp.h>
 #include <haproxy/cbuf.h>
 #include <haproxy/proto_quic.h>
 #include <haproxy/quic_tls.h>
@@ -62,17 +63,6 @@ static int quic_supported_version[] = {
 
 	/* placeholder, do not add entry after this */
 	0x0
-};
-
-/* This is the values of some QUIC transport parameters when absent.
- * Should be used to initialize any transport parameters (local or remote)
- * before updating them with customized values.
- */
-struct quic_transport_params quic_dflt_transport_params = {
-	.max_udp_payload_size = QUIC_PACKET_MAXLEN,
-	.ack_delay_exponent   = QUIC_DFLT_ACK_DELAY_COMPONENT,
-	.max_ack_delay        = QUIC_DFLT_MAX_ACK_DELAY,
-	.active_connection_id_limit = QUIC_ACTIVE_CONNECTION_ID_LIMIT,
 };
 
 /* trace source and events */
@@ -4223,40 +4213,6 @@ static int parse_retry_token(const unsigned char *token, const unsigned char *en
 	return 0;
 }
 
-/* Initialize the transport parameters for <qc> QUIC connection attached
- * to <l> listener from <pkt> Initial packet information.
- * Returns 1 if succeeded, 0 if not.
- */
-static int qc_lstnr_params_init(struct quic_conn *qc, struct listener *l,
-                                const unsigned char *token, size_t token_len,
-                                const struct quic_connection_id *icid,
-                                const struct quic_cid *dcid, const struct quic_cid *odcid)
-{
-	struct quic_cid *odcid_param = &qc->rx.params.original_destination_connection_id;
-
-	/* Copy the transport parameters. */
-	qc->rx.params = l->bind_conf->quic_params;
-	/* Copy the stateless reset token */
-	memcpy(qc->rx.params.stateless_reset_token, icid->stateless_reset_token,
-	       sizeof qc->rx.params.stateless_reset_token);
-	/* Copy original_destination_connection_id transport parameter. */
-	if (token_len) {
-		memcpy(odcid_param->data, odcid->data, odcid->len);
-		odcid_param->len = odcid->len;
-		/* Copy retry_source_connection_id transport parameter. */
-		quic_cid_cpy(&qc->rx.params.retry_source_connection_id, dcid);
-	}
-	else {
-		memcpy(odcid_param->data, dcid->data, dcid->len);
-		odcid_param->len = dcid->len;
-	}
-
-	/* Copy the initial source connection ID. */
-	quic_cid_cpy(&qc->rx.params.initial_source_connection_id, &qc->scid);
-
-	return 1;
-}
-
 /* Allocate a new QUIC connection with <version> as QUIC version. <ipv4>
  * boolean is set to 1 for IPv4 connection, 0 for IPv6. <server> is set to 1
  * for QUIC servers (or haproxy listeners).
@@ -4269,8 +4225,7 @@ static struct quic_conn *qc_new_conn(unsigned int version, int ipv4,
                                      struct quic_cid *dcid, struct quic_cid *scid,
                                      const struct quic_cid *odcid,
                                      struct sockaddr_storage *saddr,
-                                     const unsigned char *token, size_t token_len,
-                                     int server, void *owner)
+                                     int server, int token, void *owner)
 {
 	int i;
 	struct quic_conn *qc;
@@ -4389,7 +4344,11 @@ static struct quic_conn *qc_new_conn(unsigned int version, int ipv4,
 	qc->sendto_err = 0;
 	memcpy(&qc->peer_addr, saddr, sizeof qc->peer_addr);
 
-	if (server && !qc_lstnr_params_init(qc, l, token, token_len, icid, dcid, odcid))
+	if (server && !qc_lstnr_params_init(qc, &l->bind_conf->quic_params,
+	                                    icid->stateless_reset_token,
+	                                    dcid->data, dcid->len,
+	                                    qc->scid.data, qc->scid.len,
+	                                    odcid->data, odcid->len, token))
 		goto err;
 
 	qc->enc_params_len =
@@ -5376,8 +5335,8 @@ static void qc_lstnr_pkt_rcv(unsigned char *buf, const unsigned char *end,
 
 			pkt->saddr = dgram->saddr;
 			ipv4 = dgram->saddr.ss_family == AF_INET;
-			qc = qc_new_conn(pkt->version, ipv4, &pkt->dcid, &pkt->scid, &odcid, &pkt->saddr,
-			                 pkt->token, pkt->token_len, 1, l);
+			qc = qc_new_conn(pkt->version, ipv4, &pkt->dcid, &pkt->scid, &odcid,
+			                 &pkt->saddr, 1, !!pkt->token_len, l);
 			if (qc == NULL)
 				goto drop;
 