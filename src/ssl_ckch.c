haproxy/haproxy

commit 9a99e54787634532d18cd5aca3552eeb48a866bf

BUG/MINOR: ssl_ckch: Dump CRL transaction only once if show command yield
When loaded CRL files are displayed via "show ssl crl-file" command, the
in-progess transaction, if any, is also displayed. However, if the command
yield, the transaction is re-displayed again and again.

To fix the issue, old_crlfile_entry field is used to remember the transaction
was already displayed.

This patch must be backported as far as 2.5.

src/ssl_ckch.c
@@ -3762,11 +3762,12 @@ static int cli_io_handler_show_crlfile(struct appctx *appctx)
 	if (trash == NULL)
 		return 1;
 
-	if (!ctx->old_crlfile_entry) {
-		if (crlfile_transaction.old_crlfile_entry) {
-			chunk_appendf(trash, "# transaction\n");
-			chunk_appendf(trash, "*%s\n", crlfile_transaction.old_crlfile_entry->path);
-		}
+	if (!ctx->old_crlfile_entry && crlfile_transaction.old_crlfile_entry) {
+		chunk_appendf(trash, "# transaction\n");
+		chunk_appendf(trash, "*%s\n", crlfile_transaction.old_crlfile_entry->path);
+		if (applet_putchk(appctx, trash) == -1)
+			goto yield;
+		ctx->old_crlfile_entry = crlfile_transaction.old_crlfile_entry;
 	}
 
 	/* First time in this io_handler. */

commit 88041b35c3c964d1333877461f532da61dc81204

BUG/MINOR: ssl_ckch: Fix possible uninitialized value in show_crlfile I/O handler
Commit 9a99e5478 ("BUG/MINOR: ssl_ckch: Dump CRL transaction only once if
show command yield") introduced a regression leading to a build error
because of a possible uninitialized value. It is now fixed.

This patch must be backported as far as 2.5.

src/ssl_ckch.c
@@ -3715,7 +3715,7 @@ static int cli_io_handler_show_crlfile(struct appctx *appctx)
 	struct show_crlfile_ctx *ctx = appctx->svcctx;
 	struct buffer *trash = alloc_trash_chunk();
 	struct ebmb_node *node;
-	struct cafile_entry *cafile_entry;
+	struct cafile_entry *cafile_entry = NULL;
 
 	if (trash == NULL)
 		return 1;