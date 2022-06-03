haproxy/haproxy

commit 5a2154bf7cd40880cb28a8da58c708df9f5f541c

BUG/MINOR: ssl_ckch: Dump CA transaction only once if show command yield
When loaded CA files are displayed via "show ssl ca-file" command, the
in-progress transaction, if any, is also displayed. However, if the command
yield, the transaction is re-displayed again and again.

To fix the issue, old_cafile_entry field is used to remember the transaction
was already displayed.

This patch must be backported as far as 2.5.

src/ssl_ckch.c
@@ -3118,13 +3118,13 @@ static int cli_io_handler_show_cafile(struct appctx *appctx)
 	if (trash == NULL)
 		return 1;
 
-	if (!ctx->old_cafile_entry) {
-		if (cafile_transaction.old_cafile_entry) {
-			chunk_appendf(trash, "# transaction\n");
-			chunk_appendf(trash, "*%s", cafile_transaction.old_cafile_entry->path);
-
-			chunk_appendf(trash, " - %d certificate(s)\n", get_certificate_count(cafile_transaction.new_cafile_entry));
-		}
+	if (!ctx->old_cafile_entry && cafile_transaction.old_cafile_entry) {
+		chunk_appendf(trash, "# transaction\n");
+		chunk_appendf(trash, "*%s", cafile_transaction.old_cafile_entry->path);
+		chunk_appendf(trash, " - %d certificate(s)\n", get_certificate_count(cafile_transaction.new_cafile_entry));
+		if (applet_putchk(appctx, trash) == -1)
+			goto yield;
+		ctx->old_cafile_entry = cafile_transaction.new_cafile_entry;
 	}
 
 	/* First time in this io_handler. */

commit 677cb4fa91090a940ac9ba99049fb8584e7ae83d

BUG/MINOR: ssl_ckch: Fix possible uninitialized value in show_cafile I/O handler
Commit 5a2154bf7 ("BUG/MINOR: ssl_ckch: Dump CA transaction only once if
show command yield") introduced a regression leading to a build error
because of a possible uninitialized value. It is now fixed.

This patch must be backported as far as 2.5.

src/ssl_ckch.c
@@ -3075,7 +3075,7 @@ static int cli_io_handler_show_cafile(struct appctx *appctx)
 	struct show_cafile_ctx *ctx = appctx->svcctx;
 	struct buffer *trash = alloc_trash_chunk();
 	struct ebmb_node *node;
-	struct cafile_entry *cafile_entry;
+	struct cafile_entry *cafile_entry = NULL;
 
 	if (trash == NULL)
 		return 1;