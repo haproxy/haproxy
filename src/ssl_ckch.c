haproxy/haproxy

commit b0c4827c2f0332910cb459cd9898da5c392f8f36

BUG/MINOR: ssl: free the cafile entries on deinit
The cafile_tree was never free upon deinit, making valgrind and ASAN
complains when haproxy quits.

This could be backported as far as 2.2 but it requires the
ssl_store_delete_cafile_entry() helper from
5daff3c8abc658760a0d0c5fbbc633bfff1afe44.

src/ssl_ckch.c
@@ -3769,13 +3769,25 @@ void ckch_deinit()
 {
 	struct eb_node *node, *next;
 	struct ckch_store *store;
+	struct ebmb_node *canode;
 
+	/* deinit the ckch stores */
 	node = eb_first(&ckchs_tree);
 	while (node) {
 		next = eb_next(node);
 		store = ebmb_entry(node, struct ckch_store, node);
 		ckch_store_free(store);
 		node = next;
+	}
+
+	/* deinit the ca-file store */
+	canode = ebmb_first(&cafile_tree);
+	while (canode) {
+		struct cafile_entry *entry = NULL;
+
+		entry = ebmb_entry(canode, struct cafile_entry, node);
+		canode = ebmb_next(canode);
+		ssl_store_delete_cafile_entry(entry);
 	}
 }
 

commit 946580e17af62045707fd93a4bc46465024e5de4

BUG/MINOR: ssl: fix deinit of the ca-file tree
Commit b0c4827 ("BUG/MINOR: ssl: free the cafile entries on deinit")
introduced a double free.

The node was never removed from the tree before its free.

Fix issue #1836.

Must be backported where b0c4827 was backported. (2.6 for now).

src/ssl_ckch.c
@@ -3908,6 +3908,7 @@ void ckch_deinit()
 
 		entry = ebmb_entry(canode, struct cafile_entry, node);
 		canode = ebmb_next(canode);
+		ebmb_delete(&entry->node);
 		ssl_store_delete_cafile_entry(entry);
 	}
 }