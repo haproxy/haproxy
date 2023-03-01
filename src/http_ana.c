haproxy/haproxy

commit dd6496f5911a3d832b6f50f8b7eee7c1840f0b1a

CLEANUP: http-ana: Remove useless if statement about L7 retries
Thanks to the commit 1f08bffe0 ("MINOR: http-ana: Perform L7 retries because
of status codes in response analyser"), the L7 retries about the response
status code is now fully handled in the HTTP response analyser.
CF_READ_ERROR flag is no longer set on the response channel in this
case. Thus it is useless to try to catch L7 retries when CF_READ_ERROR is
set because it cannot happen.

The above commit was backported to 2.4, thus this one should also be
backported.

(cherry picked from commit 1a4449b0d0de49095aa8a1e1218e7c731d769f75)
Signed-off-by: Christopher Faulet <cfaulet@haproxy.com>

src/http_ana.c
@@ -1368,18 +1368,6 @@ int http_wait_for_response(struct stream *s, struct channel *rep, int an_bit)
 			if (objt_cs(s->si[1].end))
 				conn = objt_cs(s->si[1].end)->conn;
 
-			if (si_b->flags & SI_FL_L7_RETRY &&
-			    (!conn || conn->err_code != CO_ER_SSL_EARLY_FAILED)) {
-				/* If we arrive here, then CF_READ_ERROR was
-				 * set by si_cs_recv() because we matched a
-				 * status, otherwise it would have removed
-				 * the SI_FL_L7_RETRY flag, so it's ok not
-				 * to check s->be->retry_type.
-				 */
-				if (co_data(rep) || do_l7_retry(s, si_b) == 0)
-					return 0;
-			}
-
 			/* Perform a L7 retry because server refuses the early data. */
 			if ((si_b->flags & SI_FL_L7_RETRY) &&
 			    (s->be->retry_type & PR_RE_EARLY_ERROR) &&

commit 6f78ac56059ac00265197c04b801a819dd730d8e

BUG/MINOR: http-ana: Do a L7 retry on read error if there is no response
A regression about "empty-response" L7 retry was introduced with the commit
dd6496f591 ("CLEANUP: http-ana: Remove useless if statement about L7
retries").

The if statetement was removed on a wrong assumption. Indeed, L7 retries on
status is now handled in the HTTP analysers. Thus, the stream-connector
(formely the conn-stream, and before again the stream-interface) no longer
report a read error to force a retry. But it is still possible to get a read
error with no response. In this case, we must perform a retry is
"empty-response" is enabled.

So the if statement is re-introduced, reverting the cleanup.

This patch should fix the issue #2061. It must be backported as far as 2.4.

src/http_ana.c
@@ -1217,7 +1217,15 @@ int http_wait_for_response(struct stream *s, struct channel *rep, int an_bit)
 		if (sc_ep_test(s->scb, SE_FL_ERROR)) {
 			struct connection *conn = sc_conn(s->scb);
 
-			/* Perform a L7 retry because server refuses the early data. */
+
+			if ((txn->flags & TX_L7_RETRY) &&
+			    (s->be->retry_type & PR_RE_DISCONNECTED) &&
+			    (!conn || conn->err_code != CO_ER_SSL_EARLY_FAILED)) {
+				if (co_data(rep) || do_l7_retry(s, s->scb) == 0)
+					return 0;
+			}
+
+			/* Perform a L7 retry on empty response or because server refuses the early data. */
 			if ((txn->flags & TX_L7_RETRY) &&
 			    (s->be->retry_type & PR_RE_EARLY_ERROR) &&
 			    conn && conn->err_code == CO_ER_SSL_EARLY_FAILED &&