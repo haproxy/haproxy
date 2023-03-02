677c006c5c66db1b421eaac81926407e7ec686da
Willy Tarreau <w@1wt.eu>
2023-03-02 15:05:31 +0100

    MINOR: fd/cli: report the polling mask in "show fd"
    It's missing and often needed when trying to debug a situation, let's
    report the polling mask as well in "show fd".

--- src/cli.c
+++ src/cli.c
@@ -1323,7 +1323,7 @@ static int cli_io_handler_show_fd(struct appctx *appctx)
 			suspicious = 1;
 
 		chunk_printf(&trash,
-			     "  %5d : st=0x%06x(%c%c %c%c%c%c%c W:%c%c%c R:%c%c%c) ref=%#x gid=%d tmask=0x%lx umask=0x%lx owner=%p iocb=%p(",
+			     "  %5d : st=0x%06x(%c%c %c%c%c%c%c W:%c%c%c R:%c%c%c) ref=%#x gid=%d tmask=0x%lx umask=0x%lx prmsk=0x%lx pwmsk=0x%lx owner=%p iocb=%p(",
 			     fd,
 			     fdt.state,
 			     (fdt.state & FD_CLONED) ? 'C' : 'c',
@@ -1342,6 +1342,8 @@ static int cli_io_handler_show_fd(struct appctx *appctx)
 			     (fdt.refc_tgid >> 4) & 0xffff,
 			     (fdt.refc_tgid) & 0xffff,
 			     fdt.thread_mask, fdt.update_mask,
+			     polled_mask[fd].poll_recv,
+			     polled_mask[fd].poll_send,
 			     fdt.owner,
 			     fdt.iocb);
 		resolve_sym_name(&trash, NULL, fdt.iocb);
