haproxy/haproxy

commit 00e00fb424f5b4c363587d1a43f45812aef80ca8

REORG: cfgparse: extract curproxy as a global variable
This extracts curproxy from cfg_parse_listen so that it can be referenced by
keywords that need the context of the proxy they are being used within.

src/cfgparse.c
@@ -101,6 +101,9 @@ struct list postparsers = LIST_HEAD_INIT(postparsers);
 
 extern struct proxy *mworker_proxy;
 
+/* curproxy is only valid during parsing and will be NULL afterwards. */
+struct proxy *curproxy;
+
 char *cursection = NULL;
 int cfg_maxpconn = 0;                   /* # of simultaneous connections per proxy (-N) */
 int cfg_maxconn = 0;			/* # of simultaneous connections, (-n) */

include/haproxy/cfgparse.h
@@ -97,6 +97,8 @@ extern struct cfg_kw_list cfg_keywords;
 extern char *cursection;
 extern int non_global_section_parsed;
 
+extern struct proxy *curproxy;
+
 int cfg_parse_global(const char *file, int linenum, char **args, int inv);
 int cfg_parse_listen(const char *file, int linenum, char **args, int inv);
 int cfg_parse_track_sc_num(unsigned int *track_sc_num,

include/haproxy/timeshift.h
@@ -1,0 +1,10 @@
+#include <sys/time.h>
+#include <sys/epoll.h>
+
+#define gettimeofday(tv, tz) timeshift_gettimeofday(tv, tz)
+#define clock_gettime(clk_id, tp) timeshift_clock_gettime(clk_id, tp)
+#define epoll_wait(epfd, events, maxevents, timeout) timeshift_epoll_wait(epfd, events, maxevents, timeout)
+
+int timeshift_gettimeofday(struct timeval *tv, void *tz);
+int timeshift_clock_gettime(clockid_t clk_id, struct timespec *tp);
+int timeshift_epoll_wait(int epfd, struct epoll_event *events, int maxevents, int timeout);

src/cfgparse-listen.c
@@ -201,7 +201,6 @@ int warnif_misplaced_tcp_conn(struct proxy *proxy, const char *file, int line, c
 
 int cfg_parse_listen(const char *file, int linenum, char **args, int kwm)
 {
-	static struct proxy *curproxy = NULL;
 	static struct proxy *curr_defproxy = NULL;
 	static struct proxy *last_defproxy = NULL;
 	const char *err;

commit bbc3e4463e1e745ed4f9d9e2a45be5bd48b6365b

BUILD: cfgparse: keep a single "curproxy"
Surprisingly, commit 00e00fb42 ("REORG: cfgparse: extract curproxy as a
global variable") caused a build breakage on the CI but not on two
developers' machines. It looks like it's dependent on the linker version
used. What happens is that flt_spoe.c already has a curproxy struct which
already is a copy of the one passed by the parser because it also needed
it to be exported, so they now conflict. Let's just drop this unused copy.

src/cfgparse.c
@@ -102,7 +102,7 @@ struct list postparsers = LIST_HEAD_INIT(postparsers);
 extern struct proxy *mworker_proxy;
 
 /* curproxy is only valid during parsing and will be NULL afterwards. */
-struct proxy *curproxy;
+struct proxy *curproxy = NULL;
 
 char *cursection = NULL;
 int cfg_maxpconn = 0;                   /* # of simultaneous connections per proxy (-N) */

src/flt_spoe.c
@@ -71,9 +71,6 @@ const char *spoe_filter_id = "SPOE filter";
 /* Set if the handle on SIGUSR1 is registered */
 static int sighandler_registered = 0;
 
-/* proxy used during the parsing */
-struct proxy *curproxy = NULL;
-
 /* The name of the SPOE engine, used during the parsing */
 char *curengine = NULL;
 
@@ -4169,12 +4166,11 @@ parse_spoe_flt(char **args, int *cur_arg, struct proxy *px,
 	cfg_register_section("spoe-message", cfg_parse_spoe_message, NULL);
 
 	/* Parse SPOE filter configuration file */
+	BUG_ON(px != curproxy);
 	curengine = engine;
-	curproxy  = px;
 	curagent  = NULL;
 	curmsg    = NULL;
 	ret = readcfgfile(file);
-	curproxy = NULL;
 
 	/* unregister SPOE sections and restore previous sections */
 	cfg_unregister_sections();