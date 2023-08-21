2c6fe2400172e257c9758653d041c0a56f79f3d5
Willy Tarreau <w@1wt.eu>
2023-08-21 08:12:12 +0200

    MINOR: ssl_sock: avoid iterating realloc(+1) on stored context
    The SSL context storage in servers is per-thread, and the contents are
    allocated for a length that is determined from the session. It turns out
    that placing some traces there revealed that the realloc() that is called
    to grow the area can be called multiple times in a row even for just
    health checks, to grow the area by just one or two bytes. Given that
    malloc() allocates in multiples of 8 or 16 anyway, let's round the
    allocated size up to the nearest multiple of 8 to avoid this unneeded
    operation.

--- src/ssl_sock.c
+++ src/ssl_sock.c
@@ -4279,6 +4279,7 @@ static int ssl_sess_new_srv_cb(SSL *ssl, SSL_SESSION *sess)
 		if (s->ssl_ctx.reused_sess[tid].ptr && s->ssl_ctx.reused_sess[tid].allocated_size >= len) {
 			ptr = s->ssl_ctx.reused_sess[tid].ptr;
 		} else {
+			len = (len + 7) & -8; /* round to the nearest 8 bytes */
 			ptr = realloc(s->ssl_ctx.reused_sess[tid].ptr, len);
 			if (!ptr)
 				free(s->ssl_ctx.reused_sess[tid].ptr);
