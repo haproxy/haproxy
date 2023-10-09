5fa7bf207ef799acfc0ede7a753abc7fb0b4eed5
William Lallemand <wlallemand@haproxy.com>
2023-10-10 00:29:06 +0200

    CI: github: update wolfssl to git revision d83f2fa
    WolfSSL 5.6.3 does not pass all the haproxy reg-tests since some fixes
    are still unreleased in the master branch.
    
    Build wolfSSL with a recent git revision to have passing reg-tests.

--- .github/matrix.py
+++ .github/matrix.py
@@ -194,7 +194,7 @@ def main(ref_name):
             "OPENSSL_VERSION=1.0.2u",
             "OPENSSL_VERSION=1.1.1s",
             "QUICTLS=yes",
-            "WOLFSSL_VERSION=5.6.3",
+            "WOLFSSL_VERSION=git-d83f2fa",
             # "BORINGSSL=yes",
         ]
 
