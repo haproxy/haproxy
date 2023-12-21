haproxy/haproxy

commit 2421c6fa7db5b06109a88bffa411c8ba8f948384

BUG/MEDIUM: stconn: Block zero-copy forwarding if EOS/ERROR on consumer side
When the producer side (h1 for now) negociates with the consumer side to
perform a zero-copy forwarding, we now consider the consumer side as blocked
if it is closed and this was reported to the SE via a end-of-stream or a
(pending) error.

It is performed before calling ->nego_ff callback function, in se_nego_ff().
This way, all consumer are concerned automatically. The aim of this patch is
to fix an issue with the QUIC mux. Indeed, it is unexpected to send a frame
on an closed stream. This triggers a BUG_ON(). Other muxes are not affected
but it remains useless to try to send data if the stream is closed.

This patch should fix the issue #2372. It must be backported to 2.9.

include/haproxy/stconn.h
@@ -511,6 +511,16 @@ static inline size_t se_nego_ff(struct sedesc *se, struct buffer *input, size_t 
 
 		se->iobuf.flags &= ~IOBUF_FL_FF_BLOCKED;
 		if (mux->nego_fastfwd && mux->done_fastfwd) {
+			/* Declare SE as blocked if EOS or an error was reported.
+			 * This may happen if fast-forward was scheduled before the I/O processing on <SC>.
+			 * Wake <SC> up in this case.
+			 */
+			if (se_fl_test(se, SE_FL_EOS|SE_FL_ERROR|SE_FL_ERR_PENDING)) {
+				se->iobuf.flags |= IOBUF_FL_FF_BLOCKED;
+				tasklet_wakeup(se->sc->wait_event.tasklet);
+				goto end;
+			}
+
 			ret = mux->nego_fastfwd(se->sc, input, count, may_splice);
 			if ((se->iobuf.flags & IOBUF_FL_FF_BLOCKED) && !(se->sc->wait_event.events & SUB_RETRY_SEND)) {
 				/* The SC must be subs for send to be notify when some

commit 123a9e7d83213bcc0478840bab2270113275fb0a

BUG/MAJOR: stconn: Disable zero-copy forwarding if consumer is shut or in error
A regression was introduced by commit 2421c6fa7d ("BUG/MEDIUM: stconn: Block
zero-copy forwarding if EOS/ERROR on consumer side"). When zero-copy
forwarding is inuse and the consumer side is shut or in error, we declare it
as blocked and it is woken up. The idea is to handle this state at the
stream-connector level. However this definitly blocks receives on the
producer side. So if the mux is unable to close by itself, but instead wait
the peer to shut, this can lead to a wake up loop. And indeed, with the
passthrough multiplexer this may happen.

To fix the issue and prevent any loop, instead of blocking the zero-copy
forwarding, we now disable it. This way, the stream-connector on producer
side will fallback on classical receives and will be able to handle peer
shutdown properly. In addition, the wakeup of the consumer side was
removed. This will be handled, if necessary, by sc_notify().

This patch should fix the issue #2395. It must be backported to 2.9.

include/haproxy/stconn.h
@@ -511,13 +511,9 @@ static inline size_t se_nego_ff(struct sedesc *se, struct buffer *input, size_t 
 
 		se->iobuf.flags &= ~IOBUF_FL_FF_BLOCKED;
 		if (mux->nego_fastfwd && mux->done_fastfwd) {
-			/* Declare SE as blocked if EOS or an error was reported.
-			 * This may happen if fast-forward was scheduled before the I/O processing on <SC>.
-			 * Wake <SC> up in this case.
-			 */
+			/* Disable zero-copy forwarding if EOS or an error was reported. */
 			if (se_fl_test(se, SE_FL_EOS|SE_FL_ERROR|SE_FL_ERR_PENDING)) {
-				se->iobuf.flags |= IOBUF_FL_FF_BLOCKED;
-				tasklet_wakeup(se->sc->wait_event.tasklet);
+				se->iobuf.flags |= IOBUF_FL_NO_FF;
 				goto end;
 			}
 