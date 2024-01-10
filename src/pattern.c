haproxy/haproxy

commit 5fea59754b7765ca83aea3813f8b375160c7f3b0

MEDIUM: map/acl: Accelerate several functions using pat_ref_elt struct ->head list
Replace as much as possible list_for_each*() around ->head list, member of
pat_ref_elt struct by use of its ->ebpt_root member which is an ebtree.

src/pattern.c
@@ -1650,16 +1650,11 @@ void pat_ref_delete_by_ptr(struct pat_ref *ref, struct pat_ref_elt *elt)
  */
 int pat_ref_delete_by_id(struct pat_ref *ref, struct pat_ref_elt *refelt)
 {
-	struct pat_ref_elt *elt, *safe;
+	int ret = !!refelt->node.node.leaf_p;
 
-	/* delete pattern from reference */
-	list_for_each_entry_safe(elt, safe, &ref->head, list) {
-		if (elt == refelt) {
-			pat_ref_delete_by_ptr(ref, elt);
-			return 1;
-		}
-	}
-	return 0;
+	ebpt_delete(&refelt->node);
+
+	return ret;
 }
 
 /* This function removes all patterns matching <key> from the reference
@@ -1668,15 +1663,18 @@ int pat_ref_delete_by_id(struct pat_ref *ref, struct pat_ref_elt *refelt)
  */
 int pat_ref_delete(struct pat_ref *ref, const char *key)
 {
-	struct pat_ref_elt *elt, *safe;
+	struct ebpt_node *node;
 	int found = 0;
 
 	/* delete pattern from reference */
-	list_for_each_entry_safe(elt, safe, &ref->head, list) {
-		if (strcmp(key, elt->pattern) == 0) {
-			pat_ref_delete_by_ptr(ref, elt);
-			found = 1;
-		}
+	node = ebis_lookup(&ref->ebpt_root, key);
+	while (node) {
+		struct pat_ref_elt *elt;
+
+		elt = ebpt_entry(node, struct pat_ref_elt, node);
+		node = ebpt_next_dup(node);
+		pat_ref_delete_by_ptr(ref, elt);
+		found = 1;
 	}
 
 	return found;
@@ -1782,15 +1780,10 @@ static inline int pat_ref_set_elt(struct pat_ref *ref, struct pat_ref_elt *elt,
  */
 int pat_ref_set_by_id(struct pat_ref *ref, struct pat_ref_elt *refelt, const char *value, char **err)
 {
-	struct pat_ref_elt *elt;
-
-	/* Look for pattern in the reference. */
-	list_for_each_entry(elt, &ref->head, list) {
-		if (elt == refelt) {
-			if (!pat_ref_set_elt(ref, elt, value, err))
-				return 0;
-			return 1;
-		}
+	if (refelt->node.node.leaf_p) {
+		if (!pat_ref_set_elt(ref, refelt, value, err))
+			return 0;
+		return 1;
 	}
 
 	memprintf(err, "key or pattern not found");
@@ -1955,6 +1948,8 @@ struct pat_ref_elt *pat_ref_append(struct pat_ref *ref, const char *pattern, con
 	elt->list_head = NULL;
 	elt->tree_head = NULL;
 	LIST_APPEND(&ref->head, &elt->list);
+	/* Even if calloc()'ed, ensure this node is not linked to a tree. */
+	elt->node.node.leaf_p = NULL;
 	elt->node.key = elt->pattern;
 	ebis_insert(&ref->ebpt_root, &elt->node);
 	return elt;
@@ -2427,6 +2422,7 @@ int pattern_read_from_file(struct pattern_head *head, unsigned int refflags,
 {
 	struct pat_ref *ref;
 	struct pattern_expr *expr;
+	struct ebpt_node *node;
 	struct pat_ref_elt *elt;
 	int reuse = 0;
 
@@ -2517,7 +2513,10 @@ int pattern_read_from_file(struct pattern_head *head, unsigned int refflags,
 		return 1;
 
 	/* Load reference content in the pattern expression. */
-	list_for_each_entry(elt, &ref->head, list) {
+	node = ebpt_first(&ref->ebpt_root);
+	while (node) {
+		elt = ebpt_entry(node, struct pat_ref_elt, node);
+		node = ebpt_next(node);
 		if (!pat_ref_push(elt, expr, patflags, err)) {
 			if (elt->line > 0)
 				memprintf(err, "%s at line %d of file '%s'",

commit b546bb6d67145db0ebd14c79d72199064208dc2f

BUG/MINOR: map: list-based matching potential ordering regression
An unexpected side-effect was introduced by 5fea597 ("MEDIUM: map/acl:
Accelerate several functions using pat_ref_elt struct ->head list")

The above commit tried to use eb tree API to manipulate elements as much
as possible in the hope to accelerate some functions.

Prior to 5fea597, pattern_read_from_file() used to iterate over all
elements from the map file in the same order they were seen in the file
(using list_for_each_entry) to push them in the pattern expression.

Now, since eb api is used to iterate over elements, the ordering is lost
very early.

This is known to cause behavior changes with existing setups (same conf
and map file) when compared with previous versions for some list-based
matching methods as described in GH #2400. For instance, the map_dom()
converter may return a different matching key from the one that was
returned by older haproxy versions.

For IP or STR matching, matching is based on tree lookups for better
efficiency, so in this case the ordering is lost at the name of
performance. The order in which they are loaded doesn't matter because
tree ordering is based on the content, it is not positional.

But with some other types, matching is based on list lookups (e.g.: dom),
and the order in which elements are pushed into the list can affect the
matching element that will be returned (in case of multiple matches, since
only the first matching element in the list will be returned).

Despite the documentation not officially stating that the file ordering
should be preserved for list-based matching methods, it's probably best
to be conservative here and stick to historical behavior. Moreover, there
was no performance benefit from using the eb tree api to iterate over
elements in pattern_read_from_file() since all elements are visited
anyway.

This should be backported to 2.9.

src/pattern.c
@@ -2419,7 +2419,6 @@ int pattern_read_from_file(struct pattern_head *head, unsigned int refflags,
 {
 	struct pat_ref *ref;
 	struct pattern_expr *expr;
-	struct ebmb_node *node;
 	struct pat_ref_elt *elt;
 	int reuse = 0;
 
@@ -2511,11 +2510,11 @@ int pattern_read_from_file(struct pattern_head *head, unsigned int refflags,
 	if (reuse)
 		return 1;
 
-	/* Load reference content in the pattern expression. */
-	node = ebmb_first(&ref->ebmb_root);
-	while (node) {
-		elt = ebmb_entry(node, struct pat_ref_elt, node);
-		node = ebmb_next(node);
+	/* Load reference content in the pattern expression.
+	 * We need to load elements in the same order they were seen in the
+	 * file as list-based matching types may rely on it.
+	 */
+	list_for_each_entry(elt, &ref->head, list) {
 		if (!pat_ref_push(elt, expr, patflags, err)) {
 			if (elt->line > 0)
 				memprintf(err, "%s at line %d of file '%s'",