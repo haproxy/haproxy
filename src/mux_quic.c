bd384a359b443aadb6552971248c007932e4c1e2
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-03-22 15:13:42 +0100

    BUG/MINOR: mux-quic: close all QCS before freeing QCC tasklet
    QUIC MUX is freed via qcc_release(). This in turn liberate all the
    remaining QCS instances. For each one of them, their corresponding
    stream-desc is released via qc_stream_desc_release().
    
    This last function may itself notifies QUIC MUX when new buffers are
    available. This is useful when QCS are closed individually without the
    whole connection. However, when the connection is closed through
    qcc_release(), this may cause issue as some elements of QUIC MUX are
    already freed.
    
    In 2.9.6, a bug was detected directly linked to this. Indeed, QCC
    instance may be woken up on stream-desc release. If called through
    qcc_release(), this is an issue because QCC tasklet is freed before QCS
    instances. However, this bug is not systematic and relies on prior
    conditions : in particular, QUIC MUX must be under Tx buffers exhaustion
    prior to the qcc_release() invocation.
    
    The current dev tree is not impacted by this bug, thanks to QUIC MUX
    refactoring. Indeed, notifying accross layers have changed and now
    stream-desc release notifies individual QCS instances instead of the QCC
    element, which is a safer mechanism. However, to simplify backport
    process, bugfix is introduced in the current dev tree as it does not
    have any impact.
    
    Note that a proper fix would be to set quic-conn MUX state to
    QC_MUX_RELEASED. However, it is not possible to call quic_close()
    without having releasing all stream-desc elements first. The simpler
    solution was chosen to prevent other breaking issues during backports.
    
    This should fix github issue #2494.
    
    It should be backported up to 2.6. Note that prior to 2.7 qcc_release()
    was named qc_release().

--- src/mux_quic.c
+++ src/mux_quic.c
@@ -2464,19 +2464,19 @@ static void qcc_release(struct qcc *qcc)
 		qcc->task = NULL;
 	}
 
-	tasklet_free(qcc->wait_event.tasklet);
-	if (conn && qcc->wait_event.events) {
-		conn->xprt->unsubscribe(conn, conn->xprt_ctx,
-		                        qcc->wait_event.events,
-		                        &qcc->wait_event);
-	}
-
 	/* liberate remaining qcs instances */
 	node = eb64_first(&qcc->streams_by_id);
 	while (node) {
 		struct qcs *qcs = eb64_entry(node, struct qcs, by_id);
 		node = eb64_next(node);
 		qcs_free(qcs);
+	}
+
+	tasklet_free(qcc->wait_event.tasklet);
+	if (conn && qcc->wait_event.events) {
+		conn->xprt->unsubscribe(conn, conn->xprt_ctx,
+		                        qcc->wait_event.events,
+		                        &qcc->wait_event);
 	}
 
 	while (!LIST_ISEMPTY(&qcc->lfctl.frms)) {
