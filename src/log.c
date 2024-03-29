haproxy/haproxy

commit b61147fd2a54d4cfc1053a51e87c91cc306bb7ed

MEDIUM: log/balance: merge tcp/http algo with log ones
"log-balance" directive was recently introduced to configure the
balancing algorithm to use when in a log backend. However, it is
confusing and it causes issues when used in default section.

In this patch, we take another approach: first we remove the
"log-balance" directive, and instead we rely on existing "balance"
directive to configure log load balancing in log backend.

Some algorithms such as roundrobin can be used as-is in a log backend,
and for log-only algorithms, they are implemented as "log-$name" inside
the "backend" directive.

The documentation was updated accordingly.

src/log.c
@@ -910,7 +910,7 @@ static int postcheck_log_backend(struct proxy *be)
 	be->srv_bck = 0;
 
 	/* "log-balance hash" needs to compile its expression */
-	if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_SMP) {
+	if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_LH) {
 		struct sample_expr *expr;
 		char *expr_str = NULL;
 		char *err_str = NULL;
@@ -2198,7 +2198,7 @@ static inline void __do_send_log_backend(struct proxy *be, struct log_header hdr
 		 */
 		targetid = HA_ATOMIC_FETCH_ADD(&be->lbprm.log.lastid, 1) % nb_srv;
 	}
-	else if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_FAS) {
+	else if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_LS) {
 		/* sticky mode: use first server in the pool, which will always stay
 		 * first during dequeuing and requeuing, unless it becomes unavailable
 		 * and will be replaced by another one
@@ -2209,7 +2209,7 @@ static inline void __do_send_log_backend(struct proxy *be, struct log_header hdr
 		/* random mode */
 		targetid = statistical_prng() % nb_srv;
 	}
-	else if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_SMP) {
+	else if ((be->lbprm.algo & BE_LB_ALGO) == BE_LB_ALGO_LH) {
 		struct sample result;
 
 		/* log-balance hash */

include/haproxy/backend-t.h
@@ -65,6 +65,7 @@
 #define BE_LB_NEED_ADDR 0x00000100  /* only source address needed */
 #define BE_LB_NEED_DATA 0x00000200  /* some payload is needed     */
 #define BE_LB_NEED_HTTP 0x00000400  /* an HTTP request is needed  */
+#define BE_LB_NEED_LOG  0x00000800  /* LOG backend required  */
 #define BE_LB_NEED      0x0000FF00  /* mask to get/clear dependencies */
 
 /* Algorithm */
@@ -89,6 +90,8 @@
 #define BE_LB_ALGO_HH	(BE_LB_KIND_HI | BE_LB_NEED_HTTP | BE_LB_HASH_HDR) /* hash: HTTP header value  */
 #define BE_LB_ALGO_RCH	(BE_LB_KIND_HI | BE_LB_NEED_DATA | BE_LB_HASH_RDP) /* hash: RDP cookie value   */
 #define BE_LB_ALGO_SMP	(BE_LB_KIND_HI | BE_LB_NEED_DATA | BE_LB_HASH_SMP) /* hash: sample expression  */
+#define BE_LB_ALGO_LH	(BE_LB_KIND_HI | BE_LB_NEED_LOG  | BE_LB_HASH_SMP) /* log hash: sample expression  */
+#define BE_LB_ALGO_LS	(BE_LB_KIND_CB | BE_LB_NEED_LOG  | BE_LB_CB_FAS)   /* log sticky */
 #define BE_LB_ALGO      (BE_LB_KIND    | BE_LB_NEED      | BE_LB_PARM    ) /* mask to clear algo */
 
 /* Higher bits define how a given criterion is mapped to a server. In fact it

include/haproxy/backend.h
@@ -45,7 +45,6 @@ void back_handle_st_cer(struct stream *s);
 
 const char *backend_lb_algo_str(int algo);
 int backend_parse_balance(const char **args, char **err, struct proxy *curproxy);
-int backend_parse_log_balance(const char **args, char **err, struct proxy *curproxy);
 int tcp_persist_rdp_cookie(struct stream *s, struct channel *req, int an_bit);
 
 int be_downtime(struct proxy *px);

reg-tests/log/log_backend.vtc
@@ -67,7 +67,7 @@ haproxy h1 -conf {
                 mode log
 
                 # extract id (integer) from URL in the form "GET /id" and use it as hash key
-                log-balance hash 'field(-2,\"),field(2,/),field(1, )'
+                balance log-hash 'field(-2,\"),field(2,/),field(1, )'
                 hash-type map-based none
 
                 server s1 udp@${Slg1_addr}:${Slg1_port} # syslog 1 only receives "GET /0" requests
@@ -79,7 +79,7 @@ haproxy h1 -conf {
 
         backend mylog-failover
                 mode log
-		log-balance sticky
+		balance log-sticky
 
                 server s1 udp@${Slg21_addr}:${Slg21_port}        # only receives "GET /srv1" request
                 server s2 udp@${Slg22_addr}:${Slg22_port}        # only receives "GET /srv2" request

src/backend.c
@@ -2823,55 +2823,23 @@ int backend_parse_balance(const char **args, char **err, struct proxy *curproxy)
 			return -1;
 		}
 	}
-	else {
-		memprintf(err, "only supports 'roundrobin', 'static-rr', 'leastconn', 'source', 'uri', 'url_param', 'hdr(name)' and 'rdp-cookie(name)' options.");
-		return -1;
-	}
-	return 0;
-}
-
-/* This function parses a "balance" statement in a log backend section
- * describing <curproxy>. It returns -1 if there is any error, otherwise zero.
- * If it returns -1, it will write an error message into the <err> buffer which
- * will automatically be allocated and must be passed as NULL. The trailing '\n'
- * will not be written. The function must be called with <args> pointing to the
- * first word after "balance".
- */
-int backend_parse_log_balance(const char **args, char **err, struct proxy *curproxy)
-{
-	if (!*(args[0])) {
-		/* if no option is set, use round-robin by default */
-		curproxy->lbprm.algo &= ~BE_LB_ALGO;
-		curproxy->lbprm.algo |= BE_LB_ALGO_RR;
-		return 0;
-	}
-
-	if (strcmp(args[0], "roundrobin") == 0) {
-		curproxy->lbprm.algo &= ~BE_LB_ALGO;
-		curproxy->lbprm.algo |= BE_LB_ALGO_RR;
-	}
-	else if (strcmp(args[0], "sticky") == 0) {
-		curproxy->lbprm.algo &= ~BE_LB_ALGO;
-		/* we use ALGO_FAS as "sticky" mode in log-balance context */
-		curproxy->lbprm.algo |= BE_LB_ALGO_FAS;
-	}
-	else if (strcmp(args[0], "random") == 0) {
-		curproxy->lbprm.algo &= ~BE_LB_ALGO;
-		curproxy->lbprm.algo |= BE_LB_ALGO_RND;
-	}
-	else if (strcmp(args[0], "hash") == 0) {
+	else if (strcmp(args[0], "log-hash") == 0) {
 		if (!*args[1]) {
 			memprintf(err, "%s requires a converter list.", args[0]);
 			return -1;
 		}
 		curproxy->lbprm.algo &= ~BE_LB_ALGO;
-		curproxy->lbprm.algo |= BE_LB_ALGO_SMP;
+		curproxy->lbprm.algo |= BE_LB_ALGO_LH;
 
 		ha_free(&curproxy->lbprm.arg_str);
 		curproxy->lbprm.arg_str = strdup(args[1]);
 	}
+	else if (strcmp(args[0], "log-sticky") == 0) {
+		curproxy->lbprm.algo &= ~BE_LB_ALGO;
+		curproxy->lbprm.algo |= BE_LB_ALGO_LS;
+	}
 	else {
-		memprintf(err, "only supports 'roundrobin', 'sticky', 'random', 'hash' options");
+		memprintf(err, "only supports 'roundrobin', 'static-rr', 'leastconn', 'source', 'uri', 'url_param', 'hash', 'hdr(name)', 'rdp-cookie(name)', 'log-hash' and 'log-sticky' options.");
 		return -1;
 	}
 	return 0;

src/cfgparse-listen.c
@@ -554,15 +554,6 @@ int cfg_parse_listen(const char *file, int linenum, char **args, int kwm)
 			err_code |= ERR_ALERT | ERR_FATAL;
 			goto out;
 		}
-		/* mode log shares lbprm struct with other modes, but makes a different use of it,
-		 * thus, we must ensure that defproxy settings cannot persist between incompatibles
-		 * modes at this point.
-		 */
-		if ((curr_defproxy->mode == PR_MODE_SYSLOG && curproxy->mode != PR_MODE_SYSLOG) ||
-		    (curr_defproxy->mode != PR_MODE_SYSLOG && curproxy->mode == PR_MODE_SYSLOG)) {
-			/* lbprm settings from incompatible defproxy, back to defaults */
-			memset(&curproxy->lbprm, 0, sizeof(curproxy->lbprm));
-		}
 	}
 	else if (strcmp(args[0], "id") == 0) {
 		struct eb32_node *node;
@@ -2536,28 +2527,7 @@ stats_error_parsing:
 		if (warnifnotcap(curproxy, PR_CAP_BE, file, linenum, args[0], NULL))
 			err_code |= ERR_WARN;
 
-		if (curproxy->mode != PR_MODE_TCP && curproxy->mode != PR_MODE_HTTP) {
-			ha_alert("parsing [%s:%d] : '%s' requires TCP or HTTP mode.\n", file, linenum, args[0]);
-			err_code |= ERR_ALERT | ERR_FATAL;
-			goto out;
-		}
-
 		if (backend_parse_balance((const char **)args + 1, &errmsg, curproxy) < 0) {
-			ha_alert("parsing [%s:%d] : %s %s\n", file, linenum, args[0], errmsg);
-			err_code |= ERR_ALERT | ERR_FATAL;
-			goto out;
-		}
-	}
-	else if (strcmp(args[0], "log-balance") == 0) {  /* set log-balancing with optional algorithm */
-		if (warnifnotcap(curproxy, PR_CAP_BE, file, linenum, args[0], NULL))
-			err_code |= ERR_WARN;
-		if (curproxy->mode != PR_MODE_SYSLOG) {
-			ha_alert("parsing [%s:%d] : %s %s\n", file, linenum, args[0], "only available for log backends");
-			err_code |= ERR_ALERT | ERR_FATAL;
-			goto out;
-		}
-
-		if (backend_parse_log_balance((const char **)args + 1, &errmsg, curproxy) < 0) {
 			ha_alert("parsing [%s:%d] : %s %s\n", file, linenum, args[0], errmsg);
 			err_code |= ERR_ALERT | ERR_FATAL;
 			goto out;
@@ -2571,12 +2541,6 @@ stats_error_parsing:
 		 * The default hash function is sdbm for map-based and sdbm+avalanche for consistent.
 		 */
 		curproxy->lbprm.algo &= ~(BE_LB_HASH_TYPE | BE_LB_HASH_FUNC | BE_LB_HASH_MOD);
-
-		if (curproxy->mode != PR_MODE_TCP && curproxy->mode != PR_MODE_HTTP && curproxy->mode != PR_MODE_SYSLOG) {
-			ha_alert("parsing [%s:%d] : '%s' requires TCP, HTTP or LOG mode.\n", file, linenum, args[0]);
-			err_code |= ERR_ALERT | ERR_FATAL;
-			goto out;
-		}
 
 		if (warnifnotcap(curproxy, PR_CAP_BE, file, linenum, args[0], NULL))
 			err_code |= ERR_WARN;

commit d0692d7019cecaa87c8dc411312211045eac4b05

BUG/MINOR: log/balance: detect if user tries to use unsupported algo
b61147fd ("MEDIUM: log/balance: merge tcp/http algo with log ones")
introduced some ambiguities, because while it shares some algos with the
ones from mode {tcp,http}, we forgot report an error when the user tries
to use an algorithm that is not available in this mode (as per the doc).

Because of that, haproxy would silently drop log messages during runtime.

To fix that, we ensure that algo is one of the supported ones during log
backend postparsing. If the algo is not supported, we raise an error.

This should be backported in 2.9 with b61147fd

src/log.c
@@ -921,6 +921,7 @@ static void log_backend_srv_down(struct server *srv)
 static int _postcheck_log_backend_compat(struct proxy *be)
 {
 	int err_code = ERR_NONE;
+	int balance_algo = (be->lbprm.algo & BE_LB_ALGO);
 
 	if (!LIST_ISEMPTY(&be->tcp_req.inspect_rules) ||
 	    !LIST_ISEMPTY(&be->tcp_req.l4_rules) ||
@@ -977,6 +978,13 @@ static int _postcheck_log_backend_compat(struct proxy *be)
 
 		err_code |= ERR_WARN;
 		free_server_rules(&be->server_rules);
+	}
+	if (balance_algo != BE_LB_ALGO_RR &&
+	    balance_algo != BE_LB_ALGO_RND &&
+	    balance_algo != BE_LB_ALGO_LS &&
+	    balance_algo != BE_LB_ALGO_LH) {
+		ha_alert("in %s '%s': \"balance\" only supports 'roundrobin', 'random', 'sticky' and 'log-hash'.\n", proxy_type_str(be), be->id);
+		err_code |= ERR_ALERT | ERR_FATAL;
 	}
 	return err_code;
 }