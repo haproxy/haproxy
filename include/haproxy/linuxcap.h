9b3a27f70c31ac95fc092c613f3881d1be60a705
Christopher Faulet <cfaulet@haproxy.com>
2024-04-18 10:08:46 +0200

    BUILD: linuxcap: Properly declare prepare_caps_from_permitted_set()
    Expected arguments were not specified in the
    prepare_caps_from_permitted_set() function declaration. It is an issue for
    some compilers, for instance clang. But at the end, it is unexpected and
    deprecated.
    
    No backport needed, except if f0b6436f57 ("MEDIUM: capabilities: check
    process capabilities sets") is backported.

--- include/haproxy/linuxcap.h
+++ include/haproxy/linuxcap.h
@@ -3,6 +3,6 @@
 
 int prepare_caps_for_setuid(int from_uid, int to_uid);
 int finalize_caps_after_setuid(int from_uid, int to_uid);
-int prepare_caps_from_permitted_set();
+int prepare_caps_from_permitted_set(int from_uid, int to_uid, const char *program_name);
 
 #endif /* _HAPROXY_LINUXCAP_H */
