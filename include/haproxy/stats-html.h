a7caa14a643da10e26c6248915626c8d79374883
Olivier Houchard <cognet@ci0.org>
2024-04-19 01:49:12 +0200

    MINOR: stats: Get the right prototype for stats_dump_html_end().
    When the stat code was reorganized, and the prototype to
    stats_dump_html_end() was moved to its own header, it missed the function
    arguments. Fix that.
    
    This should fix issue 2540.

--- include/haproxy/stats-html.h
+++ include/haproxy/stats-html.h
@@ -15,7 +15,7 @@ int stats_dump_fields_html(struct buffer *out, const struct field *stats,
                            struct show_stat_ctx *ctx);
 void stats_dump_html_px_hdr(struct stconn *sc, struct proxy *px);
 void stats_dump_html_px_end(struct stconn *sc, struct proxy *px);
-void stats_dump_html_end();
+void stats_dump_html_end(struct buffer *out);
 
 extern struct applet http_stats_applet;
 
