haproxy/haproxy

commit 2462e5bccab61fc8afd3d8fc6cf5617a47d1726a

BUG/MINOR: log: fix potential lf->name memory leak
Recent commit 2ed6068 ("MINOR: log: custom name for logformat node")
introduced a potential memory leak because when custom name is provided,
lf->name value is allocated using strdup(), thus is expected to be freed
alongside the node when the node is released.

However lf->name was only freed in some common places within log.c
cleanups and helpers func, but in reality there are still cases where
lf nodes are manually freed without making use of freeing helpers.

So this is what this patch does, it makes sure all lf freeing places now
leverage the free_logformat_node() helper function that takes care of
freeing all known allocated elements within the node, including custom
name.

This commit depends on:
 - "MINOR: log: add free_logformat_node() helper function"

No backport needed unless 2ed6068 gets backported.

src/log.c
@@ -332,8 +332,7 @@ int parse_logformat_var(char *arg, int arg_len, char *name, int name_len, int ty
 				}
 				if (node->type == LOG_FMT_GLOBAL) {
 					*defoptions = node->options;
-					free(node->arg);
-					free(node);
+					free_logformat_node(node);
 				} else {
 					if (logformat_keywords[j].config_callback &&
 					    logformat_keywords[j].config_callback(node, curproxy) != 0) {
@@ -357,11 +356,7 @@ int parse_logformat_var(char *arg, int arg_len, char *name, int name_len, int ty
 	var[var_len] = j;
 
   error_free:
-	if (node) {
-		free(node->arg);
-		free(node->name);
-		free(node);
-	}
+	free_logformat_node(node);
 	return 0;
 }
 
@@ -480,11 +475,7 @@ int add_sample_to_logformat_list(char *text, char *name, int name_len, int typec
 	return 1;
 
   error_free:
-	release_sample_expr(expr);
-	if (node) {
-		free(node->arg);
-		free(node);
-	}
+	free_logformat_node(node);
 	return 0;
 }
 

src/cfgparse.c
@@ -3156,8 +3156,7 @@ init_proxies_list_stage1:
 				 * fall back to static rule
 				 */
 				LIST_DELETE(&node->list);
-				free(node->arg);
-				free(node);
+				free_logformat_node(node);
 			}
 
 			rule->dynamic = 0;
@@ -3227,8 +3226,7 @@ init_proxies_list_stage1:
 				 * fall back to static rule
 				 */
 				LIST_DELETE(&node->list);
-				free(node->arg);
-				free(node);
+				free_logformat_node(node);
 			}
 
 			srule->dynamic = 0;

commit 9d4a44e7137fba2c7ca2823174ba1168290a1c3b

BUG/MINOR: log: fix leak in add_sample_to_logformat_list() error path
If add_sample_to_logformat_list() fails to allocate new logformat_node,
then we directly jump to error_free label to cleanup the node using
free_logformat_node() before returning an error.

However if the node failed to allocate, then the sample expression that
was allocated just before (not yet assigned) isn't released
(free_logformat_node() is a no-op when NULL is provided). Thus if expr
wasn't assigned to the node during early failure, then it must be manually
released.

This bug was introduced by 2462e5bcc ("BUG/MINOR: log: fix potential
lf->name memory leak") which wasn't marked for backports. It only
affects 3.0.

src/log.c
@@ -547,6 +547,7 @@ static int add_sample_to_logformat_list(char *text, char *name, int name_len, in
 
 	node = calloc(1, sizeof(*node));
 	if (!node) {
+		release_sample_expr(expr);
 		memprintf(err, "out of memory error");
 		goto error_free;
 	}