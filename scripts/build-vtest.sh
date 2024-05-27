01843c47a13f01ba899d06ac968ca8a9bf7ff974
Willy Tarreau <w@1wt.eu>
2024-05-27 11:50:31 +0200

    CI: scripts: fix build of vtest regarding option -C
    On Linux, GNU make emits "w" at the beginning of the MAKEFLAGS
    variable if -C is passed, which happens since vtest d6d228bcb3.
    In fact it emits any of the command line flags without the leading
    '-' in this case. gmake doesn't do that on BSD apparently. It's
    documented under Options/Recursion in the GNU make doc. There's
    also MFLAGS that could work but it does not contain the variables
    definitions. So let's just avoid the -C that we don't really need.
    
    This needs to be backported to stable versions.

--- scripts/build-vtest.sh
+++ scripts/build-vtest.sh
@@ -7,11 +7,19 @@ mkdir ../vtest
 tar xvf VTest.tar.gz -C ../vtest --strip-components=1
 # Special flags due to: https://github.com/vtest/VTest/issues/12
 
+# Note: do not use "make -C ../vtest", otherwise MAKEFLAGS contains "w"
+# and fails (see Options/Recursion in GNU Make doc, it contains the list
+# of options without the leading '-').
+# MFLAGS works on BSD but misses variable definitions on GNU Make.
+# Better just avoid the -C and do the cd ourselves then.
+
+cd ../vtest
+
 #
 # temporarily detect Apple Silicon (it's using /opt/homebrew instead of /usr/local)
 #
 if test -f /opt/homebrew/include/pcre2.h; then
-   make -C ../vtest FLAGS="-O2 -s -Wall" INCS="-Isrc -Ilib -I/usr/local/include -I/opt/homebrew/include -pthread"
+   make FLAGS="-O2 -s -Wall" INCS="-Isrc -Ilib -I/usr/local/include -I/opt/homebrew/include -pthread"
 else
-   make -C ../vtest FLAGS="-O2 -s -Wall"
+   make FLAGS="-O2 -s -Wall"
 fi
