40cd5cc0e215ea5f649725385e052fab2ba38181
William Lallemand <wlallemand@haproxy.com>
2024-05-28 19:16:03 +0200

    CI: github: upgrade the WolfSSL job to 5.7.0
    WolfSSL 5.70 was released in March 2024,  let's upgrade our CI job to
    this version.

--- .github/matrix.py
+++ .github/matrix.py
@@ -190,7 +190,7 @@ def main(ref_name):
             "OPENSSL_VERSION=1.0.2u",
             "OPENSSL_VERSION=1.1.1s",
             "QUICTLS=yes",
-            "WOLFSSL_VERSION=5.6.6",
+            "WOLFSSL_VERSION=5.7.0",
             "AWS_LC_VERSION=1.16.0",
             # "BORINGSSL=yes",
         ]
