935b3bd1b7e7975c15ecd4f3c1d1d57c9a745102
William Lallemand <wlallemand@haproxy.com>
2024-06-13 17:14:58 +0200

    Revert "CI: github: upgrade aws-lc to 1.29.0"
    This reverts commit 6e986e7493ad2aa0c5a11c59d1235b03c02ef71c.

--- .github/matrix.py
+++ .github/matrix.py
@@ -191,7 +191,7 @@ def main(ref_name):
             "OPENSSL_VERSION=1.1.1s",
             "QUICTLS=yes",
             "WOLFSSL_VERSION=5.7.0",
-            "AWS_LC_VERSION=1.29.0",
+            "AWS_LC_VERSION=1.16.0",
             # "BORINGSSL=yes",
         ]
 
