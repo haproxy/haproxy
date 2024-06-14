haproxy/haproxy

commit b7c3d8c87cafc568bd9cfa1eb0a283fe931623ac

MINOR: log: add +bin logformat node option
Support '+bin' option argument on logformat nodes to try to preserve
binary output type with binary sample expressions.

For this, we rely on the log/sink API which is capable of conveying binary
data since all related functions don't search for a terminating NULL byte
in provided log payload as they take a string pointer and a string length
as argument.

Example:
  log-format "%{+bin}o %[bin(00AABB)]"

Will produce:
  00aabb

(output was piped to `hexdump  -ve '1/1 "%.2x"'` to dump raw bytes as HEX
characters)

This should be used carefully, because many syslog endpoints don't expect
binary data (especially NULL bytes). This is mainly intended for use with
set-var-fmt actions or with ring/udp log endpoints that know how to deal
with such binary payloads.

Also, this option is only supported globally (for use with '%o'), it will
not have any effect when set on an individual node. (it makes no sense to
have binary data in the middle of log payload that was started without
binary data option)

src/log.c
@@ -327,6 +327,7 @@ struct logformat_tag_args tag_args_list[] = {
 	{ "Q", LOG_OPT_QUOTE },
 	{ "X", LOG_OPT_HEXA },
 	{ "E", LOG_OPT_ESC },
+	{ "bin", LOG_OPT_BIN },
 	{  0,  0 }
 };
 
@@ -1748,8 +1749,10 @@ static inline void lf_buildctx_prepare(struct lf_buildctx *ctx,
 	if (node) {
 		/* per-node options are only considered if not already set
 		 * globally
+		 *
+		 * Also, ignore LOG_OPT_BIN since it is a global-only option
 		 */
-		ctx->options |= node->options;
+		ctx->options |= (node->options & ~LOG_OPT_BIN);
 
 		/* consider node's typecast setting */
 		ctx->typecast = node->typecast;
@@ -3237,18 +3240,35 @@ int sess_build_logline(struct session *sess, struct stream *s, char *dst, size_t
 
 				type = SMP_T_STR; // default
 
+				if (key && key->data.type == SMP_T_BIN &&
+				    (ctx.options & LOG_OPT_BIN)) {
+					/* output type is binary, and binary option is set:
+					 * preserve output type unless typecast is set to
+					 * force output type to string
+					 */
+					if (ctx.typecast != SMP_T_STR)
+						type = SMP_T_BIN;
+				}
+
 				if (key && !sample_convert(key, type))
 					key = NULL;
 
 				if (ctx.options & LOG_OPT_HTTP)
 					ret = lf_encode_chunk(tmplog, dst + maxsize,
 					                      '%', http_encode_map, key ? &key->data.u.str : &empty, &ctx);
-				else
-					ret = lf_text_len(tmplog,
-							  key ? key->data.u.str.area : NULL,
-							  key ? key->data.u.str.data : 0,
-							  dst + maxsize - tmplog,
-							  &ctx);
+				else {
+					if (key && type == SMP_T_BIN)
+						ret = lf_encode_chunk(tmplog, dst + maxsize,
+						                      0, no_escape_map,
+						                      &key->data.u.str,
+						                      &ctx);
+					else
+						ret = lf_text_len(tmplog,
+						                  key ? key->data.u.str.area : NULL,
+						                  key ? key->data.u.str.data : 0,
+						                  dst + maxsize - tmplog,
+						                  &ctx);
+				}
 				if (ret == NULL)
 					goto out;
 				tmplog = ret;

include/haproxy/log-t.h
@@ -47,6 +47,7 @@
 #define LOG_OPT_HTTP            0x00000020
 #define LOG_OPT_ESC             0x00000040
 #define LOG_OPT_MERGE_SPACES    0x00000080
+#define LOG_OPT_BIN             0x00000100
 
 
 /* Fields that need to be extracted from the incoming connection or request for

commit 0030f722a2fa574d1e7d90e6f242e4b6a5ace355

BUG/MINOR: log: fix broken '+bin' logformat node option
In 12d08cf912 ("BUG/MEDIUM: log: don't ignore disabled node's options"),
while trying to restore historical node option inheritance behavior, I
broke the '+bin' logformat node option recently introduced in b7c3d8c87c
("MINOR: log: add +bin logformat node option").

Indeed, because of 12d08cf912, LOG_OPT_BIN is not set anymore on
individual nodes even if it was set globally, making the feature unusable.
('+bin' is also used for binary cbor encoding)

What I should have done instead is include LOG_OPT_BIN in the options
inherited from global ones. This is what's being done in this commit.
Misleading comment was adjusted.

It must be backported in 3.0 with 12d08cf912.

src/log.c
@@ -969,14 +969,17 @@ static int lf_expr_postcheck_node_opt(struct lf_expr *lf_expr, struct logformat_
 	 * Also, ensure we don't mix encoding types, global setting
 	 * prevails over per-node one.
 	 *
-	 * Finally, ignore LOG_OPT_BIN since it is a global-only option
+	 * Finally, only consider LOG_OPT_BIN if set globally
+	 * (it is a global-only option)
 	 */
 	if (lf_expr->nodes.options & LOG_OPT_ENCODE) {
 		node->options &= ~(LOG_OPT_BIN | LOG_OPT_ENCODE);
-		node->options |= (lf_expr->nodes.options & LOG_OPT_ENCODE);
+		node->options |= (lf_expr->nodes.options & (LOG_OPT_BIN | LOG_OPT_ENCODE));
 	}
-	else
+	else {
 		node->options &= ~LOG_OPT_BIN;
+		node->options |= (lf_expr->nodes.options & LOG_OPT_BIN);
+	}
 
 	_lf_expr_postcheck_node_opt(&node->options, lf_expr->nodes.options);
 