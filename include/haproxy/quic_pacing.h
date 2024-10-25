2d0a24a2d964640391c2a5ae913b6beee96d762b
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-10-25 16:26:12 +0200

    MINOR: quic_pacing: define frms list and use it for MUX STREAM emission

--- include/haproxy/quic_pacing.h
+++ include/haproxy/quic_pacing.h
@@ -3,10 +3,30 @@
 
 #include <haproxy/quic_pacing-t.h>
 
+#include <haproxy/list.h>
+#include <haproxy/quic_frame.h>
+
 static inline void quic_pacing_init(struct quic_pacer *pacer,
                                     const struct quic_cc_path *path)
 {
+	LIST_INIT(&pacer->frms);
 	pacer->path = path;
+}
+
+static inline void quic_pacing_reset(struct quic_pacer *pacer)
+{
+	struct quic_frame *frm;
+
+	while (!LIST_ISEMPTY(&pacer->frms)) {
+		frm = LIST_ELEM(pacer->frms.n, struct quic_frame *, list);
+		/* qc_frm_free is responsible to detach frm from pacer list. */
+		qc_frm_free(NULL, &frm);
+	}
+}
+
+static inline struct list *quic_pacing_frms(struct quic_pacer *pacer)
+{
+	return &pacer->frms;
 }
 
 #endif /* _HAPROXY_QUIC_PACING_H */
