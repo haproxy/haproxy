b70880cdc9c01602197fd124c84ab264f6b4ddfb
Amaury Denoyelle <adenoyelle@haproxy.com>
2024-11-07 11:08:05 +0100

    CLEANUP: guid: remove global tree export
    guid_tree is not directly used outside of functions provided by the guid
    module. Remove its export from the include file.

--- include/haproxy/guid.h
+++ include/haproxy/guid.h
@@ -3,8 +3,6 @@
 
 #include <haproxy/guid-t.h>
 
-extern struct eb_root guid_tree;
-
 void guid_init(struct guid_node *node);
 int guid_insert(enum obj_type *obj_type, const char *uid, char **errmsg);
 void guid_remove(struct guid_node *guid);
