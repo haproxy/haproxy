143a1036969e7d025d93c3b8bd1d1f2dcbd44c3e
Willy Tarreau <w@1wt.eu>
2024-12-17 14:24:28 +0100

    CLEANUP: htx: use ASSUME_NONNULL() to mark the start line as non-null
    In http_replace_req_uri(), this assumption was previously made using
    ALREADY_CHECKED() but the new one is cleaner (and smaller, 24 bytes
    less).

--- src/http_htx.c
+++ src/http_htx.c
@@ -388,8 +388,8 @@ int http_replace_req_uri(struct htx *htx, const struct ist uri)
 	if (!http_replace_stline(htx, meth, uri, vsn))
 		goto fail;
 
-	sl = http_get_stline(htx);
-	ALREADY_CHECKED(sl); /* the stline exists because http_replace_stline() succeeded */
+	/* the stline exists because http_replace_stline() succeeded */
+	sl = ASSUME_NONNULL(http_get_stline(htx));
 
 	sl->flags &= ~HTX_SL_F_NORMALIZED_URI;
 	if (!http_update_host(htx, sl, uri))
