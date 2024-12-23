a3e6c783cd39bb630b73a15ff91f1a287f421c25
Ilia Shipitsin <chipitsine@gmail.com>
2024-12-23 22:00:48 +0100

    BUG/MINOR: mux_h1: handle a possible strdup() failure
    This defect was found by the coccinelle script "unchecked-strdup.cocci".
    It can be backported to all supported branches.

--- src/mux_h1.c
+++ src/mux_h1.c
@@ -5663,7 +5663,11 @@ static int cfg_parse_h1_headers_case_adjust_file(char **args, int section_type, 
 	}
 	free(hdrs_map.name);
 	hdrs_map.name = strdup(args[1]);
-        return 0;
+	if  (!hdrs_map.name) {
+		memprintf(err, "'%s %s' : out of memory", args[0], args[1]);
+		return -1;
+	}
+	return 0;
 }
 
 /* config parser for global "tune.h1.zero-copy-fwd-recv" */
