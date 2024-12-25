haproxy/haproxy

commit 665dde648177f8251638318a95d09a87fbdcc7ae

MINOR: debug: use LIM2A to show limits
It is more handy to use LIM2A in debug_parse_cli_show_dev(), as it allows to
show a custom string ("unlimited"), if a given limit value equals to 0.

normalize_rlim() handler is needed to convert properly RLIM_INFINITY to zero,
with the respect of type sizes, as rlim_t is always 4 bytes on 32bit and
64bit arch.

include/haproxy/limits.h
@@ -8,9 +8,22 @@
 #ifndef _HAPROXY_LIMITS_H
 #define _HAPROXY_LIMITS_H
 #include <sys/resource.h>
+#include <haproxy/compat.h>
 
 extern unsigned int rlim_fd_cur_at_boot;
 extern unsigned int rlim_fd_max_at_boot;
+
+/* returns 0 if the given limit was not set (reported as infinity), otherwise
+ * returns the limit, useful to print limit values as strings in err messages
+ * via LIM2A macros.
+ */
+static inline ulong normalize_rlim(ulong rlim)
+{
+	if (rlim == RLIM_INFINITY)
+		return 0;
+
+	return rlim;
+}
 
 /* handlers to compute internal process limits, if they are not provided via
  * cmd line or via configuration file.

src/debug.c
@@ -38,6 +38,7 @@
 #include <haproxy/global.h>
 #include <haproxy/hlua.h>
 #include <haproxy/http_ana.h>
+#include <haproxy/limits.h>
 #if defined(USE_LINUX_CAP)
 #include <haproxy/linuxcap.h>
 #endif
@@ -605,14 +606,14 @@ static int debug_parse_cli_show_dev(char **args, char *payload, struct appctx *a
 		chunk_appendf(&trash, "  capget() failed at runtime with: %s.\n",
 			      strerror(post_mortem.process.caps.err_run));
 #endif
-	if ((ulong)post_mortem.process.limit_fd.rlim_cur != RLIM_INFINITY)
-		chunk_appendf(&trash, "  fd limit (soft): %lu\n", (ulong)post_mortem.process.limit_fd.rlim_cur);
-	if ((ulong)post_mortem.process.limit_fd.rlim_max != RLIM_INFINITY)
-		chunk_appendf(&trash, "  fd limit (hard): %lu\n", (ulong)post_mortem.process.limit_fd.rlim_max);
-	if ((ulong)post_mortem.process.limit_ram.rlim_cur != RLIM_INFINITY)
-		chunk_appendf(&trash, "  ram limit (soft): %lu\n", (ulong)post_mortem.process.limit_ram.rlim_cur);
-	if ((ulong)post_mortem.process.limit_ram.rlim_max != RLIM_INFINITY)
-		chunk_appendf(&trash, "  ram limit (hard): %lu\n", (ulong)post_mortem.process.limit_ram.rlim_max);
+	chunk_appendf(&trash, "  fd limit (soft): %s\n",
+		      LIM2A(normalize_rlim((ulong)post_mortem.process.limit_fd.rlim_cur), "unlimited"));
+	chunk_appendf(&trash, "  fd limit (hard): %s\n",
+		      LIM2A(normalize_rlim((ulong)post_mortem.process.limit_fd.rlim_max), "unlimited"));
+	chunk_appendf(&trash, "  ram limit (soft): %s\n",
+		      LIM2A(normalize_rlim((ulong)post_mortem.process.limit_ram.rlim_cur), "unlimited"));
+	chunk_appendf(&trash, "  ram limit (hard): %s\n",
+		      LIM2A(normalize_rlim((ulong)post_mortem.process.limit_ram.rlim_max), "unlimited"));
 
 	return cli_msg(appctx, LOG_INFO, trash.area);
 }

src/limits.c
@@ -5,7 +5,6 @@
  *
  */
 
-#include <haproxy/compat.h>
 #include <haproxy/global.h>
 #include <haproxy/limits.h>
 #include <haproxy/proxy.h>

commit f486f976c7b981f70db962165970baed48504c36

BUILD: limits: make normalize_rlim() take an rlim_t to fix build on m68k
As can be seen here, the build fails on m68k since commit 665dde648
("MINOR: debug: use LIM2A to show limits") in 3.1:

  https://github.com/haproxy/haproxy/actions/runs/12440234399/job/34735360177

The reason is the comparison between a ulong limit and RLIM_INFINITY.
Indeed, on m68k, rlim_t is an unsigned long long. Let's just change
the function's input type to take an rlim_t instead. This also allows
to get rid of the casts in the call place.

This can be backported to 3.1 though it's not important given the low
prevalence of this platform for such use cases.

include/haproxy/limits.h
@@ -19,12 +19,12 @@ extern unsigned int rlim_fd_max_at_boot;
  * returns the limit, useful to print limit values as strings in err messages
  * via LIM2A macros.
  */
-static inline ulong normalize_rlim(ulong rlim)
+static inline ulong normalize_rlim(rlim_t rlim)
 {
 	if (rlim == RLIM_INFINITY)
 		return 0;
 
-	return rlim;
+	return (ulong)rlim;
 }
 
 /* handlers to compute internal process limits, if they are not provided via

src/debug.c
@@ -636,23 +636,23 @@ static int debug_parse_cli_show_dev(char **args, char *payload, struct appctx *a
 #endif
 	chunk_appendf(&trash, "  boot limits:\n");
 	chunk_appendf(&trash, "  \tfd limit (soft): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.boot_lim_fd.rlim_cur), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.boot_lim_fd.rlim_cur), "unlimited"));
 	chunk_appendf(&trash, "  \tfd limit (hard): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.boot_lim_fd.rlim_max), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.boot_lim_fd.rlim_max), "unlimited"));
 	chunk_appendf(&trash, "  \tram limit (soft): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.boot_lim_ram.rlim_cur), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.boot_lim_ram.rlim_cur), "unlimited"));
 	chunk_appendf(&trash, "  \tram limit (hard): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.boot_lim_ram.rlim_max), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.boot_lim_ram.rlim_max), "unlimited"));
 
 	chunk_appendf(&trash, "  runtime limits:\n");
 	chunk_appendf(&trash, "  \tfd limit (soft): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.run_lim_fd.rlim_cur), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.run_lim_fd.rlim_cur), "unlimited"));
 	chunk_appendf(&trash, "  \tfd limit (hard): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.run_lim_fd.rlim_max), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.run_lim_fd.rlim_max), "unlimited"));
 	chunk_appendf(&trash, "  \tram limit (soft): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.run_lim_ram.rlim_cur), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.run_lim_ram.rlim_cur), "unlimited"));
 	chunk_appendf(&trash, "  \tram limit (hard): %s\n",
-		      LIM2A(normalize_rlim((ulong)post_mortem.process.run_lim_ram.rlim_max), "unlimited"));
+		      LIM2A(normalize_rlim(post_mortem.process.run_lim_ram.rlim_max), "unlimited"));
 
 	return cli_msg(appctx, LOG_INFO, trash.area);
 }