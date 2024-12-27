bbd1cedefcdb39b4ea2e82c0ba2ca219a2fb4f5f
Ilia Shipitsin <chipitsine@gmail.com>
2024-12-27 21:55:38 +0100

    BUG/MINOR: cfgparse-tcp: handle a possible strdup() failure
    This defect was found by the coccinelle script "unchecked-strdup.cocci".
    It can be backported to all supported branches.

--- src/cfgparse-tcp.c
+++ src/cfgparse-tcp.c
@@ -144,6 +144,10 @@ static int bind_parse_interface(char **args, int cur_arg, struct proxy *px, stru
 
 	ha_free(&conf->settings.interface);
 	conf->settings.interface = strdup(args[cur_arg + 1]);
+	if (!conf->settings.interface) {
+		memprintf(err, "'%s %s' : out of memory", args[cur_arg], args[cur_arg + 1]);
+		return ERR_ALERT | ERR_FATAL;
+	}
 	return 0;
 }
 #endif
