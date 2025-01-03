f578811c4e8965f243cd16a6f569909a2a07498c
Christopher Faulet <cfaulet@haproxy.com>
2025-01-03 10:10:08 +0100

    MINOR: config: Alert about extra arguments for errorfile and errorloc
    errorfile and errorloc directives expect excatly two arguments. But extra
    arguments were just ignored while an error should be emitted. It is now
    fixed.
    
    This patch could be backported as far as 2.2 if necessary.

--- src/http_htx.c
+++ src/http_htx.c
@@ -1999,6 +1999,11 @@ static int proxy_parse_errorloc(char **args, int section, struct proxy *curpx,
 		ret = -1;
 		goto out;
 	}
+	if (*(args[3])) {
+		memprintf(errmsg, "%s : expects exactly two arguments.\n", args[0]);
+		ret = -1;
+		goto out;
+	}
 
 	status = atol(args[1]);
 	errloc = (strcmp(args[0], "errorloc303") == 0 ? 303 : 302);
@@ -2063,6 +2068,11 @@ static int proxy_parse_errorfile(char **args, int section, struct proxy *curpx,
 
 	if (*(args[1]) == 0 || *(args[2]) == 0) {
 		memprintf(errmsg, "%s : expects <status_code> and <file> as arguments.\n", args[0]);
+		ret = -1;
+		goto out;
+	}
+	if (*(args[3])) {
+		memprintf(errmsg, "%s : expects exactly two arguments.\n", args[0]);
 		ret = -1;
 		goto out;
 	}
