41e9dca85ebc8ad50915cce5a354e1293f47f90e
Willy Tarreau <w@1wt.eu>
2025-01-06 19:38:45 +0100

    MINOR: cpu-topo: skip CPU detection when /sys/.../cpu does not exist
    There's no point scanning all entries when /cpu doesn't exist in the
    first place. Let's check once for it and skip the loop in this case.

--- src/cpu_topo.c
+++ src/cpu_topo.c
@@ -249,6 +249,10 @@ int cpu_detect_topology(void)
 	 * thread_set_id, cluster_id, l1/l2/l3 id, etc. We don't revisit entries
 	 * already filled from the list provided by another CPU.
 	 */
+
+	if (!is_dir_present(NUMA_DETECT_SYSTEM_SYSFS_PATH "/cpu"))
+		goto skip_cpu;
+
 	for (cpu = 0; cpu <= lastcpu; cpu++) {
 		struct hap_cpuset cpus_list;
 		int next_level = 1; // assume L1 if unknown
@@ -425,6 +429,7 @@ int cpu_detect_topology(void)
 		}
 	}
 
+ skip_cpu:
 	/* Now locate NUMA node IDs if any */
 
 	dir = opendir(NUMA_DETECT_SYSTEM_SYSFS_PATH "/node");
