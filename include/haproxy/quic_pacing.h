2178bf1192650da0196dc38987e64dfafd74de43
Amaury Denoyelle <adenoyelle@haproxy.com>
2025-01-22 16:16:17 +0100

    CLEANUP: quic: remove unused prototype
    Remove undefined quic_pacing_send() function prototype from quic_pacing
    module.
    
    This should be backported up to 3.1.

--- include/haproxy/quic_pacing.h
+++ include/haproxy/quic_pacing.h
@@ -17,6 +17,4 @@ int quic_pacing_expired(const struct quic_pacer *pacer);
 
 void quic_pacing_sent_done(struct quic_pacer *pacer, int sent);
 
-enum quic_tx_err quic_pacing_send(struct quic_pacer *pacer, struct quic_conn *qc);
-
 #endif /* _HAPROXY_QUIC_PACING_H */
