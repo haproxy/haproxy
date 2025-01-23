e731f525de3d9b8b6fcf6d78fb4c0c1811a05841
Willy Tarreau <w@1wt.eu>
2025-01-23 18:00:51 +0100

    MINOR: cpu-topo: add a function to dump CPU topology
    The new function cpu_dump_topology() will centralize most debugging
    calls, and it can make efforts of not dumping some possibly irrelevant
    fields (e.g. non-existing cache levels).

--- include/haproxy/cpu_topo.h
+++ include/haproxy/cpu_topo.h
@@ -7,4 +7,9 @@
 
 extern struct ha_cpu_topo *ha_cpu_topo;
 
+/* Dump the CPU topology <topo> for up to <maxcpus> for debugging purposes.
+ * Offline CPUs are skipped.
+ */
+void cpu_dump_topology(const struct ha_cpu_topo *topo, int maxcpus);
+
 #endif /* _HAPROXY_CPU_TOPO_H */
--- src/cpu_topo.c
+++ src/cpu_topo.c
@@ -6,6 +6,56 @@
 /* CPU topology information, ha_cpuset_size() entries, allocated at boot */
 struct ha_cpu_topo *ha_cpu_topo = NULL;
 
+
+/* Dump the CPU topology <topo> for up to <maxcpus> for debugging purposes.
+ * Offline CPUs are skipped.
+ */
+void cpu_dump_topology(const struct ha_cpu_topo *topo, int maxcpus)
+{
+	int lastcpu = 0;
+	int has_smt = 0;
+	int cpu, lvl;
+
+	for (cpu = 0; cpu < maxcpus; cpu++) {
+		if (!(ha_cpu_topo[cpu].st & HA_CPU_F_OFFLINE))
+			lastcpu = cpu;
+		if (ha_cpu_topo[cpu].th_cnt > 1)
+			has_smt = 1;
+	}
+
+	for (cpu = 0; cpu <= lastcpu; cpu++) {
+		printf("%3d: cpu=%3d excl=%d pk=%02d no=%02d cl=%03d(%03d)",
+		       cpu, ha_cpu_topo[cpu].idx,
+		       (ha_cpu_topo[cpu].st & HA_CPU_F_EXCL_MASK),
+		       ha_cpu_topo[cpu].pk_id,
+		       ha_cpu_topo[cpu].no_id,
+		       ha_cpu_topo[cpu].cl_gid,
+		       ha_cpu_topo[cpu].cl_lid);
+
+		/* list only relevant cache levels */
+		for (lvl = 4; lvl >= 0; lvl--) {
+			if (ha_cpu_topo[cpu].ca_id[lvl] < 0)
+				continue;
+			printf(lvl < 3 ? " l%d=%02d" : " l%d=%03d", lvl, ha_cpu_topo[cpu].ca_id[lvl]);
+		}
+
+		printf(" ts=%03d capa=%d",
+		       ha_cpu_topo[cpu].ts_id,
+		       ha_cpu_topo[cpu].capa);
+
+		if (has_smt) {
+			if (ha_cpu_topo[cpu].th_cnt > 1)
+				printf(" smt=%d/%d",
+				       ha_cpu_topo[cpu].th_id,
+				       ha_cpu_topo[cpu].th_cnt);
+			else
+				printf(" smt=%d",
+				       ha_cpu_topo[cpu].th_cnt);
+		}
+		putchar('\n');
+	}
+}
+
 /* Allocates everything needed to store CPU topology at boot.
  * Returns non-zero on success, zero on failure.
  */
