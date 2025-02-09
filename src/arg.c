d4c05f60c83ca6cdd3503baa4e7c8b3d09a6d8d6
Александр Ушаков <aushakov@astralinux.ru>
2025-02-09 14:09:09 +0300

    Fixed segfault in make_arg_list() arg.c

--- src/arg.c
+++ src/arg.c
@@ -223,6 +223,9 @@ int make_arg_list(const char *in, int len, uint64_t mask, struct arg **argp,
 		if (len && *in && *in != ',' && *in != ')')
 			goto buffer_err;
 
+		if (!trash.data)
+			goto empty_err;
+
 		trash.area[trash.data] = 0;
 
 		arg->type = (mask >> (pos * ARGT_BITS)) & ARGT_MASK;
