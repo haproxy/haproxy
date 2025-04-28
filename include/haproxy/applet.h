1ced5ef2fdf9a4b4ab0941849be0f4627066331f
Aurelien DARRAGON <adarragon@haproxy.com>
2025-04-28 18:03:36 +0200

    MINOR: applet: add appctx_schedule() macro
    Just like task_schedule() but for applets to wakeup an applet at a
    specific time, leverages _task_schedule() internally

--- include/haproxy/applet.h
+++ include/haproxy/applet.h
@@ -146,6 +146,9 @@ static inline void __appctx_free(struct appctx *appctx)
 #define appctx_wakeup(ctx) \
 	_task_wakeup((ctx)->t, TASK_WOKEN_OTHER, MK_CALLER(WAKEUP_TYPE_APPCTX_WAKEUP, 0, 0))
 
+#define appctx_schedule(ctx, w) \
+        _task_schedule((ctx)->t, w, MK_CALLER(WAKEUP_TYPE_TASK_SCHEDULE, 0, 0))
+
 /* returns the stream connector the appctx is attached to, via the sedesc */
 static inline struct stconn *appctx_sc(const struct appctx *appctx)
 {
