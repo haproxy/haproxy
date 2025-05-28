c2b715a745758647bc3cb3f03f371fe9842d7971
Amaury Denoyelle <adenoyelle@haproxy.com>
2025-05-28 11:24:43 +0200

    MINOR: h3: implement request header encoding

--- include/haproxy/qpack-enc.h
+++ include/haproxy/qpack-enc.h
@@ -1,12 +1,16 @@
 #ifndef QPACK_ENC_H_
 #define QPACK_ENC_H_
 
+#include <haproxy/http-t.h>
 #include <haproxy/istbuf.h>
 
 struct buffer;
 
 int qpack_encode_field_section_line(struct buffer *out);
 int qpack_encode_int_status(struct buffer *out, unsigned int status);
+int qpack_encode_method(struct buffer *out, enum http_meth_t meth);
+int qpack_encode_scheme(struct buffer *out);
+int qpack_encode_path(struct buffer *out, const struct ist path);
 int qpack_encode_header(struct buffer *out, const struct ist n, const struct ist v);
 
 #endif /* QPACK_ENC_H_ */
