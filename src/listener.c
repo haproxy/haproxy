haproxy/haproxy

commit 16eb0fab319dae5cfde1b69de8927c757e1dba78

MAJOR: counters: dispatch counters over thread groups
Most fe and be counters are good candidates for being shared between
processes. They are now grouped inside "shared" struct sub member under
be_counters and fe_counters.

Now they are properly identified, they would greatly benefit from being
shared over thread groups to reduce the cost of atomic operations when
updating them. For this, we take the current tgid into account so each
thread group only updates its own counters. For this to work, it is
mandatory that the "shared" member from {fe,be}_counters is initialized
AFTER global.nbtgroups is known, because each shared counter causes the stat
to be allocated lobal.nbtgroups times. When updating a counter without
concurrency, the first counter from the array may be updated.

To consult the shared counters (which requires aggregation of per-tgid
individual counters), some helper functions were added to counter.h to
ease code maintenance and avoid computing errors.

src/listener.c
@@ -22,6 +22,7 @@
 #include <haproxy/cfgparse.h>
 #include <haproxy/cli-t.h>
 #include <haproxy/connection.h>
+#include <haproxy/counters.h>
 #include <haproxy/errors.h>
 #include <haproxy/fd.h>
 #include <haproxy/freq_ctr.h>
@@ -1089,11 +1090,22 @@ void listener_accept(struct listener *l)
 	}
 #endif
 	if (p && p->fe_sps_lim) {
-		int max = freq_ctr_remain(&p->fe_counters.shared->sess_per_sec, p->fe_sps_lim, 0);
+		int max = 0;
+
+		for (int it = 0; it < global.nbtgroups; it++)
+			max += freq_ctr_remain(&p->fe_counters.shared->tg[it]->sess_per_sec, p->fe_sps_lim, 0);
 
 		if (unlikely(!max)) {
+			unsigned int min_wait = 0;
+
+			for (int it = 0; it < global.nbtgroups; it++) {
+				unsigned int cur_wait = next_event_delay(&p->fe_counters.shared->tg[it]->sess_per_sec, p->fe_sps_lim, 0);
+				if (!it || cur_wait < min_wait)
+					min_wait = cur_wait;
+			}
+
 			/* frontend accept rate limit was reached */
-			expire = tick_add(now_ms, next_event_delay(&p->fe_counters.shared->sess_per_sec, p->fe_sps_lim, 0));
+			expire = tick_add(now_ms, min_wait);
 			goto limit_proxy;
 		}
 
@@ -1573,7 +1585,7 @@ void listener_accept(struct listener *l)
 		dequeue_all_listeners();
 
 		if (p && !MT_LIST_ISEMPTY(&p->listener_queue) &&
-		    (!p->fe_sps_lim || freq_ctr_remain(&p->fe_counters.shared->sess_per_sec, p->fe_sps_lim, 0) > 0))
+		    (!p->fe_sps_lim || COUNTERS_SHARED_TOTAL_ARG2(p->fe_counters.shared->tg, sess_per_sec, freq_ctr_remain, p->fe_sps_lim, 0) > 0))
 			dequeue_proxy_listeners(p, 0);
 	}
 	return;
@@ -1632,14 +1644,14 @@ void listener_release(struct listener *l)
 	dequeue_all_listeners();
 
 	if (fe && !MT_LIST_ISEMPTY(&fe->listener_queue) &&
-	    (!fe->fe_sps_lim || freq_ctr_remain(&fe->fe_counters.shared->sess_per_sec, fe->fe_sps_lim, 0) > 0))
+	    (!fe->fe_sps_lim || COUNTERS_SHARED_TOTAL_ARG2(fe->fe_counters.shared->tg, sess_per_sec, freq_ctr_remain, fe->fe_sps_lim, 0) > 0))
 		dequeue_proxy_listeners(fe, 0);
 	else if (fe) {
 		unsigned int wait;
 		int expire = TICK_ETERNITY;
 
 		if (fe->task && fe->fe_sps_lim &&
-		    (wait = next_event_delay(&fe->fe_counters.shared->sess_per_sec,fe->fe_sps_lim, 0))) {
+		    (wait = COUNTERS_SHARED_TOTAL_ARG2(fe->fe_counters.shared->tg, sess_per_sec, next_event_delay, fe->fe_sps_lim, 0))) {
 			/* we're blocking because a limit was reached on the number of
 			 * requests/s on the frontend. We want to re-check ASAP, which
 			 * means in 1 ms before estimated expiration date, because the

include/haproxy/backend.h
@@ -86,7 +86,7 @@ static inline int be_usable_srv(struct proxy *be)
 /* set the time of last session on the backend */
 static inline void be_set_sess_last(struct proxy *be)
 {
-	HA_ATOMIC_STORE(&be->be_counters.shared->last_sess, ns_to_sec(now_ns));
+	HA_ATOMIC_STORE(&be->be_counters.shared->tg[tgid - 1]->last_sess, ns_to_sec(now_ns));
 }
 
 /* This function returns non-zero if the designated server will be

include/haproxy/counters-t.h
@@ -32,6 +32,9 @@
 #define COUNTERS_SHARED                                                              \
 	struct {                                                                     \
 		uint16_t flags;                         /* COUNTERS_SHARED_F flags */\
+	};
+#define COUNTERS_SHARED_TG                                                           \
+	struct {                                                                     \
 		unsigned long last_change;              /* last time, when the state was changed */\
 		long long srv_aborts;                   /* aborted responses during DATA phase caused by the server */\
 		long long cli_aborts;                   /* aborted responses during DATA phase caused by the client */\
@@ -52,11 +55,14 @@
 // for convenience (generic pointer)
 struct counters_shared {
 	COUNTERS_SHARED;
+	struct {
+		COUNTERS_SHARED_TG;
+	} *tg[MAX_TGROUPS];
 };
 
 /* counters used by listeners and frontends */
-struct fe_counters_shared {
-	COUNTERS_SHARED;
+struct fe_counters_shared_tg {
+	COUNTERS_SHARED_TG;
 
 	long long denied_sess;                  /* denied session requests (tcp-req-sess rules) */
 	long long denied_conn;                  /* denied connection requests (tcp-req-conn rules) */
@@ -74,12 +80,15 @@ struct fe_counters_shared {
 			long long cache_lookups;/* cache lookups */
 			long long comp_rsp;     /* number of compressed responses */
 			long long rsp[6];       /* http response codes */
-
-
 		} http;
 	} p;                                    /* protocol-specific stats */
 
 	long long failed_req;                   /* failed requests (eg: invalid or timeout) */
+};
+
+struct fe_counters_shared {
+	COUNTERS_SHARED;
+	struct fe_counters_shared_tg *tg[MAX_TGROUPS];
 };
 
 struct fe_counters {
@@ -99,8 +108,8 @@ struct fe_counters {
 	} p;                                    /* protocol-specific stats */
 };
 
-struct be_counters_shared {
-	COUNTERS_SHARED;
+struct be_counters_shared_tg {
+	COUNTERS_SHARED_TG;
 
 	long long  cum_lbconn;                  /* cumulated number of sessions processed by load balancing (BE only) */
 
@@ -127,6 +136,11 @@ struct be_counters_shared {
 	long long retries;                      /* retried and redispatched connections (BE only) */
 	long long failed_resp;                  /* failed responses (BE only) */
 	long long failed_conns;                 /* failed connect() attempts (BE only) */
+};
+
+struct be_counters_shared {
+	COUNTERS_SHARED;
+	struct be_counters_shared_tg *tg[MAX_TGROUPS];
 };
 
 /* counters used by servers and backends */

include/haproxy/counters.h
@@ -22,6 +22,8 @@
 #ifndef _HAPROXY_COUNTERS_H
 # define _HAPROXY_COUNTERS_H
 
+#include <stddef.h>
+
 #include <haproxy/counters-t.h>
 #include <haproxy/guid-t.h>
 
@@ -30,5 +32,71 @@ struct be_counters_shared *counters_be_shared_get(const struct guid_node *guid);
 
 void counters_fe_shared_drop(struct fe_counters_shared *counters);
 void counters_be_shared_drop(struct be_counters_shared *counters);
+
+/* time oriented helper: get last time (relative to current time) on a given
+ * <scounter> array, for <elem> member (one member per thread group) which is
+ * assumed to be unsigned long type.
+ *
+ * wrapping is handled by taking the lowest diff between now and last counter.
+ * But since wrapping is expected once every ~136 years (starting 01/01/1970),
+ * perhaps it's not worth the extra CPU cost.. let's see.
+ */
+#define COUNTERS_SHARED_LAST_OFFSET(scounters, type, offset)                  \
+({                                                                            \
+	unsigned long last = HA_ATOMIC_LOAD((type *)((char *)scounters[0] + offset));\
+	unsigned long now_seconds = ns_to_sec(now_ns);                        \
+	int it;                                                               \
+                                                                              \
+	for (it = 1; it < global.nbtgroups; it++) {                           \
+		unsigned long cur = HA_ATOMIC_LOAD((type *)((char *)scounters[it] + offset));\
+		if ((now_seconds - cur) < (now_seconds - last))               \
+			last = cur;                                           \
+        }                                                                     \
+	last;                                                                 \
+})
+
+#define COUNTERS_SHARED_LAST(scounters, elem)                                 \
+({                                                                            \
+        int offset = offsetof(typeof(**scounters), elem);                     \
+        unsigned long last = COUNTERS_SHARED_LAST_OFFSET(scounters, typeof(scounters[0]->elem), offset);  \
+	                                                                      \
+	last;                                                                 \
+})
+
+
+/* generic unsigned integer addition for all <elem> members from
+ * <scounters> array (one member per thread group)
+ * <rfunc> is function taking pointer as parameter to read from the memory
+ * location pointed to scounters[it].elem
+ */
+#define COUNTERS_SHARED_TOTAL_OFFSET(scounters, type, offset, rfunc)          \
+({                                                                            \
+	uint64_t __ret = 0;                                                   \
+        int it;                                                               \
+                                                                              \
+	for (it = 0; it < global.nbtgroups; it++)                             \
+		__ret += rfunc((type *)((char *)scounters[it] + offset));     \
+	__ret;                                                                \
+})
+
+#define COUNTERS_SHARED_TOTAL(scounters, elem, rfunc)                         \
+({                                                                            \
+	int offset = offsetof(typeof(**scounters), elem);                     \
+	uint64_t __ret = COUNTERS_SHARED_TOTAL_OFFSET(scounters, typeof(scounters[0]->elem), offset, rfunc);\
+                                                                              \
+	__ret;                                                                \
+})
+/* same as COUNTERS_SHARED_TOTAL but with <rfunc> taking 2 extras arguments:
+ * <arg1> and <arg2>
+ */
+#define COUNTERS_SHARED_TOTAL_ARG2(scounters, elem, rfunc, arg1, arg2)        \
+({                                                                            \
+	uint64_t __ret = 0;                                                   \
+	int it;                                                               \
+                                                                              \
+	for (it = 0; it < global.nbtgroups; it++)                             \
+		__ret += rfunc(&scounters[it]->elem, arg1, arg2);             \
+	__ret;                                                                \
+})
 
 #endif /* _HAPROXY_COUNTERS_H */

include/haproxy/proxy.h
@@ -136,10 +136,10 @@ static inline void proxy_reset_timeouts(struct proxy *proxy)
 /* increase the number of cumulated connections received on the designated frontend */
 static inline void proxy_inc_fe_conn_ctr(struct listener *l, struct proxy *fe)
 {
-	_HA_ATOMIC_INC(&fe->fe_counters.shared->cum_conn);
+	_HA_ATOMIC_INC(&fe->fe_counters.shared->tg[tgid - 1]->cum_conn);
 	if (l && l->counters)
-		_HA_ATOMIC_INC(&l->counters->shared->cum_conn);
-	update_freq_ctr(&fe->fe_counters.shared->conn_per_sec, 1);
+		_HA_ATOMIC_INC(&l->counters->shared->tg[tgid - 1]->cum_conn);
+	update_freq_ctr(&fe->fe_counters.shared->tg[tgid - 1]->conn_per_sec, 1);
 	HA_ATOMIC_UPDATE_MAX(&fe->fe_counters.cps_max,
 	                     update_freq_ctr(&fe->fe_counters._conn_per_sec, 1));
 }
@@ -148,10 +148,10 @@ static inline void proxy_inc_fe_conn_ctr(struct listener *l, struct proxy *fe)
 static inline void proxy_inc_fe_sess_ctr(struct listener *l, struct proxy *fe)
 {
 
-	_HA_ATOMIC_INC(&fe->fe_counters.shared->cum_sess);
+	_HA_ATOMIC_INC(&fe->fe_counters.shared->tg[tgid - 1]->cum_sess);
 	if (l && l->counters)
-		_HA_ATOMIC_INC(&l->counters->shared->cum_sess);
-	update_freq_ctr(&fe->fe_counters.shared->sess_per_sec, 1);
+		_HA_ATOMIC_INC(&l->counters->shared->tg[tgid - 1]->cum_sess);
+	update_freq_ctr(&fe->fe_counters.shared->tg[tgid - 1]->sess_per_sec, 1);
 	HA_ATOMIC_UPDATE_MAX(&fe->fe_counters.sps_max,
 			     update_freq_ctr(&fe->fe_counters._sess_per_sec, 1));
 }
@@ -163,19 +163,19 @@ static inline void proxy_inc_fe_cum_sess_ver_ctr(struct listener *l, struct prox
                                                  unsigned int http_ver)
 {
 	if (http_ver == 0 ||
-	    http_ver > sizeof(fe->fe_counters.shared->cum_sess_ver) / sizeof(*fe->fe_counters.shared->cum_sess_ver))
+	    http_ver > sizeof(fe->fe_counters.shared->tg[tgid - 1]->cum_sess_ver) / sizeof(*fe->fe_counters.shared->tg[tgid - 1]->cum_sess_ver))
 	    return;
 
-	_HA_ATOMIC_INC(&fe->fe_counters.shared->cum_sess_ver[http_ver - 1]);
+	_HA_ATOMIC_INC(&fe->fe_counters.shared->tg[tgid - 1]->cum_sess_ver[http_ver - 1]);
 	if (l && l->counters)
-		_HA_ATOMIC_INC(&l->counters->shared->cum_sess_ver[http_ver - 1]);
+		_HA_ATOMIC_INC(&l->counters->shared->tg[tgid - 1]->cum_sess_ver[http_ver - 1]);
 }
 
 /* increase the number of cumulated streams on the designated backend */
 static inline void proxy_inc_be_ctr(struct proxy *be)
 {
-	_HA_ATOMIC_INC(&be->be_counters.shared->cum_sess);
-	update_freq_ctr(&be->be_counters.shared->sess_per_sec, 1);
+	_HA_ATOMIC_INC(&be->be_counters.shared->tg[tgid - 1]->cum_sess);
+	update_freq_ctr(&be->be_counters.shared->tg[tgid - 1]->sess_per_sec, 1);
 	HA_ATOMIC_UPDATE_MAX(&be->be_counters.sps_max,
 			     update_freq_ctr(&be->be_counters._sess_per_sec, 1));
 }
@@ -187,13 +187,13 @@ static inline void proxy_inc_be_ctr(struct proxy *be)
 static inline void proxy_inc_fe_req_ctr(struct listener *l, struct proxy *fe,
                                         unsigned int http_ver)
 {
-	if (http_ver >= sizeof(fe->fe_counters.shared->p.http.cum_req) / sizeof(*fe->fe_counters.shared->p.http.cum_req))
+	if (http_ver >= sizeof(fe->fe_counters.shared->tg[tgid - 1]->p.http.cum_req) / sizeof(*fe->fe_counters.shared->tg[tgid - 1]->p.http.cum_req))
 	    return;
 
-	_HA_ATOMIC_INC(&fe->fe_counters.shared->p.http.cum_req[http_ver]);
+	_HA_ATOMIC_INC(&fe->fe_counters.shared->tg[tgid - 1]->p.http.cum_req[http_ver]);
 	if (l && l->counters)
-		_HA_ATOMIC_INC(&l->counters->shared->p.http.cum_req[http_ver]);
-	update_freq_ctr(&fe->fe_counters.shared->req_per_sec, 1);
+		_HA_ATOMIC_INC(&l->counters->shared->tg[tgid - 1]->p.http.cum_req[http_ver]);
+	update_freq_ctr(&fe->fe_counters.shared->tg[tgid - 1]->req_per_sec, 1);
 	HA_ATOMIC_UPDATE_MAX(&fe->fe_counters.p.http.rps_max,
 	                     update_freq_ctr(&fe->fe_counters.p.http._req_per_sec, 1));
 }

include/haproxy/server.h
@@ -181,8 +181,8 @@ const struct mux_ops *srv_get_ws_proto(struct server *srv);
 /* increase the number of cumulated streams on the designated server */
 static inline void srv_inc_sess_ctr(struct server *s)
 {
-	_HA_ATOMIC_INC(&s->counters.shared->cum_sess);
-	update_freq_ctr(&s->counters.shared->sess_per_sec, 1);
+	_HA_ATOMIC_INC(&s->counters.shared->tg[tgid - 1]->cum_sess);
+	update_freq_ctr(&s->counters.shared->tg[tgid - 1]->sess_per_sec, 1);
 	HA_ATOMIC_UPDATE_MAX(&s->counters.sps_max,
 	                     update_freq_ctr(&s->counters._sess_per_sec, 1));
 }
@@ -190,7 +190,7 @@ static inline void srv_inc_sess_ctr(struct server *s)
 /* set the time of last session on the designated server */
 static inline void srv_set_sess_last(struct server *s)
 {
-	HA_ATOMIC_STORE(&s->counters.shared->last_sess,  ns_to_sec(now_ns));
+	HA_ATOMIC_STORE(&s->counters.shared->tg[tgid - 1]->last_sess,  ns_to_sec(now_ns));
 }
 
 /* returns the current server throttle rate between 0 and 100% */

include/haproxy/stream.h
@@ -362,8 +362,8 @@ static inline void stream_choose_redispatch(struct stream *s)
 		s->scb->state = SC_ST_REQ;
 	} else {
 		if (objt_server(s->target))
-			_HA_ATOMIC_INC(&__objt_server(s->target)->counters.shared->retries);
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->retries);
+			_HA_ATOMIC_INC(&__objt_server(s->target)->counters.shared->tg[tgid - 1]->retries);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->retries);
 		s->scb->state = SC_ST_ASS;
 	}
 

src/backend.c
@@ -27,6 +27,7 @@
 #include <haproxy/backend.h>
 #include <haproxy/channel.h>
 #include <haproxy/check.h>
+#include <haproxy/counters.h>
 #include <haproxy/frontend.h>
 #include <haproxy/global.h>
 #include <haproxy/hash.h>
@@ -824,8 +825,8 @@ int assign_server(struct stream *s)
 			goto out;
 		}
 		else if (srv != prev_srv) {
-			_HA_ATOMIC_INC(&s->be->be_counters.shared->cum_lbconn);
-			_HA_ATOMIC_INC(&srv->counters.shared->cum_lbconn);
+			_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->cum_lbconn);
+			_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->cum_lbconn);
 		}
 		s->target = &srv->obj_type;
 	}
@@ -999,11 +1000,11 @@ int assign_server_and_queue(struct stream *s)
 					s->txn->flags |= TX_CK_DOWN;
 				}
 				s->flags |= SF_REDISP;
-				_HA_ATOMIC_INC(&prev_srv->counters.shared->redispatches);
-				_HA_ATOMIC_INC(&s->be->be_counters.shared->redispatches);
+				_HA_ATOMIC_INC(&prev_srv->counters.shared->tg[tgid - 1]->redispatches);
+				_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->redispatches);
 			} else {
-				_HA_ATOMIC_INC(&prev_srv->counters.shared->retries);
-				_HA_ATOMIC_INC(&s->be->be_counters.shared->retries);
+				_HA_ATOMIC_INC(&prev_srv->counters.shared->tg[tgid - 1]->retries);
+				_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->retries);
 			}
 		}
 	}
@@ -2084,13 +2085,13 @@ int connect_server(struct stream *s)
 		s->scb->flags |= SC_FL_NOLINGER;
 
 	if (s->flags & SF_SRV_REUSED) {
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->reuse);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->reuse);
 		if (srv)
-			_HA_ATOMIC_INC(&srv->counters.shared->reuse);
+			_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->reuse);
 	} else {
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->connect);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->connect);
 		if (srv)
-			_HA_ATOMIC_INC(&srv->counters.shared->connect);
+			_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->connect);
 	}
 
 	err = do_connect_server(s, srv_conn);
@@ -2279,8 +2280,8 @@ int srv_redispatch_connect(struct stream *s)
 			s->conn_err_type = STRM_ET_QUEUE_ERR;
 		}
 
-		_HA_ATOMIC_INC(&srv->counters.shared->failed_conns);
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+		_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->failed_conns);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 		return 1;
 
 	case SRV_STATUS_NOSRV:
@@ -2289,7 +2290,7 @@ int srv_redispatch_connect(struct stream *s)
 			s->conn_err_type = STRM_ET_CONN_ERR;
 		}
 
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 		return 1;
 
 	case SRV_STATUS_QUEUED:
@@ -2318,8 +2319,8 @@ int srv_redispatch_connect(struct stream *s)
 		if (srv)
 			srv_set_sess_last(srv);
 		if (srv)
-			_HA_ATOMIC_INC(&srv->counters.shared->failed_conns);
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+			_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->failed_conns);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 
 		/* release other streams waiting for this server */
 		if (may_dequeue_tasks(srv, s->be))
@@ -2393,8 +2394,8 @@ void back_try_conn_req(struct stream *s)
 			if (srv)
 				srv_set_sess_last(srv);
 			if (srv)
-				_HA_ATOMIC_INC(&srv->counters.shared->failed_conns);
-			_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+				_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->failed_conns);
+			_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 
 			/* release other streams waiting for this server */
 			sess_change_server(s, NULL);
@@ -2460,8 +2461,8 @@ void back_try_conn_req(struct stream *s)
 			pendconn_cond_unlink(s->pend_pos);
 
 			if (srv)
-				_HA_ATOMIC_INC(&srv->counters.shared->failed_conns);
-			_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+				_HA_ATOMIC_INC(&srv->counters.shared->tg[tgid - 1]->failed_conns);
+			_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 			sc_abort(sc);
 			sc_shutdown(sc);
 			req->flags |= CF_WRITE_TIMEOUT;
@@ -2716,8 +2717,8 @@ void back_handle_st_cer(struct stream *s)
 		}
 
 		if (objt_server(s->target))
-			_HA_ATOMIC_INC(&objt_server(s->target)->counters.shared->failed_conns);
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->failed_conns);
+			_HA_ATOMIC_INC(&objt_server(s->target)->counters.shared->tg[tgid - 1]->failed_conns);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->failed_conns);
 		sess_change_server(s, NULL);
 		if (may_dequeue_tasks(objt_server(s->target), s->be))
 			process_srv_queue(objt_server(s->target));
@@ -2749,8 +2750,8 @@ void back_handle_st_cer(struct stream *s)
 			s->conn_err_type = STRM_ET_CONN_OTHER;
 
 		if (objt_server(s->target))
-			_HA_ATOMIC_INC(&objt_server(s->target)->counters.shared->internal_errors);
-		_HA_ATOMIC_INC(&s->be->be_counters.shared->internal_errors);
+			_HA_ATOMIC_INC(&objt_server(s->target)->counters.shared->tg[tgid - 1]->internal_errors);
+		_HA_ATOMIC_INC(&s->be->be_counters.shared->tg[tgid - 1]->internal_errors);
 		sess_change_server(s, NULL);
 		if (may_dequeue_tasks(objt_server(s->target), s->be))
 			process_srv_queue(objt_server(s->target));
@@ -2887,8 +2888,8 @@ void back_handle_st_rdy(struct stream *s)
  */
 void set_backend_down(struct proxy *be)
 {
-	HA_ATOMIC_STORE(&be->be_counters.shared->last_change, ns_to_sec(now_ns));
-	_HA_ATOMIC_INC(&be->be_counters.shared->down_trans);
+	HA_ATOMIC_STORE(&be->be_counters.shared->tg[tgid - 1]->last_change, ns_to_sec(now_ns));
+	_HA_ATOMIC_INC(&be->be_counters.shared->tg[tgid - 1]->down_trans);
 
 	if (!(global.mode & MODE_STARTING)) {
 		ha_alert("%s '%s' has no server available!\n", proxy_type_str(be), be->id);
@@ -2960,7 +2961,7 @@ no_cookie:
 }
 
 int be_downtime(struct proxy *px) {
-	unsigned long last_change = HA_ATOMIC_LOAD(&px->be_counters.shared->last_change);
+	unsigned long last_change = COUNTERS_SHARED_LAST(px->be_counters.shared->tg, last_change);
 
 	if (px->lbprm.tot_weight && last_change < ns_to_sec(now_ns))  // ignore negative time
 		return px->down_time;
@@ -3403,7 +3404,7 @@ smp_fetch_be_sess_rate(const struct arg *args, struct sample *smp, const char *k
 
 	smp->flags = SMP_F_VOL_TEST;
 	smp->data.type = SMP_T_SINT;
-	smp->data.u.sint = read_freq_ctr(&px->be_counters.shared->sess_per_sec);
+	smp->data.u.sint = COUNTERS_SHARED_TOTAL(px->be_counters.shared->tg, sess_per_sec, read_freq_ctr);
 	return 1;
 }
 
@@ -3586,7 +3587,7 @@ smp_fetch_srv_sess_rate(const struct arg *args, struct sample *smp, const char *
 {
 	smp->flags = SMP_F_VOL_TEST;
 	smp->data.type = SMP_T_SINT;
-	smp->data.u.sint = read_freq_ctr(&args->data.srv->counters.shared->sess_per_sec);
+	smp->data.u.sint = COUNTERS_SHARED_TOTAL(args->data.srv->counters.shared->tg, sess_per_sec, read_freq_ctr);
 	return 1;
 }
 

commit b5067a972c6bbbe52344c76aed15f437f9bb5cdd

BUILD: listener: fix 'for' loop inline variable declaration
commit 16eb0fab3 ("MAJOR: counters: dispatch counters over thread groups")
introduced a build regression on some compilers:

  src/listener.c: In function 'listener_accept':
  src/listener.c:1095:3: error: 'for' loop initial declarations are only allowed in C99 mode
     for (int it = 0; it < global.nbtgroups; it++)
     ^
  src/listener.c:1095:3: note: use option -std=c99 or -std=gnu99 to compile your code
  src/listener.c:1101:4: error: 'for' loop initial declarations are only allowed in C99 mode
      for (int it = 0; it < global.nbtgroups; it++) {
      ^
  make: *** [src/listener.o] Error 1
  make: *** Waiting for unfinished jobs....

Let's fix that.
No backport needed

src/listener.c
@@ -1091,14 +1091,15 @@ void listener_accept(struct listener *l)
 #endif
 	if (p && p->fe_sps_lim) {
 		int max = 0;
+		int it;
 
-		for (int it = 0; it < global.nbtgroups; it++)
+		for (it = 0; it < global.nbtgroups; it++)
 			max += freq_ctr_remain(&p->fe_counters.shared->tg[it]->sess_per_sec, p->fe_sps_lim, 0);
 
 		if (unlikely(!max)) {
 			unsigned int min_wait = 0;
 
-			for (int it = 0; it < global.nbtgroups; it++) {
+			for (it = 0; it < global.nbtgroups; it++) {
 				unsigned int cur_wait = next_event_delay(&p->fe_counters.shared->tg[it]->sess_per_sec, p->fe_sps_lim, 0);
 				if (!it || cur_wait < min_wait)
 					min_wait = cur_wait;