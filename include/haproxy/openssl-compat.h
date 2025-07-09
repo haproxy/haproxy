c01eb1040ecffb4bdda84be1c8522dc370e0f202
Frederic Lecaille <flecaille@haproxy.com>
2025-07-09 14:31:36 +0200

    MINOR: quic: Prevent QUIC build with OpenSSL 3.5 new QUIC API version < 3.5.1
    The QUIC listener part was impacted by the 3.5.0 OpenSSL new QUIC API with several
    issues which have been fixed by 3.5.1.
    
    Add a #error to prevent such OpenSSL 3.5 new QUIC API use with version below 3.5.1.
    
    Must be backported to 3.2.

--- include/haproxy/openssl-compat.h
+++ include/haproxy/openssl-compat.h
@@ -52,6 +52,11 @@
 /* This macro is defined by the new OpenSSL 3.5.0 QUIC TLS API and it is not
  * defined by quictls.
  */
+
+#if defined(USE_QUIC) && (OPENSSL_VERSION_NUMBER < 0x30500010L)
+#error "OpenSSL 3.5 QUIC API should only be used with OpenSSL 3.5.1 version and newer"
+#endif
+
 #define HAVE_OPENSSL_QUIC
 #define SSL_set_quic_transport_params   SSL_set_quic_tls_transport_params
 #define SSL_set_quic_early_data_enabled SSL_set_quic_tls_early_data_enabled
