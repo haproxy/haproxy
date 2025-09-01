haproxy/haproxy

commit 6ea50ba462692d6dcf301081f23cab3e0f6086e4

MINOR: acl; Warn when matching method based on a suffix is overwritten
From time to time, issues are reported about string matching based on suffix
(for instance path_beg). Each time, it appears these ACLs are used in
conjunction with a converter or followed by an explicit matching method
(-m).

Unfortunatly, it is not an issue but an expected behavior, while it is not
obvious. matching suffixes can be consider as aliases on the corresponding
'-m' matching method. Thus "path_beg" is equivalent to "path -m beg". When a
converter is used the original matching (string) is used and the suffix is
lost. When followed by an explicit matching method, it overwrites the
matching method based on the suffix.

It is expected but confusing. Thus now a warning is emitted because it is a
configuration issue for sure. Following sample fetch functions are concerned:

 * base
 * path
 * req.cook
 * req.hdr
 * res.hdr
 * url
 * urlp

The configuration manual was modified to make it less ambiguous.

src/acl.c
@@ -406,6 +406,15 @@ struct acl_expr *parse_acl_expr(const char **args, char **err, struct arg_list *
 		goto out_free_expr;
 	}
 
+	if (aclkw) {
+		if (((aclkw->match_type == PAT_MATCH_BEG || aclkw->match_type == PAT_MATCH_DIR || aclkw->match_type == PAT_MATCH_DOM ||
+		     aclkw->match_type == PAT_MATCH_DOM || aclkw->match_type == PAT_MATCH_END || aclkw->match_type == PAT_MATCH_LEN ||
+		     aclkw->match_type == PAT_MATCH_REG) && expr->pat.match != pat_match_fcts[aclkw->match_type]) ||
+		    (aclkw->match && expr->pat.match != pat_match_fcts[aclkw->match_type]))
+			ha_warning("parsing [%s:%d] : original matching method '%s' was overwritten and will not be applied as expected.\n",
+				   file, line, aclkw->kw);
+	}
+
 	/* Create displayed reference */
 	snprintf(trash.area, trash.size, "acl '%s' file '%s' line %d",
 		 expr->kw, file, line);

commit 8c555a4a4ee96d11d07bc800bf09a6bb970ea7e1

BUG/MINOR: acl: Properly detect overwritten matching method
A bug was introduced by the commit 6ea50ba46 ("MINOR: acl; Warn when
matching method based on a suffix is overwritten"). The test on the match
function, when defined was not correct. It is now fixed.

No backport needed, except if the commit above is backported.

src/acl.c
@@ -410,7 +410,7 @@ struct acl_expr *parse_acl_expr(const char **args, char **err, struct arg_list *
 		if (((aclkw->match_type == PAT_MATCH_BEG || aclkw->match_type == PAT_MATCH_DIR || aclkw->match_type == PAT_MATCH_DOM ||
 		     aclkw->match_type == PAT_MATCH_DOM || aclkw->match_type == PAT_MATCH_END || aclkw->match_type == PAT_MATCH_LEN ||
 		     aclkw->match_type == PAT_MATCH_REG) && expr->pat.match != pat_match_fcts[aclkw->match_type]) ||
-		    (aclkw->match && expr->pat.match != pat_match_fcts[aclkw->match_type]))
+		    (aclkw->match && expr->pat.match != aclkw->match))
 			ha_warning("parsing [%s:%d] : original matching method '%s' was overwritten and will not be applied as expected.\n",
 				   file, line, aclkw->kw);
 	}