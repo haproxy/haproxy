haproxy/haproxy

commit 37abe56b186356f570d63d0f6e2fb4b851ae8660

BUG/MEDIUM: resolvers: Properly cache do-resolv resolution
As stated by the documentation, when a do-resolv resolution is performed,
the result should be cached for <hold.valid> milliseconds. However, the only
way to cache the result is to always have a requester. When the last
requester is unlink from the resolution, the resolution is released. So, for
a do-resolv resolution, it means it could only work by chance if the same
FQDN is requested enough to always have at least two streams waiting for the
resolution. And because in that case, the cached result is used, it means
the traffic must be quite high.

In fact, a good approach to fix the issue is to keep orphan resolutions to
be able cache the result and only release them after hold.valid milliseconds
after the last real resolution. The resolver's task already releases orphan
resolutions. So we only need to check the expiration date and take care to
not release the resolution when the last stream is unlink from it.

This patch should be backported to all stable versions. We can start to
backport it as far as 3.1 and then wait a bit.

src/resolvers.c
@@ -370,7 +370,7 @@ static inline uint16_t resolv_rnd16(void)
 
 static inline int resolv_resolution_timeout(struct resolv_resolution *res)
 {
-	return res->resolvers->timeout.resolve;
+	return (!LIST_ISEMPTY(&res->requesters) ? res->resolvers->timeout.resolve : res->resolvers->hold.valid);
 }
 
 /* Updates a resolvers' task timeout for next wake up and queue it */
@@ -2205,7 +2205,19 @@ static void _resolv_unlink_resolution(struct resolv_requester *requester)
 	if (!LIST_ISEMPTY(&res->requesters))
 		req = LIST_NEXT(&res->requesters, struct resolv_requester *, list);
 	else {
-		abort_resolution(res);
+		/* If the last requester was a stream and the resolution was a
+		 * success, keep it to use it as a cache for <hold.valid>
+		 * milliseconds.
+		 */
+		if (obj_type(requester->owner) != OBJ_TYPE_STREAM ||
+		    res->status != RSLV_STATUS_VALID ||
+		    res->resolvers->hold.valid == 0)
+			abort_resolution(res);
+		else {
+			if (!tick_isset(res->last_resolution))
+				res->last_resolution = now_ms;
+			resolv_update_resolvers_timeout(res->resolvers);
+		}
 		return;
 	}
 
@@ -2549,7 +2561,12 @@ struct task *process_resolvers(struct task *t, void *context, unsigned int state
 		}
 
 		if (LIST_ISEMPTY(&res->requesters)) {
-			abort_resolution(res);
+			/* Abort inactive resolution only if expired */
+			exp = tick_add(res->last_resolution, resolvers->hold.valid);
+			if (!tick_isset(res->last_resolution) || tick_is_expired(exp, now_ms)) {
+				abort_resolution(res);
+				res = resback;
+			}
 			continue;
 		}
 

commit 6cf2401edaf80806828526d214d1a9d5e25ba5d9

BUG/MEDIUM: resolvers: Make resolution owns its hostname_dn value
The commit 37abe56b1 ("BUG/MEDIUM: resolvers: Properly cache do-resolv
resolution") introduced a regression. A resolution does not own its
hostname_dn value, it is a pointer on the first request value. But since the
commit above, it is possible to have orphan resolution, with no
requester. So it is important to modify the resolutions to make it owns its
hostname_dn value by duplicating it when it is created.

This patch must be backported with the commit above.

src/resolvers.c
@@ -1940,10 +1940,10 @@ static struct resolv_resolution *resolv_pick_resolution(struct resolvers *resolv
 							char **hostname_dn, int hostname_dn_len,
 							int query_type)
 {
-	struct resolv_resolution *res;
+	struct resolv_resolution *res = NULL;
 
 	if (!*hostname_dn)
-		goto from_pool;
+		goto err;
 
 	/* Search for same hostname and query type in resolutions.curr */
 	list_for_each_entry(res, &resolvers->resolutions.curr, list) {
@@ -1981,7 +1981,9 @@ static struct resolv_resolution *resolv_pick_resolution(struct resolvers *resolv
 
 		res->prefered_query_type = query_type;
 		res->query_type          = query_type;
-		res->hostname_dn         = *hostname_dn;
+		res->hostname_dn         = strdup(*hostname_dn);
+		if (res->hostname_dn == NULL)
+			goto err;
 		res->hostname_dn_len     = hostname_dn_len;
 
 		++resolution_uuid;
@@ -1990,6 +1992,10 @@ static struct resolv_resolution *resolv_pick_resolution(struct resolvers *resolv
 		LIST_APPEND(&resolvers->resolutions.wait, &res->list);
 	}
 	return res;
+  err:
+	if (res)
+		resolv_free_resolution(res);
+	return NULL;
 }
 
 /* deletes and frees all answer_items from the resolution's answer_list */
@@ -2016,7 +2022,7 @@ static void resolv_free_resolution(struct resolv_resolution *resolution)
 
 	/* clean up configuration */
 	resolv_reset_resolution(resolution);
-	resolution->hostname_dn = NULL;
+	ha_free(&resolution->hostname_dn);
 	resolution->hostname_dn_len = 0;
 
 	list_for_each_entry_safe(req, reqback, &resolution->requesters, list) {
@@ -2193,7 +2199,6 @@ void resolv_detach_from_resolution_answer_items(struct resolv_resolution *res,  
 static void _resolv_unlink_resolution(struct resolv_requester *requester)
 {
 	struct resolv_resolution *res;
-	struct resolv_requester  *req;
 
 	/* Nothing to do */
 	if (!requester || !requester->resolution)
@@ -2208,9 +2213,7 @@ static void _resolv_unlink_resolution(struct resolv_requester *requester)
 	resolv_detach_from_resolution_answer_items(res,  requester);
 
 	/* We need to find another requester linked on this resolution */
-	if (!LIST_ISEMPTY(&res->requesters))
-		req = LIST_NEXT(&res->requesters, struct resolv_requester *, list);
-	else {
+	if (LIST_ISEMPTY(&res->requesters)) {
 		/* If the last requester was a stream and the resolution was a
 		 * success, keep it to use it as a cache for <hold.valid>
 		 * milliseconds.
@@ -2225,26 +2228,6 @@ static void _resolv_unlink_resolution(struct resolv_requester *requester)
 			resolv_update_resolvers_timeout(res->resolvers);
 		}
 		return;
-	}
-
-	/* Move hostname_dn related pointers to the next requester */
-	switch (obj_type(req->owner)) {
-		case OBJ_TYPE_SERVER:
-			res->hostname_dn     = __objt_server(req->owner)->hostname_dn;
-			res->hostname_dn_len = __objt_server(req->owner)->hostname_dn_len;
-			break;
-		case OBJ_TYPE_SRVRQ:
-			res->hostname_dn     = __objt_resolv_srvrq(req->owner)->hostname_dn;
-			res->hostname_dn_len = __objt_resolv_srvrq(req->owner)->hostname_dn_len;
-			break;
-		case OBJ_TYPE_STREAM:
-			res->hostname_dn     = __objt_stream(req->owner)->resolv_ctx.hostname_dn;
-			res->hostname_dn_len = __objt_stream(req->owner)->resolv_ctx.hostname_dn_len;
-			break;
-		default:
-			res->hostname_dn     = NULL;
-			res->hostname_dn_len = 0;
-			break;
 	}
 }
 