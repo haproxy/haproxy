haproxy/haproxy

commit 88aa7a780c7d93faea4bc968fc56ce0527e47075

MINOR: http-client: Trigger an error if first response block isn't a start-line
The first HTX block of a response must be a start-line. There is no reason
to wait for something else. And if there are output data in the response
channel buffer, it means we must found the start-line.

src/http_client.c
@@ -654,13 +654,10 @@ void httpclient_applet_io_handler(struct appctx *appctx)
 				if (!co_data(res))
 					goto out;
 				htx = htxbuf(&res->buf);
-				if (htx_is_empty(htx))
-					goto out;
-				blk = htx_get_head_blk(htx);
-				if (blk && (htx_get_blk_type(blk) == HTX_BLK_RES_SL))
-					sl = htx_get_blk_ptr(htx, blk);
-				if (!sl || (!(sl->flags & HTX_SL_F_IS_RESP)))
-					goto out;
+				if (htx_get_first_type(htx) != HTX_BLK_RES_SL)
+					goto error;
+				blk = DISGUISE(htx_get_head_blk(htx));
+                                sl = htx_get_blk_ptr(htx, blk);
 
 				/* Skipp any 1XX interim responses */
 				if (sl->info.res.status < 200) {

commit 331689d2167520ccd5c6098802db468fd9237e39

BUG/MEDIUM: http-client: Fix the test on the response start-line
The commit 88aa7a780 ("MINOR: http-client: Trigger an error if first
response block isn't a start-line") introduced a bug. From an endpoint, an
applet or a mux, the <first> index must never be used. It is reserved to the
HTTP analyzers. From endpoint, this value may be undefined or just point on
any other block that the first one. Instead we must always get the head
block.

In taht case, to be sure the first HTX block in a response is a start-line,
we must use htx_get_head_type() function instead of htx_get_first_type().
Otherwise, we can trigger an error while the response is in fact properly
formatted.

It is a 3.3-speific issue. cNo backport needed.

src/http_client.c
@@ -653,7 +653,7 @@ void httpclient_applet_io_handler(struct appctx *appctx)
 
 				/* copy the start line in the hc structure,then remove the htx block */
 				htx = htxbuf(inbuf);
-				if (htx_get_first_type(htx) != HTX_BLK_RES_SL)
+				if (htx_get_head_type(htx) != HTX_BLK_RES_SL)
 					goto error;
 				blk = DISGUISE(htx_get_head_blk(htx));
 				sl = htx_get_blk_ptr(htx, blk);