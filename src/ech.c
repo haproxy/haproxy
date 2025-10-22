369b46493253e9af0057f228ca9b5b72317c3310
William Lallemand <wlallemand@haproxy.com>
2025-10-22 16:28:16 +0200

    add age-in-secs in 'del ssh ech'

--- src/ech.c
+++ src/ech.c
@@ -416,7 +416,7 @@ static struct cli_kw_list cli_kws = {{ },{
     { { "show", "ssl", "ech", NULL},  "show ssl ech [<name>]                   : display a named ECH configuation or all",      cli_parse_show_ech, cli_io_handler_ech_details },
     { { "add", "ssl", "ech", NULL },  "add ssl ech <name> <payload>            : add a new PEM-formatted ECH config and key ",  cli_parse_add_ech, NULL, NULL },
     { { "set", "ssl", "ech", NULL },  "set ssl ech <name> <payload>            : replace all ECH configs with that provided",   cli_parse_set_ech, NULL, NULL },
-    { { "del", "ssl", "ech", NULL },  "del ssl ech <name>                      : delete ECH configs",                          cli_parse_del_ech, NULL, NULL },
+    { { "del", "ssl", "ech", NULL },  "del ssl ech <name> [<age-in-secs>]      : delete ECH configs",                          cli_parse_del_ech, NULL, NULL },
     { { NULL }, NULL, NULL, NULL, NULL },
 
 }};
