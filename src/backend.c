haproxy/haproxy

commit 95ac5fe4a800c882484c930b9d897803c86b7604

MEDIUM: ssl_sock: always use the SSL's server name, not the one from the tid
In ssl_sock_set_servername(), we're retrieving the current server name
from the current thread, hoping it will not have changed. This is a
bit dangerous as strictly speaking it's not easy to prove that no other
connection had to use one between the moment it was retrieved in
ssl_sock_init() and the moment it's being read here. In addition, this
forces us to maintain one session per thread while this is not the real
need, in practice we only need one session per SNI. And the current model
prevents us from sharing sessions between threads.

This had been done in 2.5 via commit e18d4e828 ("BUG/MEDIUM: ssl: backend
TLS resumption with sni and TLSv1.3"), but as analyzed with William, it
turns out that a saner approach consists in keeping the call to
SSL_get_servername() there and instead to always assign the SNI to the
current SSL context via SSL_set_tlsext_host_name() immediately when the
session is retreived. This way the session and SNI are consulted atomically
and the host name is only checked from the session and not from possibly
changing elements.

As a bonus the rdlock that was added by that commit could now be removed,
though it didn't cost much.

src/ssl_sock.c
@@ -5722,9 +5722,13 @@ static int ssl_sock_init(struct connection *conn, void **xprt_ctx)
 			if (sess && !SSL_set_session(ctx->ssl, sess)) {
 				SSL_SESSION_free(sess);
 				ha_free(&srv->ssl_ctx.reused_sess[tid].ptr);
+				if (srv->ssl_ctx.reused_sess[tid].sni)
+					SSL_set_tlsext_host_name(ctx->ssl, srv->ssl_ctx.reused_sess[tid].sni);
 			} else if (sess) {
 				/* already assigned, not needed anymore */
 				SSL_SESSION_free(sess);
+				if (srv->ssl_ctx.reused_sess[tid].sni)
+					SSL_set_tlsext_host_name(ctx->ssl, srv->ssl_ctx.reused_sess[tid].sni);
 			}
 		}
 		HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.lock);
@@ -6836,7 +6840,6 @@ void ssl_sock_set_servername(struct connection *conn, const char *hostname)
 {
 #ifdef SSL_CTRL_SET_TLSEXT_HOSTNAME
 	struct ssl_sock_ctx *ctx = conn_get_ssl_sock_ctx(conn);
-	struct server *s;
 	char *prev_name;
 
 	if (!ctx)
@@ -6845,22 +6848,21 @@ void ssl_sock_set_servername(struct connection *conn, const char *hostname)
 	BUG_ON(!(conn->flags & CO_FL_WAIT_L6_CONN));
 	BUG_ON(!(conn->flags & CO_FL_SSL_WAIT_HS));
 
-	s = __objt_server(conn->target);
-
 	/* if the SNI changes, we must destroy the reusable context so that a
 	 * new connection will present a new SNI. compare with the SNI
-	 * previously stored in the reused_sess */
-	/* the RWLOCK is used to ensure that we are not trying to flush the
-	 * cache from the CLI */
+	 * previously stored in the reused_sess. If the session was reused,
+	 * the associated SNI (if any) has already been assigned to the SSL
+	 * during ssl_sock_init() so SSL_get_servername() will properly
+	 * retrieve the currently known hostname for the SSL.
+	 */
 
-	HA_RWLOCK_RDLOCK(SSL_SERVER_LOCK, &s->ssl_ctx.lock);
-	prev_name = s->ssl_ctx.reused_sess[tid].sni;
+	prev_name = (char *)SSL_get_servername(ctx->ssl, TLSEXT_NAMETYPE_host_name);
 	if ((!prev_name && hostname) ||
-	    (prev_name && (!hostname || strcmp(hostname, prev_name) != 0)))
+	    !hostname ||
+	    strcmp(hostname, prev_name) != 0) {
 		SSL_set_session(ctx->ssl, NULL);
-	HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &s->ssl_ctx.lock);
-
-	SSL_set_tlsext_host_name(ctx->ssl, hostname);
+		SSL_set_tlsext_host_name(ctx->ssl, hostname);
+	}
 #endif
 }
 

commit 53987a6929ad15841419c2ff1026f985d7116803

BUG/MEDIUM: server/ssl: Don't reuse TLS session if the connection's SNI differs
When a new SSL server connection is created, if no SNI is set, it is
possible to inherit from the one of the reused TLS session. The bug was
introduced by the commit 95ac5fe4a ("MEDIUM: ssl_sock: always use the SSL's
server name, not the one from the tid"). The mixup is possible between
regular connections but also with health-checks connections.

But it is only the visible part of the bug. If the SNI of the cached TLS
session does not match the one of the new connection, no reuse must be
performed at all.

To fix the bug, the connection SNI is now retrieved earlier, to be available
when the SSL context is initialized and when we try to reuse a TLS
session. So before calling conn_prepare() function. This SNI is temporarily
saved in the connection itself to be compared in
ssl_sock_srv_try_reuse_sess() function.

This patch should fix the issue #3195. It must be slowly backported as far
as 3.0.

On 3.3, it relies on:

  * MINOR: connection: Add a SNI member in the connection structure
  * MEDIUM: quic: Add connection as argument when qc_new_conn() is called

On older versions, only the first one must be backported but this patch must
also be adapted. On these versions, ssl_sock_srv_try_reuse_sess() does not
exist and changes must be applied on ssl_sock_init().

WARNING: Because of this patch, the TLS session reuse no longer works on
         WolfSSL if a SNI is used on the connection because the SNI cannot
         be retrieved. It is the reason why the reuse is not performed. But
         on older versions, the SNI is not available too and this remains an
         issue too.

src/backend.c
@@ -2034,9 +2034,21 @@ int connect_server(struct stream *s)
 
 	if (!srv_conn->xprt) {
 		/* set the correct protocol on the output stream connector */
-
 		if (srv) {
 			struct protocol *proto = protocol_lookup(srv_conn->dst->ss_family, srv->addr_type.proto_type, srv->alt_proto);
+#ifdef USE_OPENSSL
+			struct sample *sni_smp = NULL;
+
+			/* Set socket SNI unless connection is reused. */
+			if (srv && srv->xprt && srv->xprt->get_ssl_sock_ctx && srv->ssl_ctx.sni) {
+				srv_conn->sni = NULL;
+				sni_smp = sample_fetch_as_type(s->be, s->sess, s,
+							       SMP_OPT_DIR_REQ | SMP_OPT_FINAL,
+							       srv->ssl_ctx.sni, SMP_T_STR);
+				if (smp_make_safe(sni_smp))
+					srv_conn->sni = sni_smp->data.u.str.area;
+			}
+#endif /* USE_OPENSSL */
 
 			if (conn_prepare(srv_conn, proto, srv->xprt)) {
 				conn_free(srv_conn);
@@ -2153,19 +2165,6 @@ int connect_server(struct stream *s)
 	err = do_connect_server(s, srv_conn);
 	if (err != SF_ERR_NONE)
 		return err;
-
-#ifdef USE_OPENSSL
-	/* Set socket SNI unless connection is reused. */
-	if (conn_is_ssl(srv_conn) && srv && srv->ssl_ctx.sni && !(s->flags & SF_SRV_REUSED)) {
-		struct sample *sni_smp = NULL;
-
-		sni_smp = sample_fetch_as_type(s->be, s->sess, s,
-		                               SMP_OPT_DIR_REQ | SMP_OPT_FINAL,
-		                               srv->ssl_ctx.sni, SMP_T_STR);
-		if (smp_make_safe(sni_smp))
-			ssl_sock_set_servername(srv_conn, sni_smp->data.u.str.area);
-	}
-#endif /* USE_OPENSSL */
 
 	/* The CO_FL_SEND_PROXY flag may have been set by the connect method,
 	 * if so, add our handshake pseudo-XPRT now.

src/quic_ssl.c
@@ -1357,6 +1357,10 @@ int qc_alloc_ssl_sock_ctx(struct quic_conn *qc, void *target)
 			}
 		}
 #endif
+		if (qc->conn && qc->conn->sni) {
+			SSL_set_tlsext_host_name(ctx->ssl, qc->conn->sni);
+			qc->conn->sni = NULL;
+		}
 
 		SSL_set_connect_state(ctx->ssl);
 	}

src/ssl_sock.c
@@ -5688,22 +5688,36 @@ int ssl_sock_srv_try_reuse_sess(struct ssl_sock_ctx *ctx, struct server *srv)
 	 * (transport parameters and ALPN) are successfully reused.
 	 */
 	int ret = qc && (srv->ssl_ctx.options & SRV_SSL_O_EARLY_DATA) ? 0 : 1;
+	struct connection *conn = qc ? qc->conn : ctx->conn;
+
 #else
 	/* Always succeeds for TCP sockets. */
 	int ret = 1;
+	struct connection *conn = ctx->conn;
+
 #endif
 
 	HA_RWLOCK_RDLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.lock);
 	if (srv->ssl_ctx.reused_sess[tid].ptr) {
+		const unsigned char *ptr;
+		SSL_SESSION *sess;
+
+		/* If sni of the cached SSL session does not match the one of
+		 * the new connection, don't reuse it
+		 */
+		if (!conn || (!srv->ssl_ctx.reused_sess[tid].sni && conn->sni) ||
+		    (srv->ssl_ctx.reused_sess[tid].sni && !conn->sni) ||
+		    (conn->sni && strcmp(srv->ssl_ctx.reused_sess[tid].sni, conn->sni) != 0))
+			goto out;
+
 		/* let's recreate a session from (ptr,size) and assign
 		 * it to ctx->ssl. Its refcount will be updated by the
 		 * creation and by the assignment, so after assigning
 		 * it or failing to, we must always free it to decrement
 		 * the refcount.
 		 */
-		const unsigned char *ptr = srv->ssl_ctx.reused_sess[tid].ptr;
-		SSL_SESSION *sess = d2i_SSL_SESSION(NULL, &ptr, srv->ssl_ctx.reused_sess[tid].size);
-
+		ptr = srv->ssl_ctx.reused_sess[tid].ptr;
+		sess = d2i_SSL_SESSION(NULL, &ptr, srv->ssl_ctx.reused_sess[tid].size);
 		if (sess && !SSL_set_session(ctx->ssl, sess)) {
 			uint old_tid = HA_ATOMIC_LOAD(&srv->ssl_ctx.last_ssl_sess_tid); // 0=none, >0 = tid + 1
 			if (old_tid == tid + 1)
@@ -5711,16 +5725,10 @@ int ssl_sock_srv_try_reuse_sess(struct ssl_sock_ctx *ctx, struct server *srv)
 			SSL_SESSION_free(sess);
 			HA_RWLOCK_WRLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
 			ha_free(&srv->ssl_ctx.reused_sess[tid].ptr);
-			HA_RWLOCK_WRTORD(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
-			if (srv->ssl_ctx.reused_sess[tid].sni)
-				SSL_set_tlsext_host_name(ctx->ssl, srv->ssl_ctx.reused_sess[tid].sni);
-			HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
+			HA_RWLOCK_WRUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
 		} else if (sess) {
 			/* already assigned, not needed anymore */
 			SSL_SESSION_free(sess);
-			HA_RWLOCK_RDLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
-			if (srv->ssl_ctx.reused_sess[tid].sni)
-				SSL_set_tlsext_host_name(ctx->ssl, srv->ssl_ctx.reused_sess[tid].sni);
 #ifdef USE_QUIC
 			if (qc && srv->ssl_ctx.options & SRV_SSL_O_EARLY_DATA) {
 				unsigned char *alpn = (unsigned char *)srv->path_params.nego_alpn;
@@ -5731,7 +5739,6 @@ int ssl_sock_srv_try_reuse_sess(struct ssl_sock_ctx *ctx, struct server *srv)
 					ret = 1;
 			}
 #endif
-			HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[tid].sess_lock);
 		}
 	} else {
 		/* No session available yet, let's see if we can pick one
@@ -5746,6 +5753,15 @@ int ssl_sock_srv_try_reuse_sess(struct ssl_sock_ctx *ctx, struct server *srv)
 
 		if (old_tid) {
 			HA_RWLOCK_RDLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[old_tid-1].sess_lock);
+			/* If sni of the cached SSL session does not match the one of
+			 * the new connection, don't reuse it
+			 */
+			if (!conn || (!srv->ssl_ctx.reused_sess[old_tid-1].sni && conn->sni) ||
+			    (srv->ssl_ctx.reused_sess[old_tid-1].sni && !conn->sni) ||
+			    (conn->sni && strcmp(srv->ssl_ctx.reused_sess[old_tid-1].sni, conn->sni) != 0)) {
+				HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[old_tid-1].sess_lock);
+				goto out;
+			}
 
 			ptr = srv->ssl_ctx.reused_sess[old_tid-1].ptr;
 			if (ptr) {
@@ -5766,13 +5782,10 @@ int ssl_sock_srv_try_reuse_sess(struct ssl_sock_ctx *ctx, struct server *srv)
 					SSL_SESSION_free(sess);
 				}
 			}
-
-			if (srv->ssl_ctx.reused_sess[old_tid-1].sni)
-				SSL_set_tlsext_host_name(ctx->ssl, srv->ssl_ctx.reused_sess[old_tid-1].sni);
-
 			HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.reused_sess[old_tid-1].sess_lock);
 		}
 	}
+  out:
 	HA_RWLOCK_RDUNLOCK(SSL_SERVER_LOCK, &srv->ssl_ctx.lock);
 
 	return ret;
@@ -5859,6 +5872,11 @@ static int ssl_sock_init(struct connection *conn, void **xprt_ctx)
 			SSL_set_renegotiate_mode(ctx->ssl, ssl_renegotiate_freely);
 #endif
 		ssl_sock_srv_try_reuse_sess(ctx, srv);
+
+		if (conn && conn->sni) {
+			SSL_set_tlsext_host_name(ctx->ssl, conn->sni);
+			conn->sni = NULL;
+		}
 
 #ifdef HA_USE_KTLS
 		if ((srv->ssl_ctx.options & SRV_SSL_O_KTLS) && !(global.tune.options & GTUNE_NO_KTLS)) {

src/tcpcheck.c
@@ -1456,6 +1456,19 @@ enum tcpcheck_eval_ret tcpcheck_eval_connect(struct check *check, struct tcpchec
 		? xprt_get(XPRT_SSL)
 		: ((connect->options & TCPCHK_OPT_DEFAULT_CONNECT) ? check->xprt : xprt_get(XPRT_RAW)));
 
+
+#ifdef USE_OPENSSL
+	if (xprt && xprt->get_ssl_sock_ctx) {
+		conn->sni = NULL;
+		if (connect->sni)
+			conn->sni = connect->sni;
+		else if ((connect->options & TCPCHK_OPT_DEFAULT_CONNECT) && s && s->check.sni)
+			conn->sni = s->check.sni;
+		else if (auto_sni)
+			conn->sni = b_orig(auto_sni);
+	}
+#endif
+
 	if (conn_prepare(conn, proto, xprt) < 0) {
 		TRACE_ERROR("xprt allocation error", CHK_EV_TCPCHK_CONN|CHK_EV_TCPCHK_ERR, check);
 		status = SF_ERR_RESOURCE;
@@ -1503,13 +1516,6 @@ enum tcpcheck_eval_ret tcpcheck_eval_connect(struct check *check, struct tcpchec
 
 #ifdef USE_OPENSSL
 	if (conn_is_ssl(conn)) {
-		if (connect->sni)
-			ssl_sock_set_servername(conn, connect->sni);
-		else if ((connect->options & TCPCHK_OPT_DEFAULT_CONNECT) && s && s->check.sni)
-			ssl_sock_set_servername(conn, s->check.sni);
-		else if (auto_sni)
-			ssl_sock_set_servername(conn, b_orig(auto_sni));
-
 		if (connect->alpn)
 			ssl_sock_set_alpn(conn, (unsigned char *)connect->alpn, connect->alpn_len);
 		else if ((connect->options & TCPCHK_OPT_DEFAULT_CONNECT) && s && s->check.alpn_str)