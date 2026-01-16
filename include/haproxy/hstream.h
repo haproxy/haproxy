714131f6a6d823f7819ce7e4dcbbd071f844db26
Frederic Lecaille <flecaille@haproxy.com>
2026-01-16 11:06:10 +0100

    WIP: httpterm: add httpterm sources

---
+++ include/haproxy/hstream.h
@@ -0,0 +1,13 @@
+#ifndef _HAPROXY_HSTREAM_H
+#define  _HAPROXY_HSTREAM_H
+
+#include <haproxy/cfgparse.h>
+#include <haproxy/hstream-t.h>
+
+void init_httpterm_cfg(int argc, char **argv, struct cfgfile *cfg);
+struct task *sc_hstream_io_cb(struct task *t, void *ctx, unsigned int state);
+int hstream_wake(struct stconn *sc);
+void hstream_shutdown(struct stconn *sc);
+void *hstream_new(struct session *sess, struct stconn *sc, struct buffer *input);
+
+#endif /* _HAPROXY_HSTREAM_H */
