ce912271db4e1a793a71484892b7df2c4b7c2b9c
Christopher Faulet <cfaulet@haproxy.com>
2026-02-03 11:55:10 +0100

    MEDIUM: chunk: Add support for large chunks
    Because there is now a memory pool for large buffers, we must also add the
    support for large chunks. So, if large buffers are configured, a dedicated
    memory pool is created to allocate large chunks. alloc_large_trash_chunk()
    must be used to allocate a large chunk. alloc_trash_chunk_sz() can be used to
    allocate a chunk with the best size. However free_trash_chunk() remains the
    only way to release a chunk, regular or large.
    
    In addition, large trash buffers are also created, using the same mechanism
    than for regular trash buffers. So three thread-local trash buffers are
    created. get_large_trash_chunk() must be used to get a large trash buffer.
    And get_trash_chunk_sz() may be used to get a trash buffer with the best
    size.

--- src/chunk.c
+++ src/chunk.c
@@ -16,6 +16,7 @@
 #include <string.h>
 
 #include <haproxy/api.h>
+#include <haproxy/buf.h>
 #include <haproxy/chunk.h>
 #include <haproxy/global.h>
 #include <haproxy/tools.h>
@@ -35,6 +36,22 @@ struct pool_head *pool_head_trash __read_mostly = NULL;
 
 /* this is used to drain data, and as a temporary buffer for sprintf()... */
 THREAD_LOCAL struct buffer trash = { };
+
+/* large trash chunks used for various conversions */
+static THREAD_LOCAL struct buffer *large_trash_chunk;
+static THREAD_LOCAL struct buffer large_trash_chunk1;
+static THREAD_LOCAL struct buffer large_trash_chunk2;
+
+/* large trash buffers used for various conversions */
+static int large_trash_size __read_mostly = 0;
+static THREAD_LOCAL char *large_trash_buf1 = NULL;
+static THREAD_LOCAL char *large_trash_buf2 = NULL;
+
+/* the trash pool for reentrant allocations */
+struct pool_head *pool_head_large_trash __read_mostly = NULL;
+
+/* this is used to drain data, and as a temporary large buffer */
+THREAD_LOCAL struct buffer trash_large = { };
 
 /*
 * Returns a pre-allocated and initialized trash chunk that can be used for any
@@ -62,6 +79,63 @@ struct buffer *get_trash_chunk(void)
 	return trash_chunk;
 }
 
+/* Similar to get_trash_chunk() but return a pre-allocated large chunk
+ * instead. Becasuse large buffers are not enabled by default, this function may
+ * return NULL.
+ */
+struct buffer *get_large_trash_chunk(void)
+{
+	char *large_trash_buf;
+
+	if (!large_trash_size)
+		return NULL;
+
+	if (large_trash_chunk == &large_trash_chunk1) {
+		large_trash_chunk = &large_trash_chunk2;
+		large_trash_buf = large_trash_buf2;
+	}
+	else {
+		large_trash_chunk = &large_trash_chunk1;
+		large_trash_buf = large_trash_buf1;
+	}
+	*large_trash_buf = 0;
+	chunk_init(large_trash_chunk, large_trash_buf, large_trash_size);
+	return large_trash_chunk;
+}
+
+/* Returns a trash chunk accordingly to the requested size. This function may
+ * fail if the requested size is too big or if the large chubks are not
+ * configured.
+ */
+struct buffer *get_trash_chunk_sz(size_t size)
+{
+	if (likely(size <= trash_size))
+	    return get_trash_chunk();
+	else if (large_trash_size && size <= large_trash_size)
+		return get_large_trash_chunk();
+	else
+		return NULL;
+}
+
+/* Returns a larger buffer than <chk> if possible or NULL otherwise. If a larger
+ * buffer is returned, content of <chk> are copied.
+ */
+struct buffer *get_larger_trash_chunk(struct buffer *chk)
+{
+	struct buffer *chunk;
+
+	if (!chk)
+		return get_trash_chunk();
+
+	/* No large buffers or current chunk is alread a large trash chunk */
+	if (!large_trash_size || chk->size == large_trash_size)
+		return NULL;
+
+	chunk = get_large_trash_chunk();
+	b_xfer(chunk, chk, b_data(chk));
+	return chunk;
+}
+
 /* (re)allocates the trash buffers. Returns 0 in case of failure. It is
  * possible to call this function multiple times if the trash size changes.
  */
@@ -74,9 +148,27 @@ static int alloc_trash_buffers(int bufsize)
 	return trash.area && trash_buf1 && trash_buf2;
 }
 
+/* allocates the trash large buffers if necessary. Returns 0 in case of
+ * failure. Unlike alloc_trash_buffers(), It is unexpected to call this function
+ * multiple times. Large buffers are not used during configuration parsing.
+ */
+static int alloc_large_trash_buffers(int bufsize)
+{
+	large_trash_size = bufsize;
+	if (!large_trash_size)
+		return 1;
+
+	BUG_ON(trash_large.area && large_trash_buf1 && large_trash_buf2);
+
+	chunk_init(&trash_large, my_realloc2(trash_large.area, bufsize), bufsize);
+	large_trash_buf1 = (char *)my_realloc2(large_trash_buf1, bufsize);
+	large_trash_buf2 = (char *)my_realloc2(large_trash_buf2, bufsize);
+	return trash_large.area && large_trash_buf1 && large_trash_buf2;
+}
+
 static int alloc_trash_buffers_per_thread()
 {
-	return alloc_trash_buffers(global.tune.bufsize);
+	return alloc_trash_buffers(global.tune.bufsize) && alloc_large_trash_buffers(global.tune.bufsize_large);
 }
 
 static void free_trash_buffers_per_thread()
@@ -84,6 +176,10 @@ static void free_trash_buffers_per_thread()
 	chunk_destroy(&trash);
 	ha_free(&trash_buf2);
 	ha_free(&trash_buf1);
+
+	chunk_destroy(&trash_large);
+	ha_free(&large_trash_buf2);
+	ha_free(&large_trash_buf1);
 }
 
 /* Initialize the trash buffers. It returns 0 if an error occurred. */
@@ -103,6 +199,14 @@ int init_trash_buffers(int first)
 				      MEM_F_EXACT);
 	if (!pool_head_trash || !alloc_trash_buffers(global.tune.bufsize))
 		return 0;
+
+	if (!first && global.tune.bufsize_large) {
+		pool_head_large_trash = create_pool("large_trash",
+						    sizeof(struct buffer) + global.tune.bufsize_large,
+						    MEM_F_EXACT);
+		if (!pool_head_large_trash)
+			return 0;
+	}
 	return 1;
 }
 
