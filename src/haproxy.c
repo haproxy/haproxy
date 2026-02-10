haproxy/haproxy

commit 3674afe8a085613213fcb1e38dc203fa4f7161d5

BUG/MEDIUM: threads: Atomically set TH_FL_SLEEPING and clr FL_NOTIFIED
When we're about to enter polling, atomically set TH_FL_SLEEPING and
remove TH_FL_NOTIFIED, instead of doing it in sequence. Otherwise,
another thread may sett that both the TH_FL_SLEEPING and the
TH_FL_NOTIFIED bits are set, and don't wake up the thread then it should
be doing that.
This prevents a bug where a thread is sleeping while it should be
handling a new connection, which can happen if there are very few
incoming connection. This is easy to reproduce when using only two
threads, and injecting with only one connection, the connection may then
never be handled.

This should be backported up to 2.8.

src/haproxy.c
@@ -2918,9 +2918,12 @@ void run_poll_loop()
 		if (thread_has_tasks())
 			activity[tid].wake_tasks++;
 		else {
-			_HA_ATOMIC_OR(&th_ctx->flags, TH_FL_SLEEPING);
-			_HA_ATOMIC_AND(&th_ctx->flags, ~TH_FL_NOTIFIED);
-			__ha_barrier_atomic_store();
+			unsigned int flags = _HA_ATOMIC_LOAD(&th_ctx->flags);
+
+			while (unlikely(!HA_ATOMIC_CAS(&th_ctx->flags, &flags,
+(flags | TH_FL_SLEEPING) & ~TH_FL_NOTIFIED)))
+				__ha_cpu_relax();
+
 			if (thread_has_tasks()) {
 				activity[tid].wake_tasks++;
 				_HA_ATOMIC_AND(&th_ctx->flags, ~TH_FL_SLEEPING);

commit 3b45beb465a92db5343863e13e13b7f40a4cb6cc

CLEANUP: haproxy: fix bad line wrapping in run_poll_loop()
Commit 3674afe8a0 ("BUG/MEDIUM: threads: Atomically set TH_FL_SLEEPING
and clr FL_NOTIFIED") accidentally left a strange-looking line wrapping
making one think of an editing mistake, let's fix it and keep it on a
single line given that even indented wrapping is almost as large.

This can be backported with the fix above till 2.8 to keep the patch
context consistent between versions.

src/haproxy.c
@@ -2925,8 +2925,7 @@ void run_poll_loop()
 		else {
 			unsigned int flags = _HA_ATOMIC_LOAD(&th_ctx->flags);
 
-			while (unlikely(!HA_ATOMIC_CAS(&th_ctx->flags, &flags,
-(flags | TH_FL_SLEEPING) & ~TH_FL_NOTIFIED)))
+			while (unlikely(!HA_ATOMIC_CAS(&th_ctx->flags, &flags, (flags | TH_FL_SLEEPING) & ~TH_FL_NOTIFIED)))
 				__ha_cpu_relax();
 
 			if (thread_has_tasks()) {