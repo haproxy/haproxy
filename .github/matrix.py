623a256b7836ff0937fa09509b02b66f8a470e75
William Lallemand <wlallemand@haproxy.com>
2026-02-19 14:51:05 +0100

    CI: github: only enable OS X on development branches
    Don't use the macOS job on maintenance branches, it's mainly use for
    development and checking portability, but we don't support actively
    macOS on stable branches.

--- .github/matrix.py
+++ .github/matrix.py
@@ -275,24 +275,21 @@ def main(ref_name):
                 }
             )
 
-    # macOS
-
-    if "haproxy-" in ref_name:
-        os = "macos-13"     # stable branch
-    else:
+    # macOS on dev branches
+    if "haproxy-" not in ref_name:
         os = "macos-26"     # development branch
 
-    TARGET = "osx"
-    for CC in ["clang"]:
-        matrix.append(
-            {
-                "name": "{}, {}, no features".format(os, CC),
-                "os": os,
-                "TARGET": TARGET,
-                "CC": CC,
-                "FLAGS": [],
-            }
-        )
+        TARGET = "osx"
+        for CC in ["clang"]:
+            matrix.append(
+                {
+                    "name": "{}, {}, no features".format(os, CC),
+                    "os": os,
+                    "TARGET": TARGET,
+                    "CC": CC,
+                    "FLAGS": [],
+                }
+            )
 
     # Print matrix
 
