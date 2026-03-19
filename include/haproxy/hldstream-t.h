7f894cc6dc21edb97263bbd3af3e1520d88ce2fe
Frederic Lecaille <flecaille@haproxy.com>
2026-03-19 14:36:21 +0100

    MINOR: hldstream: add definition of hldstream struct objects
    haload is a client-side HTTP benchmarking tool designed to manage
    concurrent HTTP streams.
    
    This patch defines the hldstream C structure, which serves as the
    core object to represent a haload HTTP stream for all the HTTP protocol.
    It will be used by the upcoming haload module to handle specialized
    stream contexts.

---
+++ include/haproxy/hldstream-t.h
@@ -0,0 +1,29 @@
+#ifndef _HAPROXY_HDLSTREAM_T_H
+#define _HAPROXY_HDLSTREAM_T_H
+
+#include <haproxy/buf-t.h>
+#include <haproxy/connection-t.h>
+#include <haproxy/dynbuf-t.h>
+#include <haproxy/obj_type-t.h>
+#include <haproxy/session-t.h>
+#include <haproxy/stconn-t.h>
+#include <haproxy/task-t.h>
+
+struct hldstream {
+	enum obj_type obj_type;
+	struct connection *conn;
+	int64_t hash;
+	struct hld_usr *usr;
+	struct hld_url *url;
+	const char *path;
+	struct stconn *sc;
+	struct buffer bi, bo;
+	struct buffer_wait buf_wait; /* wait list for buffer allocation */
+	struct task *task;
+	int flags;
+	int state;
+	unsigned long long to_send; /* number of body data bytes to send */
+	struct list list;
+};
+
+#endif /* _HAPROXY_HDLSTREAM_T_H */
