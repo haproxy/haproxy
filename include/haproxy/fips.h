9ac590b55e8087a113389da4a503315bab494ca3
William Lallemand <wlallemand@haproxy.com>
2026-06-30 12:38:31 +0000

    MEDIUM: ssl: introduce src/fips.c with TLS version check
    Add src/fips.c and include/haproxy/fips.h to centralise FIPS compliance
    checks for AWS-LC builds.
    
    ssl_fips_check_version() verifies that ssl-min-ver is not set below
    TLS 1.2, which AWS-LC in FIPS mode would refuse to negotiate.
    
    The check accepts an enum obj_type * so it can resolve the owning
    bind/server and produce a precise error message including the proxy
    and object name in the standard HAProxy 'type proxy/name' form.

---
+++ include/haproxy/fips.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+
+#ifndef _HAPROXY_FIPS_H
+#define _HAPROXY_FIPS_H
+
+#ifdef USE_OPENSSL
+#include <haproxy/obj_type-t.h>
+#include <haproxy/openssl-compat.h>
+
+#if defined(OPENSSL_IS_AWSLC)
+int ssl_fips_check_version(int min_ver, const enum obj_type *obj);
+#endif
+
+#endif /* USE_OPENSSL */
+#endif /* _HAPROXY_FIPS_H */
