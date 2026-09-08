8499f76f50caedf52a9c79c6525f7a3ef881b3fb
Willy Tarreau <w@1wt.eu>
2026-09-08 14:09:18 +0200

    CLEANUP: h2: get rid of the now unused h2_make_htx_trailers()
    It's no longer used, let's get rid of this function.

--- include/haproxy/h2.h
+++ include/haproxy/h2.h
@@ -210,7 +210,6 @@ extern struct h2_frame_definition h2_frame_definition[H2_FT_ENTRIES];
 int h2_parse_cont_len_header(unsigned int *msgf, struct ist *value, unsigned long long *body_len);
 int h2_make_htx_request(struct http_hdr *list, struct htx *htx, unsigned int *msgf, unsigned long long *body_len, int relaxed);
 int h2_make_htx_response(struct http_hdr *list, struct htx *htx, unsigned int *msgf, unsigned long long *body_len, char *upgrade_protocol);
-int h2_make_htx_trailers(struct http_hdr *list, struct htx *htx);
 
 /*
  * Some helpful debugging functions.
